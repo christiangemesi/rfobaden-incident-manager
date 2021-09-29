a59a8f0df4bc5b5c0aca8bda2a0c0816c2315034
Daniel von Atzigen <daniel@vatz.ch>
2021-09-29 22:07:14 +0200

    Add eslint and adapt code to it

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ **/
+const config = {
+  eslint: {
+    dirs: [
+      'components',
+      'pages',
+      'utils',
+    ],
+  },
+}
+module.exports = config
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import { AppProps } from 'next/app';
-import React from 'react';
+import { AppProps } from 'next/app'
+import React from 'react'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
