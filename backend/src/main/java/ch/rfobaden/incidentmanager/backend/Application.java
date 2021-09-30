d511fd4db5377884bb074300ea879156d4c81615
Daniel von Atzigen <daniel@vatz.ch>
2021-09-30 13:49:53 +0200

    Add backend code generated by start.spring.io

---
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/Application.java
@@ -0,0 +1,14 @@
+package ch.rfobaden.incidentmanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
