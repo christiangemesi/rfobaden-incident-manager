ab0ff5ae6ed56acaea146cb790aa14127e09375f
Daniel von Atzigen <daniel@vatz.ch>
2021-10-06 15:55:36 +0200

    Adjust checkstyle config

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/Application.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/Application.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello, World!";
-	}
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
 }
-
