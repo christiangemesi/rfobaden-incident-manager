0811fab5e50d01b1478d758a81d2a6207e348759
Daniel von Atzigen <daniel@vatz.ch>
2021-10-06 21:17:27 +0200

    Configure page layout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,27 @@
-import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import React from 'react'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <title key="title">RFOBaden IncidentManager</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   )
 }
 export default App
 
+const GlobalStyle = createGlobalStyle`
+  html, body, #__next {
+    width: 100%;
+    height: 100%;
+    min-height: 100%;
+  }
+`
