afd256f081f5d997a8de1abb71603d358c36b5f1
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-10-07 16:32:18 +0200

    create login page with components

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,12 @@
+import UiContainer from '@/components/Ui/Container/UiContainer'
+import LoginForm from '@/components/Login/Form/LoginForm'
+import React from 'react'
+
+const LoginPage: React.VFC = () => {
+  return (
+    <UiContainer>
+      <LoginForm />
+    </UiContainer>
+  )
+}
+export default LoginPage
