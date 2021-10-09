4553dba6380b51d3e1de2431052f01c992aede3c
Daniel von Atzigen <daniel@vatz.ch>
2021-10-09 18:17:50 +0200

    Show current user on `/login` page

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,11 +1,20 @@
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import LoginForm from '@/components/Login/Form/LoginForm'
 import React from 'react'
+import { useSession } from '@/stores/SessionStore'
 
 const LoginPage: React.VFC = () => {
+  const { currentUser } = useSession()
   return (
     <UiContainer>
       <LoginForm />
+      {currentUser === null ? (
+        'not logged in'
+      ) : (
+        <React.Fragment>
+          logged in as {currentUser.name}
+        </React.Fragment>
+      )}
     </UiContainer>
   )
 }
