01d4ba6a321e3bdddea069f99fc0bb08822d9528
Daniel von Atzigen <daniel@vatz.ch>
2021-10-09 18:13:36 +0200

    Add backend response exception handling

---
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/ApiException.java
@@ -0,0 +1,23 @@
+package ch.rfobaden.incidentmanager.backend;
+
+import org.springframework.http.HttpStatus;
+
+public final class ApiException extends RuntimeException {
+    private final HttpStatus status;
+
+    private final String error;
+
+    public ApiException(HttpStatus status, String error) {
+        super("[" + status + "] " + error);
+        this.status = status;
+        this.error = error;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
---
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/ApiExceptionHandler.java
@@ -0,0 +1,26 @@
+package ch.rfobaden.incidentmanager.backend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ApiErrorResponse> handleApiError(ApiException e) {
+        var res = new ApiErrorResponse(e.getError());
+        return new ResponseEntity<>(res, e.getStatus());
+    }
+
+    public static class ApiErrorResponse {
+        private final String message;
+
+        public ApiErrorResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
