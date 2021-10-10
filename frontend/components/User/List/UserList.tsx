3b5ebcfaaa7addb1dd7dee2314924d30b19cc0e6
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-10-10 16:36:19 +0200

    view user table with first styles

--- frontend/components/User/List/UserList.tsx
+++ frontend/components/User/List/UserList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import User from '@/models/User'
+import styled from 'styled-components'
 
 interface Props {
   users: User[]
@@ -45,3 +46,30 @@ const UserList: React.VFC<Props> = ({ users }) => {
   )
 }
 export default UserList
+
+const StyledTable = styled.table`
+  display: block;
+  width: 100%;
+`
+const StyledTr = styled.tr`
+  width: 100%;
+  :nth-child(2n){
+    background-color:lightgray;
+  }
+`
+const StyledTh = styled.th`
+  padding:5px;
+  font-weight:bold;
+  text-align:left;
+`
+const StyledTd = styled.td`
+  width:100%;
+  padding:5px;
+`
+const StyledTdSmall = styled.td`
+  width: 40px;
+  padding:5px;
+  [src=""]{
+    color: red;
+  }
+`
