a3ed750be8414ba9386023d42d55b712e95bc139
Lucifer Uchiha <rowla101@gmail.com>
2021-10-11 20:12:31 +0200

    Started on UserRepository tests

---
+++ backend/src/test/java/ch/rfobaden/incidentmanager/backend/user/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package ch.rfobaden.incidentmanager.backend.user;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@DataJpaTest
+//@ExtendWith(SpringExtension.class)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // To use MySQL
+public class UserRepositoryTest {
+
+//    @Autowired
+//    private UserRepository userRepository;
+
+    // TODO get help with "Failed to load ApplicationContext" Exception
+//    @Test
+//    public void testAddUser() {
+//        //Given
+//        User newUser = new User("newUser", "newPassword");
+//        //When
+//        User savedUser = userRepository.save(newUser);
+//        //Then
+//        assertThat(savedUser.getId()).isGreaterThan(0);
+//    }
+}
