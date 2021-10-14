0f839512f352c945535c4df9cbe2b49045fd15ff
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-10-14 13:41:27 +0200

    Add `UiButton` component

---
+++ frontend/components/Ui/Button/UiButton.tsx
@@ -0,0 +1,49 @@
+import styled, { css } from 'styled-components'
+import { EventHandler, ReactNode, MouseEvent, CSSProperties } from 'react'
+import StyleHelper from '@/utils/helpers/StyleHelper'
+
+interface Props {
+  /**
+   * Disables the button.
+   */
+  isDisabled?: boolean
+
+  /**
+   * Causes the button to take up the full width available to it.
+   */
+  isFull?: boolean
+
+  /**
+   * Shows a tooltip when hovering the button.
+   */
+  tooltip?: string
+
+  children?: ReactNode
+  onClick?: EventHandler<MouseEvent>
+  className?: string
+  style?: CSSProperties
+}
+
+const UiButton = styled(StyleHelper.tag('button', ({
+  isDisabled = false,
+  tooltip,
+}: Props) => ({
+  disabled: isDisabled,
+  title: tooltip,
+})))`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  padding: 0.25rem;
+  
+  ${({ isFull }) => isFull && css`
+    width: 100%;
+  `}
+  
+  :hover:not(&[disabled]) {
+    cursor: pointer;
+  }
+`
+export default UiButton
+
