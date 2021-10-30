0bd3bc1827924aa5139ac7c9740df947597dae07
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-10-30 21:22:16 +0200

    Implement `IncidentView` component

---
+++ frontend/components/Incident/View/IncidentView.tsx
@@ -0,0 +1,95 @@
+import Incident from '@/models/Incident'
+import React, { ReactNode, Ref } from 'react'
+import styled from 'styled-components'
+import { useUser } from '@/stores/UserStore'
+import UiGrid from '@/components/Ui/Grid/UiGrid'
+
+interface Props {
+  incident: Incident
+  innerRef?: Ref<HTMLDivElement>
+}
+
+const IncidentView: React.VFC<Props> = ({ incident, innerRef }) => {
+  const author = useUser(incident.authorId)
+
+  return (
+    <Container ref={innerRef}>
+      <h1>
+        {incident.title}
+      </h1>
+      {author !== null && (
+        <div>
+          Erstellt von {author.name}
+        </div>
+      )}
+      <div style={{ width: '100%' }}>
+        <UiGrid style={{ justifyContent: 'center' }}>
+          <UiGrid.Col size={{ md: 8, lg: 5 }}>
+            <Info name="Status">
+              {getStatusMessage(incident)}
+            </Info>
+            {incident.isClosed && (
+              <>
+                <Info name="Abschlussgrund">
+                  {incident.closeReason}
+                </Info>
+              </>
+            )}
+
+            <div style={{ marginTop: '1rem' }} />
+            <Info name="erstellt am">
+              {incident.createdAt.toLocaleString()}
+            </Info>
+            <Info name="zuletzt bearbeitet am">
+              {incident.updatedAt.toLocaleString()}
+            </Info>
+          </UiGrid.Col>
+        </UiGrid>
+      </div>
+      <article style={{ marginTop: '1.5rem' }}>
+        {incident.description}
+      </article>
+    </Container>
+  )
+}
+export default IncidentView
+
+interface InfoProp {
+  name: string
+  children: ReactNode
+}
+
+const Info: React.VFC<InfoProp> = ({ name, children }) => {
+  return (
+    <UiGrid gap={1}>
+      <UiGrid.Col size={6} style={{ textAlign: 'right' }}>
+        <span style={{ fontWeight: 600 }}>
+          {name}
+        </span>
+      </UiGrid.Col>
+      <UiGrid.Col>
+        {children}
+      </UiGrid.Col>
+    </UiGrid>
+  )
+}
+
+const getStatusMessage = (incident: Incident): string => {
+  if (incident.isClosed) {
+    return 'geschlossen'
+  }
+  if (incident.startsAt !== null && incident.startsAt > new Date()) {
+    return 'ungeöffnet'
+  }
+  if (incident.endsAt !== null && incident.endsAt < new Date()) {
+    return 'vorbei'
+  }
+  return 'geöffnet'
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
