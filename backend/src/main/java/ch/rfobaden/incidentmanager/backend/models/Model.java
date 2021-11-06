1bbd5ab4e9af3f9948c750ac0214f7bdb1fda906
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-11-06 09:51:23 +0100

    Add abstract `Model` as model superclass

---
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Model.java
@@ -0,0 +1,68 @@
+package ch.rfobaden.incidentmanager.backend.models;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public abstract boolean equals(Object other);
+
+    protected final boolean equalsModel(Model that) {
+        return Objects.equals(id, that.id)
+            && Objects.equals(createdAt, that.createdAt)
+            && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public abstract int hashCode();
+
+    protected final int modelHashCode() {
+        return Objects.hash(id, createdAt, updatedAt);
+    }
+
+    @Override
+    public abstract String toString();
+}
