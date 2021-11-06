4537b88dbe62eb8401270e3d8a0ca7bf459bc78d
Andri Wild <andri.wild@students.fhnw.ch>
2021-11-06 10:22:34 +0100

    Add  class

---
+++ backend/src/test/java/ch/rfobaden/incidentmanager/backend/controllers/ReportControllerTest.java
@@ -0,0 +1,8 @@
+package ch.rfobaden.incidentmanager.backend.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ReportController.class)
+public class ReportControllerTest {
+
+}
