97e26db46a55b8aee53016856e8cb1ed618c490a
Andri Wild <andri.wild@students.fhnw.ch>
2021-11-06 10:12:00 +0100

    Add controller methods

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/ReportController.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/ReportController.java
@@ -1,20 +1,55 @@
 package ch.rfobaden.incidentmanager.backend.controllers;
 
+import ch.rfobaden.incidentmanager.backend.errors.ApiException;
+import ch.rfobaden.incidentmanager.backend.models.Report;
 import ch.rfobaden.incidentmanager.backend.services.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/report")
 public class ReportController {
 
-    private ReportService reportService;
+    private final ReportService reportService;
 
     @Autowired
     public ReportController(ReportService reportService) {
         this.reportService = reportService;
     }
 
+    @GetMapping
+    public List<Report> getReports() {
+        return reportService.getReports();
+    }
 
+    @GetMapping({"{reportId}"})
+    public Report getReportById(@PathVariable("reportId") Long reportId) {
+        return reportService.getReportById(reportId).orElseThrow(() -> (
+            new ApiException(HttpStatus.NOT_FOUND, "report not found")
+        ));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Report addNewReport(@RequestBody Report report) {
+        return reportService.createReport(report);
+    }
+
+    @DeleteMapping("{reportId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteReportById(@PathVariable("reportId") Long reportId) {
+        if (!reportService.deleteReportById(reportId)) {
+            throw new ApiException(HttpStatus.NOT_FOUND, "report not found");
+        }
+    }
 }
