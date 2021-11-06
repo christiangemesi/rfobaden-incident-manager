76907cc2ecb301e38f4d4a42de41863f4568929d
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-11-06 09:59:16 +0100

    Adapt `UserController` to new model

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/UserController.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/UserController.java
@@ -1,7 +1,10 @@
 package ch.rfobaden.incidentmanager.backend.controllers;
 
+import ch.rfobaden.incidentmanager.backend.controllers.base.ModelController;
 import ch.rfobaden.incidentmanager.backend.errors.ApiException;
 import ch.rfobaden.incidentmanager.backend.models.User;
+import ch.rfobaden.incidentmanager.backend.models.UserCredentials;
+import ch.rfobaden.incidentmanager.backend.services.UserCredentialsService;
 import ch.rfobaden.incidentmanager.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,53 +12,19 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Controller contains all the API mapping. This is part of the N-Tier pattern.
- */
 @RestController
 @RequestMapping(path = "api/v1/users")
-public class UserController {
-
-    private final UserService userService;
-
-    // This is Dependency injection
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping
-    public List<User> getUsers() {
-        return userService.getUsers();
-    }
-
-
-    @GetMapping(value = "{userId}")
-    public User getUserById(@PathVariable(value = "userId") Long userId) {
-        return userService.getUserById(userId).orElseThrow(() -> (
-                new ApiException(HttpStatus.NOT_FOUND, "user not found")
-        ));
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public User addNewUser(@RequestBody User user) {
-        return userService.addNewUser(user);
-    }
-
-    @DeleteMapping(value = "{userId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUserById(
-        @PathVariable(value = "userId") Long userId) {
-        if (!userService.deleteUserById(userId)) {
-            throw new ApiException(HttpStatus.NOT_FOUND, "user not found");
-        }
+public final class UserController extends ModelController<User, UserService> {
+    public UserController(UserService service) {
+        super(service);
     }
 }
