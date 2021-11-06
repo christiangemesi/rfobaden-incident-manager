9fa7d5e33363f3c3777c4596818ba02b5d8880bd
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-11-06 09:54:19 +0100

    Add `ModelRepositoryService` as abstrasct implementation of `ModelService`

---
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/base/ModelRepositoryService.java
@@ -0,0 +1,76 @@
+package ch.rfobaden.incidentmanager.backend.services.base;
+
+import ch.rfobaden.incidentmanager.backend.errors.ApiException;
+import ch.rfobaden.incidentmanager.backend.errors.UpdateConflictException;
+import ch.rfobaden.incidentmanager.backend.models.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public abstract class ModelRepositoryService<
+    TModel extends Model,
+    TRepository extends JpaRepository<TModel, Long>> implements ModelService<TModel> {
+
+    protected final TRepository repository;
+
+    public ModelRepositoryService(TRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Optional<TModel> find(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<TModel> list() {
+        return repository.findAll();
+    }
+
+    @Override
+    public TModel create(TModel record) {
+        if (record.getCreatedAt() != null) {
+            throw new IllegalArgumentException("createdAt must not be set");
+        }
+        if (record.getUpdatedAt() != null) {
+            throw new IllegalArgumentException("updatedAt must not be set");
+        }
+        record.setCreatedAt(LocalDateTime.now());
+        record.setUpdatedAt(record.getCreatedAt());
+        return repository.save(record);
+    }
+
+    @Override
+    public Optional<TModel> update(TModel record) {
+        if (record.getCreatedAt() != null) {
+            throw new IllegalArgumentException("createdAt must not be set");
+        }
+        if (record.getUpdatedAt() == null) {
+            throw new IllegalArgumentException("updatedAt must be set");
+        }
+
+        var existingRecord = find(record.getId()).orElse(null);
+        if (existingRecord == null) {
+            return Optional.empty();
+        }
+        if (!Objects.equals(existingRecord.getUpdatedAt(), record.getUpdatedAt())) {
+            throw new UpdateConflictException("record " + record.getId() + " has already been modified");
+        }
+        record.setCreatedAt(existingRecord.getCreatedAt());
+        record.setUpdatedAt(LocalDateTime.now());
+        return Optional.of(repository.save(record));
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
