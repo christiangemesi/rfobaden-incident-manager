77e6a90825e341f69df2dff9c814ade022d3f7a7
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-11-06 09:56:06 +0100

    Rework `User` model

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/User.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/User.java
@@ -1,47 +1,73 @@
 package ch.rfobaden.incidentmanager.backend.models;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * This is a JPA Entity that will be represented in the database.
- */
+
 @Entity
 @Table(name = "user")
-public class User {
+public final class User extends Model {
+    @Column(nullable = false, unique = true)
+    private String email;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String username;
-    private String password;
+    @Column(nullable = false)
+    private String firstName;
 
-    public User() {
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Role role;
+
+    public String getEmail() {
+        return email;
     }
 
-    public User(long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     @Override
     public String toString() {
         return "User{"
-                + "id="
-                + id
-                + ", username='" + username + '\''
-                + ", password='" + password + '\''
-                + '}';
+            + "id=" + getId()
+            + ", email='" + email + '\''
+            +  ", firstName='" + firstName + '\''
+            + ", lastName='" + lastName + '\''
+            + ", role=" + role
+            + ", createdAt=" + getCreatedAt()
+            + ", updatedAt=" + getUpdatedAt()
+            + '}';
     }
 
     @Override
@@ -53,37 +79,20 @@ public class User {
             return false;
         }
         var that = (User) other;
-        return id == that.id
-                && Objects.equals(username, that.username)
-                && Objects.equals(password, that.password);
+        return equalsModel(that)
+            && Objects.equals(email, that.email)
+            && Objects.equals(firstName, that.firstName)
+            && Objects.equals(lastName, that.lastName)
+            && Objects.equals(role, that.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, password);
+        return Objects.hash(modelHashCode(), email, firstName, lastName, role);
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public enum Role {
+        CREATOR,
+        ADMIN,
     }
 }
