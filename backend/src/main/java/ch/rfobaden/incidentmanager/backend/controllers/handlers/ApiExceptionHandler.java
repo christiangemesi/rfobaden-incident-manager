224e528653b31dadfd998864643f80da9b668061
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-11-06 09:57:14 +0100

    Handle `UpdateConflictException` in controllers

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/handlers/ApiExceptionHandler.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/handlers/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package ch.rfobaden.incidentmanager.backend.controllers.handlers;
 
 import ch.rfobaden.incidentmanager.backend.errors.ApiException;
+import ch.rfobaden.incidentmanager.backend.errors.UpdateConflictException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,13 @@ public class ApiExceptionHandler {
         var res = new ApiErrorResponse(e.getError());
         return new ResponseEntity<>(res, e.getStatus());
     }
+
+    @ExceptionHandler(UpdateConflictException.class)
+    public ResponseEntity<ApiErrorResponse> handleApiError(UpdateConflictException e) {
+        var res = new ApiErrorResponse("update conflict: the resource has already been modified");
+        return new ResponseEntity<>(res, HttpStatus.PRECONDITION_REQUIRED);
+    }
+
 
     public static class ApiErrorResponse {
         private final String message;
