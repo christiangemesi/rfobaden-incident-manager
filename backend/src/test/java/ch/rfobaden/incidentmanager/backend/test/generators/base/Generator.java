450c20f7bc64a2101d30518b6ba9936fbb76b8be
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-11-08 17:30:43 +0100

    Add `Generator` as more generalized supertype of `ModelGenerator`

---
+++ backend/src/test/java/ch/rfobaden/incidentmanager/backend/test/generators/base/Generator.java
@@ -0,0 +1,79 @@
+package ch.rfobaden.incidentmanager.backend.test.generators.base;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public abstract class Generator<T> {
+    public abstract T generate();
+
+    public final List<T> generate(int amount) {
+        return generate(amount, this::generate);
+    }
+
+    /**
+     * Shallow copy of a value, using reflection.
+     * Not very pretty, not entirely safe, definitely only usable in testing.
+     *
+     * <p>
+     * This could be made abstract if we ever want to move away from the unsafe,
+     * reflection-based type of copying.
+     * </p>
+     *
+     * @param value the value to copy
+     *
+     * @return a shallow copy of {@code value}
+     */
+    public T copy(T value) {
+        try {
+            Constructor<T> defaultConstructor = null;
+            @SuppressWarnings("unchecked") var constructors =
+                (Constructor<T>[]) value.getClass().getConstructors();
+            for (var constructor : constructors) {
+                if (constructor.getParameterCount() == 0) {
+                    defaultConstructor = constructor;
+                    break;
+                }
+            }
+            if (defaultConstructor == null) {
+                throw new IllegalStateException("can't copy without a default constructor");
+            }
+            var newValue = defaultConstructor.newInstance();
+            var methods = value.getClass().getMethods();
+            Arrays.stream(methods)
+                .filter(method -> method.getName().startsWith("get"))
+                .filter(method -> method.getParameterCount() == 0)
+                .forEach(getter -> {
+                    var fieldName = getter.getName().substring(3);
+                    var setter = Arrays.stream(methods)
+                        .filter(method -> method.getName().startsWith("set" + fieldName))
+                        .filter(method -> method.getParameterCount() == 1)
+                        .findFirst()
+                        .orElse(null);
+                    if (setter == null) {
+                        return;
+                    }
+                    try {
+                        var propertyValue = getter.invoke(value);
+                        setter.invoke(newValue, propertyValue);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new IllegalStateException("failed to copy " + fieldName);
+                    }
+                });
+            return newValue;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalStateException("failed to copy", e);
+        }
+    }
+
+    protected static <T> List<T> generate(int amount, Supplier<T> generate) {
+        var records = new ArrayList<T>(amount);
+        for (int i = 0; i < amount; i++) {
+            records.add(generate.get());
+        }
+        return records;
+    }
+}
--- backend/src/test/java/ch/rfobaden/incidentmanager/backend/test/generators/base/ModelGenerator.java
+++ backend/src/test/java/ch/rfobaden/incidentmanager/backend/test/generators/base/ModelGenerator.java
@@ -7,22 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 
 @Component
 @Import(TestConfig.class)
-public abstract class ModelGenerator<T extends Model> {
+public abstract class ModelGenerator<T extends Model> extends Generator<T> {
     @Autowired
     protected Faker faker;
 
     public final Long generateId() {
         return faker.number().numberBetween(1, Long.MAX_VALUE);
+    }
+
+    @Override
+    public T generate() {
+        return generatePersisted();
     }
 
     public abstract T generateNew();
@@ -53,69 +53,5 @@ public abstract class ModelGenerator<T extends Model> {
             LocalDateTime.now().minusDays(faker.random().nextInt(0, 365 * 1000))
         );
         return storedRecord;
-    }
-
-    private List<T> generate(int amount, Supplier<T> generate) {
-        var records = new ArrayList<T>(amount);
-        for (int i = 0; i < amount; i++) {
-            records.add(generate.get());
-        }
-        return records;
-    }
-
-    /**
-     * Shallow copy of a record, using reflection.
-     * Not very pretty, not entirely safe, definitely only usable in testing.
-     *
-     * <p>
-     * This could be made abstract if we ever want to move away from the unsafe,
-     * reflection-based type of copying.
-     * </p>
-     *
-     * @param record the record to copy
-     *
-     * @return a shallow copy of {@code record}
-     */
-    public final T copy(T record) {
-        try {
-
-            Constructor<T> defaultConstructor = null;
-            @SuppressWarnings("unchecked") var constructors =
-                (Constructor<T>[]) record.getClass().getConstructors();
-            for (var constructor : constructors) {
-                if (constructor.getParameterCount() == 0) {
-                    defaultConstructor = constructor;
-                    break;
-                }
-            }
-            if (defaultConstructor == null) {
-                throw new IllegalStateException("can't copy without a default constructor");
-            }
-            var newRecord = defaultConstructor.newInstance();
-            var methods = record.getClass().getMethods();
-            Arrays.stream(methods)
-                .filter(method -> method.getName().startsWith("get"))
-                .filter(method -> method.getParameterCount() == 0)
-                .forEach(getter -> {
-                    var fieldName = getter.getName().substring(3);
-                    var setter = Arrays.stream(methods)
-                        .filter(method -> method.getName().startsWith("set" + fieldName))
-                        .filter(method -> method.getParameterCount() == 1)
-                        .findFirst()
-                        .orElse(null);
-                    if (setter == null) {
-                        return;
-                    }
-                    try {
-                        var value = getter.invoke(record);
-                        setter.invoke(newRecord, value);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        throw new IllegalStateException("failed to copy " + fieldName);
-                    }
-                });
-            return newRecord;
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new IllegalStateException("failed to copy", e);
-        }
     }
 }
