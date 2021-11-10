8e4b1d054cd74a7d847f93eb8c8082f4caf06def
Arian Sejdiu <arian.sejdiu@students.fhnw.ch>
2021-11-10 11:31:14 +0100

    - Used Container - Created title - Imported and used ReportList

--- frontend/pages/meldungen.tsx
+++ frontend/pages/meldungen.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import EreignissePage from '@/pages/ereignisse'
 import ReportList from '@/components/Report/List/ReportList'
 import Report from '@/models/Report'
+import UiContainer from '@/components/Ui/Container/UiContainer'
+import UiGrid from '@/components/Ui/Grid/UiGrid'
+import IncidentList from '@/components/Incident/List/IncidentList'
 
 const MeldungenPage: React.VFC = () => {
   const reports: Report[] = [
@@ -19,12 +22,18 @@ const MeldungenPage: React.VFC = () => {
   ]
 
 
-
-
   return (
-    <div>
-      <ReportList reports={reports}/>
-    </div>
+    <UiContainer>
+      <h1>
+        Meldungen
+      </h1>
+      <UiGrid style={{ justifyContent: 'center' }}>
+        <UiGrid.Col size={{ md: 10, lg: 8, xl: 6 }}>
+          <ReportList reports={reports}/>
+        </UiGrid.Col>
+      </UiGrid>
+    </UiContainer>
+
   )
 
 }
