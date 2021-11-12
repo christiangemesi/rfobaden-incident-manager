bcae4369dc070a5a55479f34486617c170a408a5
ArianSejdiu <arian.sejdiu@students.fhnw.ch>
2021-11-12 09:19:26 +0100

    - Updated attributes - Created parseReport method

--- frontend/models/Report.ts
+++ frontend/models/Report.ts
@@ -1,10 +1,24 @@
 import Model from '@/models/base/Model'
 import Id from '@/models/base/Id'
 import User from '@/models/User'
+import Incident from './Incident'
+import { parseDate } from './Date'
 
 export default interface Report extends Model {
   title: string
-  description: string
+  description: string | null
+  incident: Incident
+  authorId: Id<User>
   assigneeId: Id<User>
+  createdAt: Date
+  updatedAt: Date
+}
 
+export const parseReport = (data: unknown): Report => {
+  const report = data as Report
+  return {
+    ...report,
+    createdAt: parseDate(report.createdAt),
+    updatedAt: parseDate(report.updatedAt),
+  }
 }
