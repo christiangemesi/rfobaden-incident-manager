8f9ccfd124b230f5c4ee0382d389f26a4d8c9662
ArianSejdiu <arian.sejdiu@students.fhnw.ch>
2021-11-12 09:37:25 +0100

    - Changed to database

--- frontend/pages/meldungen.tsx
+++ frontend/pages/meldungen.tsx
@@ -3,21 +3,10 @@ import ReportList from '@/components/Report/List/ReportList'
 import Report from '@/models/Report'
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import UiGrid from '@/components/Ui/Grid/UiGrid'
+import ReportStore, { useReports } from '@/stores/ReportStore'
 
 const MeldungenPage: React.VFC = () => {
-  const reports: Report[] = [
-    {
-      title: 'Meldung 0: ', description: 'Lorem ipsum dolor sit amet, ... ', assigneeId: -1, id: 0,
-    },
-
-    {
-      title: 'Meldung 1: ', description: 'Lorem ipsum dolor sit amet, ... ', assigneeId: -1, id: 1,
-    },
-
-    {
-      title: 'Meldung 2: ', description: 'Lorem ipsum dolor sit amet, ... ', assigneeId: -1, id: 2,
-    },
-  ]
+  const reports = useReports()
 
 
   return (
