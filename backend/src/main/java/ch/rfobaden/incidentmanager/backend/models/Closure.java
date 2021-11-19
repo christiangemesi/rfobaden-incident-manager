a3af5f2752c886ee414106b77bb3d1fdcb17bd38
Andri Wild <andri.wild@students.fhnw.ch>
2021-11-19 15:06:14 +0100

    Add fields and methods to Closure class

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Closure.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Closure.java
@@ -1,2 +1,60 @@
-package ch.rfobaden.incidentmanager.backend.models;public class Closure {
+package ch.rfobaden.incidentmanager.backend.models;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+// TODO check class name
+public class Closure {
+    private String reason;
+
+    private LocalDateTime closedAt;
+
+    public Closure() {
+    }
+
+    public Closure(String closeReason) {
+        this.reason = closeReason;
+        this.closedAt = LocalDateTime.now();
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public LocalDateTime getClosedAt() {
+        return closedAt;
+    }
+
+    public void setClosedAt(LocalDateTime closedAt) {
+        this.closedAt = closedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Closure closure = (Closure) o;
+        return reason.equals(closure.reason) && closedAt.equals(closure.closedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reason, closedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Closure{"
+                + "reason='" + reason + '\''
+                + ", closedAt=" + closedAt
+                + '}';
+    }
 }
