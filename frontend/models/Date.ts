fd85586946c65be79c088e7190a8d75f2413c996
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-11-20 14:26:13 +0100

    Fix Date parsing

--- frontend/models/Date.ts
+++ frontend/models/Date.ts
@@ -1,4 +1,4 @@
 export const parseDate = (date: unknown): Date => {
-  const [year, month, day, hours, minutes, seconds, ms] = date as [number, number, number, number, number, number, number]
-  return new Date(year, month, day, hours, minutes,seconds,ms)
+  const [year, month, day, hours, minutes, seconds, ns] = date as [number, number, number, number, number, number, number]
+  return new Date(Date.UTC(year, month - 1, day, hours, minutes, seconds, ns / 1000000))
 }
