814305508e21ec162368ee8653648adc9d5841e7
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2021-11-30 12:07:21 +0100

    create UiTitleComponent

---
+++ frontend/pages/examples/UiTitle.tsx
@@ -0,0 +1,67 @@
+import React, { ReactNode } from 'react'
+import styled from 'styled-components'
+
+interface Props {
+  children: ReactNode
+  level: number
+}
+
+const UiTitle: React.VFC<Props> = ({ children, level }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const H: any = `h${level}`
+  console.log(H)
+  //<span style = {{ color: 'black', fontSize: 24 }} />
+  return (
+    <StyledTitle as={H}>
+      {children}
+    </StyledTitle>
+  )
+}
+export default UiTitle
+
+
+//https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+const StyledTitle = styled.div`
+  h1& {
+    font-weight: 300;
+    font-size: 52px;
+    line-height: 64px;
+    letter-spacing: 0.2px;
+  }
+
+  h2& {
+    font-weight: 500;
+    font-size: 44px;
+    line-height: 54px;
+    letter-spacing: 0;
+  }
+
+  h3& {
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 40px;
+    letter-spacing: 0.1px;
+  }
+  h4& {
+    font-weight: 500;
+    font-size: 26px;
+    line-height: 32px;
+    letter-spacing: 0.2px;
+  }
+  h5& {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 26px;
+    letter-spacing: 0.2px;
+  }
+  h6& {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 24px;
+    letter-spacing: 0.2px;
+  }
+  
+  
+  
+
+`
