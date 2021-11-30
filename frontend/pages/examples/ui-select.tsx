0a31fb9e900e8ac5c4e9e47cd55c69e0412e8af3
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-11-30 22:25:47 +0100

    - optimized example select ui component

--- frontend/pages/examples/ui-select.tsx
+++ frontend/pages/examples/ui-select.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import UiGrid from '@/components/Ui/Grid/UiGrid'
 import UiSelectInput from '@/components/Ui/Input/Select/UiSelectInput'
-import UiIcon from '@/components/Ui/Icon/UiIcon'
 
 const UiButtonExample: React.VFC = () => {
   const options1 = [
@@ -11,19 +10,15 @@ const UiButtonExample: React.VFC = () => {
     'Option 3',
     'Option 4',
   ]
-  const options2 = [
-    '<UiIcon.PriorityHigh />',
-    '<UiIcon.PriorityMedium />',
-    '<UiIcon.PriorityLow />',
-  ]
+  const [currentValue, setCurrentValue] = useState<string | null>(null)
+
   return (
     <UiContainer>
       <UiGrid gap={0.5}>
         <UiGrid.Col>
-          <UiSelectInput options={options1} />
-        </UiGrid.Col>
-        <UiGrid.Col>
-          <UiSelectInput options={options2} />
+          <UiSelectInput label="options" options={options1} value={currentValue} onChange={(newValue) => {
+            setCurrentValue(newValue)
+          }} />
         </UiGrid.Col>
       </UiGrid>
     </UiContainer>
