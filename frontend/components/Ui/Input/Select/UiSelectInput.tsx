3582b3693d435ab91f537163e2a651a6f43fb4c5
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-11-30 20:24:46 +0100

    - updated default select to react select to style

--- frontend/components/Ui/Input/Select/UiSelectInput.tsx
+++ frontend/components/Ui/Input/Select/UiSelectInput.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, useCallback, useMemo } from 'react'
+import React, { useMemo } from 'react'
+import Select from 'react-select'
 import styled from 'styled-components'
 import UiInputErrors from '@/components/Ui/Input/Errors/UiInputErrors'
 import { UiInputProps } from '@/components/Ui/Input'
@@ -6,55 +7,33 @@ import { UiInputProps } from '@/components/Ui/Input'
 interface Props<T> extends UiInputProps<T | null> {
   label?: string
   options: T[]
-  optionValue?: keyof T | ((option: T) => string | number | null)
-  optionName?: keyof T | ((option: T) => string | number | null)
+  optionName?: keyof T | ((option: T) => string | null)
 }
 
-const UiSelectInput = <T,>({
+const UiSelectInput = <T, >({
   value = null,
   onChange: setValue,
   errors,
   label,
   options,
-  optionValue,
   optionName,
 }: Props<T>): JSX.Element => {
-  const getOptionValue = useOptionAttribute(optionValue)
-  const getOptionName = useOptionAttribute(optionName)
+  const optionToLabel = useOptionAttribute(optionName)
+  const mappedOptions: Option<T>[] = useMemo(() => (
+    options.map((option) => {
+      return { value: option, label: optionToLabel(option) ?? '' }
+    })
+  ), [optionToLabel, options])
 
-  const optionMapping: Map<string, T> = useMemo(() => {
-    const mapping = new Map<string, T>()
-    for (const option of options) {
-      const optionValue = getOptionValue(option)
-      if (optionValue === null) {
-        throw new Error(`option has no value: ${option}`)
-      }
-      const key = `${optionValue}`
-      if (mapping.has(key)) {
-        throw new Error(`duplicate option value: ${optionValue}`)
-      }
-      mapping.set(key, option)
-    }
-    return mapping
-  }, [options, getOptionValue])
+  const defaultValue = useMemo(()=>(
+    value === null ? null : { value, label: optionToLabel(value) ?? '' }
+  ), [optionToLabel, value])
 
-  const handleChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    const optionValue = e.target.value
-    if (optionValue.length === 0) {
-      // Default, empty option. Set the value to null.
-      if (setValue) {
-        setValue(null as unknown as T)
-      }
-      return
-    }
-    const option = optionMapping.get(optionValue)
-    if (option === undefined) {
-      throw new Error(`unknown option value: ${optionValue}`)
-    }
+  const handleChange = (e: Option<T> | null) => {
     if (setValue) {
-      setValue(option)
+      setValue(e?.value ?? null)
     }
-  }, [optionMapping, setValue])
+  }
 
   const Label = label == null ? 'div' : StyledLabel
 
@@ -65,33 +44,32 @@ const UiSelectInput = <T,>({
           {label}
         </span>
       )}
-      <StyledSelect
-        value={value === null ? undefined : getOptionValue(value) ?? undefined}
+      <Select
+        options={mappedOptions}
+        value={defaultValue}
         onChange={handleChange}
-      >
-        <option />
-        {[...optionMapping.entries()].map(([optionValue, option]) => (
-          <option key={optionValue} value={optionValue}>
-            {getOptionName(option)}
-          </option>
-        ))}
-      </StyledSelect>
+      />
       <UiInputErrors errors={errors} />
     </Label>
   )
 }
 export default UiSelectInput
 
+interface Option<T> {
+  value: T
+  label: string
+}
+
 type OptionAttribute<T> =
   | undefined
   | keyof T
-  | ((option: T) => string | number | null)
+  | ((option: T) => string | null)
 
-const useOptionAttribute = <T,>(attr: OptionAttribute<T>): (option: T) => string | number | null => {
+const useOptionAttribute = <T, >(attr: OptionAttribute<T>): (option: T) => string | null => {
   return useMemo(() => {
     if (attr === undefined) {
       return (option) => {
-        if (typeof option === 'string' || typeof option == 'number') {
+        if (typeof option === 'string') {
           return option
         }
         throw new Error(`option is not a string nor a number: ${option}`)
@@ -102,25 +80,13 @@ const useOptionAttribute = <T,>(attr: OptionAttribute<T>): (option: T) => string
     }
     return (option) => {
       const value = option[attr]
-      if (typeof value === 'string' || typeof value == 'number') {
+      if (typeof value === 'string') {
         return value
       }
       throw new Error(`option value '${option}' is not a string nor a number: ${value}`)
     }
   }, [attr])
 }
-
-const StyledSelect = styled.select`
-  padding: 0.25rem;
-  font-size: 0.9rem;
-  border-radius: 0.25rem;
-  outline: none;
-  
-  border: 1px solid gray;
-  :active, :focus {
-    border-color: cornflowerblue;
-  }
-`
 
 const StyledLabel = styled.label`
   display: flex;
@@ -134,4 +100,45 @@ const StyledLabel = styled.label`
     font-weight: bold;
   }
 `
+
+//   const getOptionValue = useOptionAttribute(optionValue)
+//   const getOptionName = useOptionAttribute(optionName)
+//
+//   const optionMapping: Map<string, T> = useMemo(() => {
+//     const mapping = new Map<string, T>()
+//     for (const option of options) {
+//       const optionValue = getOptionValue(option)
+//       if (optionValue === null) {
+//         throw new Error(`option has no value: ${option}`)
+//       }
+//       const key = `${optionValue}`
+//       if (mapping.has(key)) {
+//         throw new Error(`duplicate option value: ${optionValue}`)
+//       }
+//       mapping.set(key, option)
+//     }
+//     return mapping
+//   }, [options, getOptionValue])
+//
+//   const handleChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+//     const optionValue = e.target.value
+//     if (optionValue.length === 0) {
+//       // Default, empty option. Set the value to null.
+//       if (setValue) {
+//         setValue(null as unknown as T)
+//       }
+//       return
+//     }
+//     const option = optionMapping.get(optionValue)
+//     if (option === undefined) {
+//       throw new Error(`unknown option value: ${optionValue}`)
+//     }
+//     if (setValue) {
+//       setValue(option)
+//     }
+//   }, [optionMapping, setValue])
+//
+//   const Label = label == null ? 'div' : StyledLabel
+//
+
 
