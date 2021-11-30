ede1e330181efdd79fd7ed65a4bc0b04f79f3acd
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-11-30 13:03:02 +0100

    - redesigning ui button component

--- frontend/components/Ui/Button/UiButton.tsx
+++ frontend/components/Ui/Button/UiButton.tsx
@@ -1,21 +1,17 @@
 import styled, { css } from 'styled-components'
 import { EventHandler, ReactNode, MouseEvent, CSSProperties } from 'react'
 import StyleHelper from '@/utils/helpers/StyleHelper'
+import { ColorName } from '@/theme'
+import { contrastDark } from '@/theme'
 
 interface Props {
-  /**
-   * Disables the button.
-   */
+  // disables button
   isDisabled?: boolean
 
-  /**
-   * Causes the button to take up the full width available to it.
-   */
+  // take up the full width possible
   isFull?: boolean
 
-  /**
-   * Shows a tooltip when hovering the button.
-   */
+  // tooltip while hovering
   tooltip?: string
 
   children?: ReactNode
@@ -23,6 +19,7 @@ interface Props {
   className?: string
   style?: CSSProperties
 
+  color?: ColorName
   type?: 'button' | 'submit',
 }
 
@@ -37,19 +34,24 @@ const UiButton = styled(StyleHelper.tag('button', ({
   type,
   onClick,
 })))`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  
-  padding: 0.25rem;
-  
+  padding: 0.2rem 0.5rem;
+  border-radius: 0.5rem;
+  border: none;
+  background: ${({ theme, color }) => theme.colors[color ?? 'primary'].value};
+  color: ${({ theme, color }) => theme.colors[color ?? 'primary'].contrast};
+
   ${({ isFull }) => isFull && css`
     width: 100%;
   `}
   
+  ${({ isDisabled }) => isDisabled && css`
+    background: rgb(200,200,200);
+    color: ${contrastDark};
+  `}
+
   :hover:not(&[disabled]) {
     cursor: pointer;
   }
 `
-export default UiButton
 
+export default UiButton
