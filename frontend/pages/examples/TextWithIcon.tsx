dcf7d54ce6fcbdc99c34711ac8280777dc7eddb8
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2021-11-30 15:04:14 +0100

    created component TextWithIcon

---
+++ frontend/pages/examples/TextWithIcon.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from 'react'
+import styled from 'styled-components'
+
+
+
+
+interface Props {
+  text: string
+  children: ReactNode
+}
+
+const TextWithIcon: React.VFC<Props> = ({ children,text  }) => {
+  return (
+    <StyledTitle>
+      <StyledIcon>
+        {children}
+      </StyledIcon>
+      <StyledText>
+        {text}
+      </StyledText>
+    </StyledTitle>
+  )
+}
+export default TextWithIcon
+
+const StyledTitle = styled.div`
+  display: inline-flex;
+  align-items: center;
+`
+
+const StyledIcon = styled.span`
+  margin-right: 2px;
+`
+const StyledText = styled.span`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.1px;
+`
+
