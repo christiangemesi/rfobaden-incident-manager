07e514b2f842a2afb11c907bc3bf756227111763
Lucifer Uchiha <rowla101@gmail.com>
2021-12-01 10:35:28 +0100

    Added example

--- frontend/pages/examples/ui-datelabel.tsx
+++ frontend/pages/examples/ui-datelabel.tsx
@@ -4,11 +4,18 @@ import UiDataLabel from '@/components/Ui/DataLabel/UiDateLabel'
 const UiDateLabelExample: React.VFC = () => {
   const past = new Date(2018, 11, 24)
   const now = new Date()
+  const future = new Date()
+  future.setFullYear(now.getFullYear() + 1)
   return (
     <React.Fragment>
       <h2>UiBadge</h2>
-      <UiDataLabel firstDate={past} secondDate={now} />
-      <UiDataLabel firstDate={now} />
+      <UiDataLabel start={past} end={now} />
+      <br />
+      <UiDataLabel start={now} end={future} type="datetime" />
+      <br />
+      <UiDataLabel start={now} />
+      <br />
+      <UiDataLabel start={past} />
     </React.Fragment>
   )
 }
