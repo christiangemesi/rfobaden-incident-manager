8b2345cbd3f6d893cc5c7fde73d65d594ee8ca75
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2021-12-01 17:32:16 +0100

    made changes to uilistcontainer (removed color and margin...)

--- frontend/components/Ui/List/Container/UiListContainer.tsx
+++ frontend/components/Ui/List/Container/UiListContainer.tsx
@@ -15,10 +15,6 @@ const UiListContainer: React.VFC<Props> = ({ children }) => {
 export default UiListContainer
 
 const Box = styled.div`
-  display:inline-flex;
-  padding: 8px;
-  background-color: #D9DAD9;
-  border-radius: 20px 20px 20px 20px;
+  display: flex;
   flex-direction: column;
-  margin-left: 10px;
 `
