d01a334bb7243b2eeb5f84a1e84d3ce2419855ab
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-12-01 15:24:25 +0100

    - expand example ui icon button page

--- frontend/pages/examples/ui-icon-button-group.tsx
+++ frontend/pages/examples/ui-icon-button-group.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import UiGrid from '@/components/Ui/Grid/UiGrid'
-import UiButton from '@/components/Ui/Button/UiButton'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
 import UiTitle from '@/components/Ui/Title/UiTitle'
+import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
+import UiIconButtonGroup from '@/components/Ui/Icon/Button/Group/UiIconButtonGroup'
 
 const UiIconButtonGroupExample: React.VFC = () => {
   return (
@@ -11,13 +12,25 @@ const UiIconButtonGroupExample: React.VFC = () => {
       <UiTitle level={3}>Icon-Button-Group</UiTitle>
       <UiGrid gap={0.5}>
         <UiGrid.Col>
-
+          <UiIconButtonGroup>
+            <UiIconButton>
+              <UiIcon.PrintAction />
+            </UiIconButton>
+            <UiIconButton>
+              <UiIcon.EditAction />
+            </UiIconButton>
+            <UiIconButton>
+              <UiIcon.DeleteAction />
+            </UiIconButton>
+          </UiIconButtonGroup>
         </UiGrid.Col>
       </UiGrid>
       <UiTitle level={3}>Icon-Button</UiTitle>
       <UiGrid gap={0.5}>
         <UiGrid.Col>
-
+          <UiIconButton>
+            <UiIcon.Organization />
+          </UiIconButton>
         </UiGrid.Col>
       </UiGrid>
     </UiContainer>
