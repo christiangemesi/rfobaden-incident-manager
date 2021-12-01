0d494167d329c2118ab9f842b82efa2c7f52605e
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-01 21:26:22 +0100

    Update hover cursor in checkbox

--- frontend/components/Ui/Button/UiCheckbox.tsx
+++ frontend/components/Ui/Button/UiCheckbox.tsx
@@ -1,61 +1,74 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import React from 'react'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
 import { contrastDark, defaultTheme } from '@/theme'
 import { UiInputProps } from '@/components/Ui/Input'
 import UiInputErrors from '@/components/Ui/Input/Errors/UiInputErrors'
 
-interface Props extends UiInputProps<boolean>{
+interface Props extends UiInputProps<boolean> {
   label: string
   isDisabled?: boolean
 }
 
 const UiCheckbox: React.VFC<Props> = ({
   label = '',
-  value ,
+  value,
   isDisabled = false,
   errors = [],
   onChange: handleChange,
 },
 ) => {
   return (
-    <StyledDiv>
-      <StyledCheckbox onClick={() => handleChange(!value)} disabled={isDisabled}>
-        {value && !isDisabled ? <UiIcon.CheckboxInactive /> : <UiIcon.CheckboxActive />}
-        <StyledCheckboxLabel>
+    <div>
+      <StyledDiv onClick={() => handleChange(!value)}>
+        <StyledCheckboxLabel isDisabled={isDisabled}>
+          <StyledCheckbox>
+            {value && !isDisabled ? <UiIcon.CheckboxInactive /> : <UiIcon.CheckboxActive />}
+          </StyledCheckbox>
           {label}
         </StyledCheckboxLabel>
-      </StyledCheckbox>
+      </StyledDiv>
       <UiInputErrors errors={errors} />
-    </StyledDiv>
+    </div>
   )
 }
 
 export default UiCheckbox
 
 const StyledDiv = styled.div`
-  display: inline-flex;
-  align-items: center;
-`
-
-const StyledCheckboxLabel = styled.label`
-  margin-left: 0.25rem;
-  color: ${contrastDark};
-`
-
-const StyledCheckbox = styled.button`
-  display: inherit;
-  align-items: inherit;
-  border: none;
-  background: none;
-  color: ${defaultTheme.colors.primary.value};
-  
-  :disabled {
-    color: rgb(200, 200, 200);
-    cursor: not-allowed;
-    & > label {
-      color: rgb(200, 200, 200);
+  *  {
+    :hover {
+      cursor: pointer;
     }
   }
 `
 
+const StyledCheckboxLabel = styled.label<{ isDisabled: boolean }>`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0.25rem;
+  color: ${contrastDark}
+  
+  ${({ isDisabled }) => isDisabled && css`
+    color: rgb(200, 200, 200);
+
+    & > div {
+      color: rgb(200, 200, 200);
+    }
+
+    :hover {
+      cursor: not-allowed;
+    }
+
+    & > label {
+      color: rgb(200, 200, 200);
+    }
+  `}
+`
+
+const StyledCheckbox = styled.div`
+  color: ${defaultTheme.colors.primary.value};
+  border: none;
+  background: none;
+`
