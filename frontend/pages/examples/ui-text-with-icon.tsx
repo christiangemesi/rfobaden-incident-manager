fd9641d02c67c2c94a806db76833902e56e6866e
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-12-01 14:08:18 +0100

    Remove space from filename of `UiTextWithIcon`

--- frontend/pages/examples/ui-text-with-icon.tsx
+++ frontend/pages/examples/ui-text-with-icon.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
-import UiTextWithIcon from '@/components/Ui/TextWithIcon/UiTextWithIcon '
+import UiTextWithIcon from '@/components/Ui/TextWithIcon/UiTextWithIcon'
 
 const UiTextWithInputExample: React.VFC = () => {
   return (
