fac46c0ec9992c34be0ee8c981ab58ff6aafc598
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-01 15:41:25 +0100

    Add optional icon-button to input text

--- frontend/components/Ui/Input/Text/UiTextInput.tsx
+++ frontend/components/Ui/Input/Text/UiTextInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useCallback } from 'react'
+import React, { ChangeEvent, ReactNode, useCallback } from 'react'
 import styled, { css } from 'styled-components'
 import UiInputErrors from '@/components/Ui/Input/Errors/UiInputErrors'
 import { UiInputProps } from '@/components/Ui/Input'
@@ -8,6 +8,8 @@ interface Props extends UiInputProps<string | null> {
   label?: string
   type?: 'text' | 'password'
   placeholder?: string
+  children?: ReactNode
+  onClick?: () => void
 }
 
 const UiTextInput: React.VFC<Props> = ({
@@ -17,6 +19,8 @@ const UiTextInput: React.VFC<Props> = ({
   label,
   type = 'text',
   errors = [],
+  children,
+  onClick: handleClick,
 }) => {
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     if (setValue) {
@@ -31,6 +35,7 @@ const UiTextInput: React.VFC<Props> = ({
 
   const Label = label == null ? 'div' : StyledLabel
   const hasError = errors.length !== 0
+  const hasChildren = children !== undefined
 
   return (
     <Label>
@@ -39,37 +44,56 @@ const UiTextInput: React.VFC<Props> = ({
           {label}
         </span>
       )}
-      <StyledInput value={value ?? ''} onChange={handleChange} type={type} placeholder={placeholder} hasError={hasError} />
+      <StyledDiv hasError={hasError}>
+        <StyledInput value={value ?? ''} onChange={handleChange} type={type} placeholder={placeholder} hasChildren={hasChildren} />
+        {hasChildren ?
+          <StyledAdditionalInput onClick={handleClick}>
+            {children}
+          </StyledAdditionalInput>: null}
+      </StyledDiv>
+
       <UiInputErrors errors={errors} />
     </Label>
   )
 }
 export default UiTextInput
 
-const StyledInput = styled.input<{hasError: boolean}>`
+const StyledInput = styled.input<{hasChildren: boolean}>`
   padding: 0.5rem;
   margin-top: 0.25rem;
   font-size: 0.9rem;
   border-radius: 0.5rem;
-  outline: none;
   border-style: solid;
   border-width: 1px;
-  border-color: ${contrastDark};
-  
-  ${({ hasError }) => !hasError && css`
-    :active, :focus {
-      border-color:  ${defaultTheme.colors.primary.value};
-    }
-  `}
-  
-  ${({ hasError }) => hasError && css`
-      border-color: ${defaultTheme.colors.error.value};
+  outline: none;
+  border-color: inherit;
+  width: 100%;
+
+
+  ${({ hasChildren }) => hasChildren && css`
+    border-bottom-right-radius: 0;
+    border-top-right-radius: 0;
+    border-right: none;
   `}
 
-  transition: 250ms ease;
-  transition-property: border-color;
 `
-
+const StyledAdditionalInput = styled.div`
+  background: ${defaultTheme.colors.primary.value};
+  border-color: inherit;
+  margin-top: 0.25rem;
+  outline: none;  
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 0  0.5rem 0.5rem 0;
+  width: 40px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  
+  * {
+    color: ${defaultTheme.colors.primary.contrast};
+  }
+`
 const StyledLabel = styled.label`
   display: flex;
   flex-direction: column;
@@ -81,4 +105,21 @@ const StyledLabel = styled.label`
     font-size: 0.9rem;
     font-weight: bold;
   }
+`
+
+const StyledDiv = styled.div<{hasError: boolean}>`
+  display: flex;
+
+  ${({ hasError }) => !hasError && css`
+    :active, :focus {
+      border-color:  ${defaultTheme.colors.primary.value};
+    }
+  `}
+
+  ${({ hasError }) => hasError && css`
+      border-color: ${defaultTheme.colors.error.value};
+  `}
+  
+  transition: 250ms ease;
+  transition-property: border-color;
 `
--- frontend/pages/examples/ui-text-input.tsx
+++ frontend/pages/examples/ui-text-input.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import UiTextInput from '@/components/Ui/Input/Text/UiTextInput'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
 
 const UiTextInputExample: React.VFC = () => {
 
   const [value, setValue] = useState<string | null>()
-
   const handleChange = (input: string | null) => {
     setValue(input)
+  }
+
+  const handleClick = () => {
+    console.log('Icon clicked')
   }
 
   return (
@@ -16,6 +20,10 @@ const UiTextInputExample: React.VFC = () => {
       <UiTextInput label="Password Input" type="password" value="top secret" />
       <br />
       <UiTextInput label="Text Input Errors" value="top secret" errors={['Not valid', 'Fail !!!']} />
+      <br />
+      <UiTextInput label="Text Input with children" onClick={handleClick}>
+        <UiIcon.Organization />
+      </UiTextInput>
     </div>
   )
 }
