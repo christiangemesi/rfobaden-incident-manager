28b92ec93ad7a5ac669518c2fa174172564a4912
Ramona Marti <ramona.marti@students.fhnw.ch>
2021-12-02 17:51:54 +0100

    -  merge with task

---
+++ frontend/models/Task.ts
@@ -0,0 +1,43 @@
+import Model from '@/models/base/Model'
+import User from '@/models/User'
+import Id from '@/models/base/Id'
+import Report from '@/models/Report'
+import Incident from '@/models/Incident'
+import { parseDate } from '@/models/Date'
+
+export default interface Task extends Model {
+  title: string
+  description: string | null
+  priority: string
+  location: string | null
+
+  assigneeId: Id<User> | null
+
+  createdAt: Date
+  updatedAt: Date
+  closedAt: Date | null
+
+  startsAt: Date | null
+  endsAt: Date | null
+
+  reportId: Id<Report>
+  incidentId: Id<Incident>
+}
+
+export const parseTask = (data: Task): Task => ({
+  ...data,
+  createdAt: parseDate(data.createdAt),
+  updatedAt: parseDate(data.updatedAt),
+  startsAt: parseDateOrNull(data.startsAt),
+  endsAt: parseDateOrNull(data.endsAt),
+})
+
+const parseDateOrNull = (date: Date | null): Date | null => {
+  return date === null ? null : parseDate(date)
+}
+
+export enum TaskPriority {
+  LOW = 'LOW',
+  MEDIUM = 'MEDIUM',
+  HIGH = 'HIGH',
+}
