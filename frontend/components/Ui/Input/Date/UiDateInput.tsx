4ea3cfd845c65229ae9d52d32e530d75467edd20
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-03 11:07:09 +0100

    Update UiDateInput with parsing

--- frontend/components/Ui/Input/Date/UiDateInput.tsx
+++ frontend/components/Ui/Input/Date/UiDateInput.tsx
@@ -1,28 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import UiTextInput from '@/components/Ui/Input/Text/UiTextInput'
-import { parseDate } from '@/models/Date'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
+import { UiInputProps } from '@/components/Ui/Input'
 
 
-const UiDateInput: React.VFC = () => {
+const UiDateInput: React.VFC<UiInputProps<Date | null>> = ({
+  onChange: handleChange,
+  errors = [],
+}) => {
 
-  const [value, setValue] = useState<string | null>(null)
-  console.log('Date: ')
-  console.log(parseDate([2002, 2, 2, 14, 14, 14, 14]))
+  const [text, setText] = useState<string|null>(null)
+  const [isInvalid, setInvalid] = useState<boolean>(false)
 
+  useEffect(() => {
+    if(text === null) {
+      handleChange(null)
+      setInvalid(false)
+      return
+    }
+    const [date, time] = text.split(' ', 2).map((n) => n.trim())
+    const [hour, min] = time === undefined ? [0, 0] : time.split(':', 2).map((n) => parseInt(n.trim()))
+    const [day, month, year] = date.split('.', 3).map((n) => parseInt(n.trim()))
+    const newDate = new Date(Date.UTC(year, month - 1, day, hour - 1, min ))
 
-  const stringToDate = (input: string) => {
-    const dateItems = input.split('.')
-    return parseDate([parseInt(dateItems[2]), parseInt(dateItems[1]), parseInt(dateItems[0]), 0, 0, 0, 0])
-
-  }
-
+    if (isNaN(newDate.getTime())) {
+      handleChange(null)
+      setInvalid(true)
+      return
+    }
+    handleChange(newDate)
+    setInvalid(false)
+  }, [text])
 
   return(
     <div>
-      <UiTextInput value={value} onChange={setValue} placeholder="dd.mm.yyyy" />
-      <label>{value}</label>
-      <span>
-      </span>
+      <UiTextInput value={text} onChange={setText} placeholder="dd.mm.yyyy" errors={isInvalid ? ['ist invalid', ...errors] : errors}>
+        <UiIcon.Organization />
+      </UiTextInput>
     </div>
 
   )
--- frontend/pages/examples/ui-date-input.tsx
+++ frontend/pages/examples/ui-date-input.tsx
@@ -1,18 +1,15 @@
 import UiDateInput from '@/components/Ui/Input/Date/UiDateInput'
 import { useState } from 'react'
 
-
 const UiDateInputExample: React.VFC = () => {
-
-
+  const [value, setValue] = useState<Date | null>(null)
 
   return(
     <div>
-      Test
-      <UiDateInput />
+      <UiDateInput value={value} onChange={setValue}  />
+      {value?.toLocaleString()}
     </div>
   )
 }
-
 
 export default UiDateInputExample
