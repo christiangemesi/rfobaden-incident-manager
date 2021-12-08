911d648e51264fcce2cd2e667a0ad2916f3504cd
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-08 16:56:50 +0100

    Add ItemSeparator to ReportForm

--- frontend/components/Report/Form/ReportForm.tsx
+++ frontend/components/Report/Form/ReportForm.tsx
@@ -18,6 +18,7 @@ import UiToggle from '@/components/Ui/Toggle/UiToggle'
 import UiPrioritySlider from '@/components/Ui/PrioritySlider/UiPrioritySlider'
 import Priority from '@/models/Priority'
 import UiDateInput from '@/components/Ui/Input/Date/UiDateInput'
+import styled from 'styled-components'
 
 interface Props {
   incident: Incident
@@ -88,7 +89,7 @@ const ReportForm: React.VFC<Props> = ({ incident, report = null, onClose: handle
   return (
     <div>
       <form>
-        <UiGrid gap={0.5}>
+        <ItemSeparator>
           <UiGrid align="center">
             <UiGrid.Col textAlign="left">
               <UiForm.Field field={form.isKeyReport}>{(props) => (
@@ -108,51 +109,51 @@ const ReportForm: React.VFC<Props> = ({ incident, report = null, onClose: handle
               )}</UiForm.Field>
             </UiGrid.Col>
           </UiGrid>
+        </ItemSeparator>
+        <ItemSeparator>
+          <UiForm.Field field={form.title}>{(props) => (
+            <UiTextInput {...props} label="Titel" placeholder="Titel" />
+          )}</UiForm.Field>
+        </ItemSeparator>
 
-          <UiGrid.Col size={12}>
-            <UiForm.Field field={form.title}>{(props) => (
-              <UiTextInput {...props} label="Titel" placeholder="Titel" />
-            )}</UiForm.Field>
-          </UiGrid.Col>
+        <ItemSeparator>
+          <UiForm.Field field={form.description}>{(props) => (
+            <UiTextArea {...props} label="Beschreibung" placeholder="Beschreibung" />
+          )}</UiForm.Field>
+        </ItemSeparator>
 
-          <UiGrid.Col size={12}>
-            <UiForm.Field field={form.description}>{(props) => (
-              <UiTextArea {...props} label="Beschreibung" placeholder="Beschreibung" />
-            )}</UiForm.Field>
-          </UiGrid.Col>
+        <ItemSeparator>
+          <UiForm.Field field={form.notes}>{(props) => (
+            <UiTextArea {...props} label="Notiz" placeholder="Notiz" />
+          )}</UiForm.Field>
+        </ItemSeparator>
 
-          <UiGrid.Col size={12}>
-            <UiForm.Field field={form.notes}>{(props) => (
-              <UiTextArea {...props} label="Notiz" placeholder="Notiz" />
-            )}</UiForm.Field>
-          </UiGrid.Col>
+        <ItemSeparator>
+          <UiForm.Field field={form.location}>{(props) => (
+            <UiTextInput {...props} label="Ort / Gebiet" placeholder="Ort / Gebiet" />
+          )}</UiForm.Field>
+        </ItemSeparator>
 
-          <UiGrid.Col size={12}>
-            <UiForm.Field field={form.location}>{(props) => (
-              <UiTextInput {...props} label="Ort / Gebiet" placeholder="Ort / Gebiet" />
-            )}</UiForm.Field>
-          </UiGrid.Col>
+        <ItemSeparator>
+          <UiForm.Field field={form.assigneeId}>{(props) => (
+            <UiSelectInput {...props} label="Zuweisung" options={userIds} optionName={mapUserIdToName} />
+          )}</UiForm.Field>
+        </ItemSeparator>
 
-          <UiGrid.Col size={12}>
-            <UiForm.Field field={form.assigneeId}>{(props) => (
-              <UiSelectInput {...props} label="Zuweisung" options={userIds} optionName={mapUserIdToName} />
-            )}</UiForm.Field>
-          </UiGrid.Col>
+        <ItemSeparator>
+          <UiForm.Field field={form.startsAt}>{(props) => (
+            <UiDateInput {...props} label="Beginn" placeholder="dd.mm.yyyy hh:mm" />
+          )}</UiForm.Field>
+        </ItemSeparator>
 
-          <UiGrid.Col>
-            <UiForm.Field field={form.startsAt}>{(props) => (
-              <UiDateInput {...props} label="Beginn" placeholder="dd.mm.yyyy hh:mm" />
-            )}</UiForm.Field>
-          </UiGrid.Col>
-
-          <UiGrid.Col style={{ marginBottom: '2rem' }}>
-            <UiForm.Field field={form.endsAt}>{(props) => (
-              <UiDateInput {...props} label="Ende" placeholder="dd.mm.yyyy hh:mm" />
-            )}</UiForm.Field>
-          </UiGrid.Col>
-
-        </UiGrid>
-        <UiForm.Buttons form={form} />
+        <ItemSeparator>
+          <UiForm.Field field={form.endsAt}>{(props) => (
+            <UiDateInput {...props} label="Ende" placeholder="dd.mm.yyyy hh:mm" />
+          )}</UiForm.Field>
+        </ItemSeparator>
+        <ItemSeparator>
+          <UiForm.Buttons form={form} />
+        </ItemSeparator>
       </form>
     </div>
   )
@@ -165,3 +166,8 @@ const mapUserIdToName = (id: Id<User>): string | null => {
     ? null
     : `${user.firstName} ${user.lastName}`
 }
+
+const ItemSeparator = styled.div`
+  display: block;
+  margin: 1rem;
+`
