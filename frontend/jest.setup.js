4fa1fe2059e46781f7935924f33cb9c0beeae7ca
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-12-09 15:35:27 +0100

    Add `enzyme` and `eslint-jest-plugin` frontend dependencies

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom/extend-expect'
+
+import Enzyme from 'enzyme'
+
+// This is an unofficial adapter, since there's not yet an offical enzyme adapter for React 17 (2021.12.09).
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+Enzyme.configure({ adapter: new Adapter() })
