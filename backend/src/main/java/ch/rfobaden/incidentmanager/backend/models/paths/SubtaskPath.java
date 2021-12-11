57517218078e832cdee1b15169064c8cb8432512
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-11 13:54:31 +0100

    Update equals, hashCode and toString of SubtaskPath

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/paths/SubtaskPath.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/paths/SubtaskPath.java
@@ -4,25 +4,7 @@ import java.util.Objects;
 
 public class SubtaskPath extends TaskPath {
 
-    private Long incidentId;
-    private Long reportId;
     private Long taskId;
-
-    public Long getIncidentId() {
-        return incidentId;
-    }
-
-    public void setIncidentId(Long incidentId) {
-        this.incidentId = incidentId;
-    }
-
-    public Long getReportId() {
-        return reportId;
-    }
-
-    public void setReportId(Long reportId) {
-        this.reportId = reportId;
-    }
 
     public Long getTaskId() {
         return taskId;
@@ -40,23 +22,24 @@ public class SubtaskPath extends TaskPath {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        SubtaskPath subtaskPath = (SubtaskPath) o;
-        return Objects.equals(incidentId, subtaskPath.incidentId)
-            && Objects.equals(reportId, subtaskPath.reportId)
-            && Objects.equals(taskId, subtaskPath.taskId);
+        if (!super.equals(o)) {
+            return false;
+        }
+        SubtaskPath that = (SubtaskPath) o;
+        return Objects.equals(taskId, that.taskId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(incidentId, reportId, taskId);
+        return Objects.hash(super.hashCode(), taskId);
     }
 
     @Override
     public String toString() {
-        return "TaskPath{"
-            + "incidentId=" + incidentId
-            + ", reportId=" + reportId
-            + ", taskId=" + taskId
+        return "SubtaskPath{"
+            // TODO: check with or without super call?
+            + super.toString()
+            + "taskId=" + taskId
             + '}';
     }
 }
