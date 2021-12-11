13320c0ac2a0b3161ea32574dd1c781f0743ba81
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-11 15:16:23 +0100

    Add mockLoadRelation to SubtaskServiceTest

--- backend/src/test/java/ch/rfobaden/incidentmanager/backend/services/SubtaskServiceTest.java
+++ backend/src/test/java/ch/rfobaden/incidentmanager/backend/services/SubtaskServiceTest.java
@@ -7,9 +7,25 @@ import ch.rfobaden.incidentmanager.backend.models.paths.SubtaskPath;
 import ch.rfobaden.incidentmanager.backend.repos.ReportRepository;
 import ch.rfobaden.incidentmanager.backend.repos.SubtaskRepository;
 import ch.rfobaden.incidentmanager.backend.services.base.ModelRepositoryServiceTest;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class SubtaskServiceTest extends
     ModelRepositoryServiceTest<Subtask, SubtaskPath, SubtaskService, SubtaskRepository> {
+
+    @MockBean
+    UserService userService;
+
+    @Override
+    protected void mockLoadRelations(Subtask subtask) {
+        var assignee = subtask.getAssignee();
+        if (assignee != null) {
+            Mockito.when(userService.find(assignee.getId()))
+                .thenReturn(Optional.of(assignee));
+        }
+    }
 }
