392db091c7619a5aa2ec0bcd9298b970d716e4bd
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-11 12:00:22 +0100

    Add SubtaskController to controllers

---
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/SubtaskController.java
@@ -0,0 +1,37 @@
+package ch.rfobaden.incidentmanager.backend.controllers;
+
+import ch.rfobaden.incidentmanager.backend.controllers.base.ModelController;
+import ch.rfobaden.incidentmanager.backend.errors.ApiException;
+import ch.rfobaden.incidentmanager.backend.models.Subtask;
+import ch.rfobaden.incidentmanager.backend.models.paths.SubtaskPath;
+import ch.rfobaden.incidentmanager.backend.services.SubtaskService;
+import ch.rfobaden.incidentmanager.backend.services.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/incidents/{incidentId}/reports/{reportId}/tasks/{taskId}/subtasks")
+public class SubtaskController extends ModelController<Subtask, SubtaskPath, SubtaskService> {
+
+    private final TaskService taskService;
+
+    public SubtaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @Override
+    public Subtask create(@ModelAttribute SubtaskPath path, @RequestBody Subtask subtask) {
+        return super.create(path, subtask);
+    }
+
+    @Override
+    protected void loadPath(SubtaskPath path, Subtask subtask) {
+        var task = taskService.find(path.getTaskId()).orElseThrow(() -> (
+            new ApiException(HttpStatus.NOT_FOUND, "report not found")
+        ));
+        subtask.setTask(task);
+    }
+}
