e8040096c89c0882a17c4a7df905d7a07ff41c21
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-17 20:13:42 +0100

    Update handleClick in SubtaskListItem

--- frontend/components/Subtask/List/Item/SubtaskListItem.tsx
+++ frontend/components/Subtask/List/Item/SubtaskListItem.tsx
@@ -4,14 +4,18 @@ import { useUser } from '@/stores/UserStore'
 import UiListItemWithDetails from '@/components/Ui/List/Item/WithDetails/UiListItemWithDetails'
 import Priority from '@/models/Priority'
 import UiCheckbox from '@/components/Ui/Checkbox/UiCheckbox'
+import styled, { css } from 'styled-components'
 
-interface TaskListItemProps {
+interface Props {
   subtask: Subtask,
   isActive: boolean,
   onClick?: (Subtask: Subtask) => void,
 }
 
-const SubtaskListItem: React.VFC<TaskListItemProps> = ({ subtask, onClick: _handleClick }) => {
+const SubtaskListItem: React.VFC<Props> = ({
+  subtask,
+  isActive,
+  onClick: handleClick }) => {
 
   const assignee = useUser(subtask.assigneeId)
   const assigneeName = assignee?.firstName + ' ' + assignee?.lastName ?? ''
@@ -19,10 +23,22 @@ const SubtaskListItem: React.VFC<TaskListItemProps> = ({ subtask, onClick: _hand
   const [value, setValue] = useState(false)
 
   return (
-    <UiListItemWithDetails priority={Priority.LOW} title={subtask.title} user={assigneeName}>
+    <SelectableListItem
+      isActive={isActive}
+      priority={Priority.LOW}
+      title={subtask.title}
+      user={assigneeName}
+      onClick={handleClick && ( () => handleClick(subtask))}>
       <UiCheckbox label="" value={value} onChange={setValue} color="tertiary" />
-    </UiListItemWithDetails>
+    </SelectableListItem>
   )
 }
 
 export default SubtaskListItem
+
+const SelectableListItem = styled(UiListItemWithDetails)<{ isActive: boolean }>`
+  ${({ isActive, theme }) => isActive && css`
+    background: ${theme.colors.secondary.contrast};
+    color: ${theme.colors.secondary.value};
+  `}
+`
