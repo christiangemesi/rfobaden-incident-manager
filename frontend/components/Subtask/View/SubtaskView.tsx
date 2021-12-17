757baa23ec48b030c98a1103b5abe6af339dece7
Andri Wild <andri.wild@students.fhnw.ch>
2021-12-17 20:14:49 +0100

    Add SubtaskView to Subtask

--- frontend/components/Subtask/View/SubtaskView.tsx
+++ frontend/components/Subtask/View/SubtaskView.tsx
@@ -1,17 +1,110 @@
 import Subtask from '@/models/Subtask'
 import React from 'react'
+import { useUser } from '@/stores/UserStore'
+import styled from 'styled-components'
+import UiGrid from '@/components/Ui/Grid/UiGrid'
+import UiTitle from '@/components/Ui/Title/UiTitle'
+import UiDateLabel from '@/components/Ui/DateLabel/UiDateLabel'
+import UiIconButtonGroup from '@/components/Ui/Icon/Button/Group/UiIconButtonGroup'
+import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
+import UiModal from '@/components/Ui/Modal/UiModal'
+import UiTextWithIcon from '@/components/Ui/TextWithIcon/UiTextWithIcon'
+import SubtaskForm from '@/components/Subtask/Form/SubtaskForm'
+import Task from '@/models/Task'
+import Report from '@/models/Report'
+import Incident from '@/models/Incident'
+import { contrastDark } from '@/theme'
+import BackendService from '@/services/BackendService'
+import ReportStore from '@/stores/ReportStore'
+
 
 interface Props {
+  incident: Incident
+  report: Report
+  task: Task
   subtask: Subtask
 }
 
-const SubtaskView: React.VFC<Props> = ({ subtask }) => {
+const SubtaskView: React.VFC<Props> = ({ subtask, task, report, incident }) => {
+  const assignee = useUser(subtask.assigneeId)
+  const assigneeName = assignee?.firstName + ' ' + assignee?.lastName ?? ''
 
+  const startDate = subtask.startsAt !== null ? subtask.startsAt : subtask.createdAt
+
+  const handleDelete = async () => {
+    if (confirm(`Sind sie sicher, dass sie die Meldung "${subtask.title}" schliessen wollen?`)) {
+      await BackendService.delete(`incidents/${incident.id}/reports/${report.id}/tasks/${task.id}/subtasks`, subtask.id)
+      ReportStore.remove(subtask.id)
+    }
+  }
 
   return (
-    <div>Subtask View</div>
+    <Details>
+      <Line />
+      <TitleIconContainer>
+        <UiTitle level={2}>
+          {subtask.title}
+        </UiTitle>
+        <UiIconButtonGroup>
+          <UiIconButton onClick={() => alert('not yet implemented')}>
+            <UiIcon.PrintAction />
+          </UiIconButton>
 
+          <UiModal isFull>
+            <UiModal.Activator>{({ open }) => (
+              <UiIconButton onClick={open}>
+                <UiIcon.EditAction />
+              </UiIconButton>
+            )}</UiModal.Activator>
+            <UiModal.Body>{({ close }) => (
+              <React.Fragment>
+                <UiTitle level={1} isCentered>
+                  Teilauftrag bearbeiten
+                </UiTitle>
+                <SubtaskForm incident={incident} report={report} task={task} subtask={subtask} onClose={close} />
+              </React.Fragment>
+            )}</UiModal.Body>
+          </UiModal>
+
+          <UiIconButton onClick={handleDelete}>
+            <UiIcon.DeleteAction />
+          </UiIconButton>
+
+        </UiIconButtonGroup>
+
+      </TitleIconContainer>
+
+      <UiDateLabel start={startDate} end={subtask.endsAt} />
+
+
+      {assignee && (
+        <UiTextWithIcon text={assigneeName}>
+          <UiIcon.UserInCircle />
+        </UiTextWithIcon>
+      )}
+      {subtask.description}
+
+    </Details>
   )
 }
 
 export default SubtaskView
+
+const TitleIconContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Line = styled.div`
+  height: 1px;
+  background: ${contrastDark};
+  margin-bottom: 2rem;
+`
+
+const Details = styled.div`
+  display: flex;  
+  flex-direction: column;
+  gap: 1rem;
+`
