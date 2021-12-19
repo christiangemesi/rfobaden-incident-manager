8d6a64748ff21d0d53b22ea878466777a93197e4
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2021-12-19 10:07:13 +0100

    Make `User.organization` optional

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/User.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/User.java
@@ -25,7 +25,7 @@ import javax.validation.constraints.NotBlank;
 public final class User extends Model.Basic implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     @JoinColumn
     private Organization organization;
 
