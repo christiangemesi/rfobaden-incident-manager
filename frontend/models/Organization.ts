78dd6285882a2bebb6820ca7f88e7e9695a6006f
Andri Wild <andri.wild@students.fhnw.ch>
2022-01-02 15:41:19 +0100

    Update type of userId in organization model

--- frontend/models/Organization.ts
+++ frontend/models/Organization.ts
@@ -1,10 +1,12 @@
 import Model from '@/models/base/Model'
 import { parseDate } from '@/models/Date'
+import User from '@/models/User'
+import Id from '@/models/base/Id'
 
 export default interface Organization extends Model{
   name: string
   email: string
-  userIds: number[]
+  userIds: Id<User>[]
 }
 
 export const parseOrganization = (data: Organization): Organization => ({
