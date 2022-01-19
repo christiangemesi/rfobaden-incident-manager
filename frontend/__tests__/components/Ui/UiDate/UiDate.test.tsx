b49be74599d887cdb53846ed6e3d76b0d9bb9f7f
Lucifer Uchiha <rowla101@gmail.com>
2022-01-19 10:06:21 +0100

    Improved UiDate.test.tsx

--- frontend/__tests__/components/Ui/UiDate/UiDate.test.tsx
+++ frontend/__tests__/components/Ui/UiDate/UiDate.test.tsx
@@ -10,6 +10,22 @@ const singleDigitDate = new Date(2012, 7, 8, 1, 2)
 const normalDate = new Date(2013, 10, 20, 20, 30)
 
 describe('UiDate', () => {
+  describe('with default type (datetime)', () => {
+    it('should correctly display a date and time with leading zeros', () => {
+      const html = shallow(<UiDate value={singleDigitDate} />)
+      expect(html.html()).toBe('08.08.2012 01:02')
+    })
+
+    it('should correctly display a date and time', () => {
+      const html = shallow(<UiDate value={normalDate} />)
+      expect(html.html()).toBe('20.11.2013 20:30')
+    })
+  })
+  describe('with incorrect type', () => {
+    it('should throw an error', () => {
+      expect(()=>shallow(<UiDate value={singleDigitDate} type="Banana" />)).toThrow('Invalid type passed to UiDate')
+    })
+  })
   describe('with type date', () => {
     it('should correctly display a date with leading zeros', () => {
       const html = shallow(<UiDate value={singleDigitDate} type="date" />)
