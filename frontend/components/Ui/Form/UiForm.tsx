accebc2aed0f1231a031387c64226474235929e7
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-02-14 09:01:56 +0100

    Add `UiForm` component to capture submit events

--- frontend/components/Ui/Form/UiForm.tsx
+++ frontend/components/Ui/Form/UiForm.tsx
@@ -1,8 +1,37 @@
 import UiFormButtons from '@/components/Ui/Form/Buttons/UiFormButtons'
 import UiFormField from '@/components/Ui/Form/Field/UiFormField'
+import { getFormBaseState, UiFormState } from '@/components/Ui/Form/index'
+import { FormEvent, ReactNode, useCallback } from 'react'
 
-const UiForm = {
+interface Props<T> {
+  form: UiFormState<T>
+  children: ReactNode
+}
+
+const UiForm = <T,>({ form, children }: Props<T>) => {
+  const baseForm = getFormBaseState(form)
+  const {
+    isValid,
+    value,
+  } = baseForm
+
+  const pushSubmit = baseForm.onSubmit
+
+  const handleSubmit = useCallback(async (e: FormEvent) => {
+    e.preventDefault();
+    if (isValid && pushSubmit !== null) {
+      await pushSubmit(value)
+    }
+  }, [isValid, value, pushSubmit])
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {children}
+    </form>
+  )
+}
+
+export default Object.assign(UiForm, {
   Field: UiFormField,
   Buttons: UiFormButtons,
-}
-export default UiForm
+})
