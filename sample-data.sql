6ce7b1aae5b6b487cde2e62ff44a2d8ae5d1e497
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-02-14 09:23:58 +0100

    Update `sample-data.sql` to ignore entries that exist already

--- sample-data.sql
+++ sample-data.sql
@@ -1,5 +1,5 @@
 -- Mail values are only placeholders.
-INSERT INTO organization
+INSERT IGNORE INTO organization
 VALUES (1, _utf8'1970-01-01 00:00:00.001000', _utf8'2021-12-30 11:53:51', _utf8'zso@rfobaden.ch.ch', _utf8'ZSO Baden'),
        (2, _utf8'2021-12-30 11:54:26', _utf8'2021-12-30 11:54:28', _utf8'rfo@rfobaden.chbaden.ch', _utf8'RFO Baden'),
        (3, _utf8'2021-12-30 11:54:26', _utf8'2021-12-30 11:54:28', _utf8'stapo@rfobaden.ch', _utf8'StaPo Baden'),
@@ -27,28 +27,28 @@ VALUES (1, _utf8'1970-01-01 00:00:00.001000', _utf8'2021-12-30 11:53:51', _utf8'
        (25, _utf8'2021-12-30 11:54:26', _utf8'2021-12-30 11:54:28', _utf8'wd-turgi@rfobaden.ch', _utf8'Werkdienst Turgi'),
        (26, _utf8'2021-12-30 11:54:26', _utf8'2021-12-30 11:54:28', _utf8'wd-wuerenlingen@rfobaden.ch', _utf8'Werkdienst Würenlingen');
 
-INSERT INTO user
+INSERT IGNORE INTO user
 VALUES (1, _utf8'2021-12-02 14:51:47.581611', _utf8'2021-12-02 14:51:47.581611', _utf8'g@g.g', _utf8'Admin', _utf8'A', _utf8'ADMIN',1),
        (3, _utf8'2021-12-02 14:53:33.676581', _utf8'2021-12-02 14:53:33.676581', _utf8'a@a.a', _utf8'Agent', _utf8'C', _utf8'AGENT',1),
        (5, _utf8'2021-12-02 14:54:31.758215', _utf8'2021-12-02 14:54:31.758215', _utf8'a@b.c', _utf8'Ad', _utf8'Ad', _utf8'ADMIN',2),
        (7, _utf8'2021-12-02 14:55:59.500941', _utf8'2021-12-02 14:55:59.500941', _utf8'a@b.b', _utf8'cd', _utf8'cd', _utf8'AGENT',2);
 
-INSERT INTO user_credentials
+INSERT IGNORE INTO user_credentials
 VALUES (2, _utf8'2021-12-02 14:51:47.579464', _utf8'2021-12-02 14:52:31.555873', _utf8'$2a$10$jOSBFmH2aEupssLgdr.1POH.OG9FIks2HZs0ymnU8x5.QnMw2imQq', _utf8'2021-12-02 14:52:31.555873',1),
        (4, _utf8'2021-12-02 14:53:33.676537', _utf8'2021-12-02 14:54:03.574307', _utf8'$2a$10$OctDKD5QYsdrLMU8vkoqIOerDZtXFDJr.9MzTEZiFHJVdxD5az7lu', _utf8'2021-12-02 14:54:03.574307',3),
        (6, _utf8'2021-12-02 14:54:31.758092', _utf8'2021-12-02 14:55:09.385737', _utf8'$2a$10$wvCLMMHAckRla3HIIk.1W.Ru3wlU//CXEfJW.IA8Hd1icvqHrbvmy', _utf8'2021-12-02 14:55:09.385737',5),
        (8, _utf8'2021-12-02 14:55:59.500889', _utf8'2021-12-02 14:56:30.697138', _utf8'$2a$10$A2xYMyLTiZYsKAWLD02LzugnnOQ0K2hsKkRXIICnC9ToTfUzDgEka', _utf8'2021-12-02 14:56:30.697138',7);
 
-INSERT INTO close_reason
+INSERT IGNORE INTO close_reason
 VALUES (1, _utf8'2021-12-02 15:06:18.886164', _utf8'Fertig',NULL),
        (2, _utf8'2021-12-02 15:06:23.404000', _utf8'Fertig',NULL),
        (3, _utf8'2021-12-02 15:07:11.104795', _utf8'Fertig',NULL);
 
-INSERT INTO incident
+INSERT IGNORE INTO incident
 VALUES (1, _utf8'2021-12-02 14:57:40.302645', _utf8'2021-12-02 15:30:37.272910', _utf8'Wassermassen in Turgi haben Insel zwischen Limmatkanal und Limmat teilweise abgetragen (bei Limmatkraftwerk)', _utf8'2021-12-22 15:30:40.332617',FALSE, _utf8'2021-11-02 15:30:40.332617', _utf8'Hochwasser Limmat',NULL),
        (2, _utf8'2021-12-02 14:58:08.700227', _utf8'2021-12-02 15:30:37.272910', _utf8'Dachstock',NULL,TRUE,NULL, _utf8'Brand Turgi',3);
 
-INSERT INTO report
+INSERT IGNORE INTO report
 VALUES (1, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:06:18.886201', _utf8'Baumstämme und Schutt bedrohen die alte Holzbrücke am Limmatweg.', _utf8'2021-12-22 15:00:01.257671',TRUE,TRUE, _utf8'Turgi', _utf8'ACHTUNG: starke Strömung und morsches Holz',2, _utf8'2021-12-02 15:00:01.257671', _utf8'Bedrohung alte Holzbrücke 2',1,1),
        (2, _utf8'2021-12-02 15:06:39.928700', _utf8'2021-12-02 15:06:39.928737', _utf8'Baumstämme und Schutt bedrohen die alte Holzbrücke am Limmatweg.', _utf8'2021-12-12 15:00:01.257671',TRUE,FALSE, _utf8'Turgi', _utf8'ACHTUNG: starke Strömung und morsches Holz',1,NULL, _utf8'Bedrohung alte Holzbrücke',3,1),
        (3, _utf8'2021-12-02 15:06:46.951669', _utf8'2021-12-02 15:07:11.104841', _utf8'Baumstämme und Schutt bedrohen die alte Holzbrücke am Limmatweg.',NULL,FALSE,TRUE, _utf8'Turgi', _utf8'ACHTUNG: starke Strömung und morsches Holz',0, _utf8'2021-11-02 15:00:01.257671', _utf8'Bedrohung alte Holzbrücke',3,1),
@@ -57,7 +57,7 @@ VALUES (1, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:06:18.886201',
        (6, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257717', _utf8'Baumstämme und Schutt bedrohen die alte Holzbrücke am Limmatweg.',NULL,TRUE,FALSE, _utf8'Turgi', _utf8'ACHTUNG: starke Strömung und morsches Holz',2,NULL, _utf8'Bedrohung alte Holzbrücke',1,1),
        (7, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257717', _utf8'Baumstämme und Schutt bedrohen die alte Holzbrücke am Limmatweg.',NULL,FALSE,FALSE, _utf8'Turgi', _utf8'ACHTUNG: starke Strömung und morsches Holz',2,NULL, _utf8'Bedrohung alte Holzbrücke',1,1);
 
-INSERT INTO task
+INSERT IGNORE INTO task
 VALUES (1, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test 1',NULL, _utf8'Turgi',0,NULL, _utf8'Umleitung aufstellen',1,1),
        (2, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test',NULL, _utf8'Turgi',1,NULL, _utf8'Brücke sperren',1,1),
        (3, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test',NULL, _utf8'Visp',2,NULL, _utf8'Brücke sperren',1,1),
@@ -66,7 +66,7 @@ VALUES (1, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671',
        (6, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test',NULL, _utf8'Herzogenbuchsee',0,NULL, _utf8'Brücke sperren',1,1),
        (7, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test',NULL, _utf8'Turgi',0,NULL, _utf8'Brücke sperren',1,1);
 
-INSERT INTO subtask
+INSERT IGNORE INTO subtask
 VALUES (1, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test',NULL,FALSE,0,NULL, _utf8'Umleitung aufstellen',1,1),
        (2, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test',NULL,FALSE,1,NULL, _utf8'Umleitung erstellen',3,2),
        (3, _utf8'2021-12-02 15:00:01.257671', _utf8'2021-12-02 15:00:01.257671', _utf8'Test',NULL,FALSE,2,NULL, _utf8'Brücke sperren',3,1),
