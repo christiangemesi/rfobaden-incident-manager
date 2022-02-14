622bfdf5daf1f60262dc768f2fde0479578c750b
Andri Wild <andri.wild@students.fhnw.ch>
2022-02-14 17:32:06 +0100

    Update changelog

--- frontend/pages/changelog.tsx
+++ frontend/pages/changelog.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import fileContent from '../CHANGELOG.md'
 import UiContainer from '@/components/Ui/Container/UiContainer'
+import fileContent from '../CHANGELOG.md'
 
 import 'github-markdown-css/github-markdown-light.css'
 
