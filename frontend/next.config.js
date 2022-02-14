68a629c3a38d976bf8b22ff6100d25e2c028f8d2
Andri Wild <andri.wild@students.fhnw.ch>
2022-02-14 14:21:27 +0100

    Update changelog page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -18,26 +18,11 @@ const config = {
   },
 
   webpack: (config) => {
-    return Object.assign({}, config, {
-      externals: Object.assign({}, config.externals, {
-        fs: 'fs',
-      }),
-      module: Object.assign({}, config.module, {
-        rules: config.module.rules.concat([
-          {
-            test: /\.md$/,
-            loader: 'emit-file-loader',
-            options: {
-              name: 'dist/[path][name].[ext]',
-            },
-          },
-          {
-            test: /\.md$/,
-            loader: 'raw-loader',
-          },
-        ]),
-      }),
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
     })
+    return config
   },
 
   webpackDevMiddleware: (config) => {
--- frontend/pages/changelog.tsx
+++ frontend/pages/changelog.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import fileContent from '../CHANGELOG.md'
+import UiContainer from '@/components/Ui/Container/UiContainer'
 
 const ChangelogPage : React.VFC = () => {
-  const [content, setContent] = useState('')
-
-  useEffect(() => {
-    fetch(fileContent).then((res) => res.text()).then((text) => setContent(text))
-  })
   return (
-    <ReactMarkdown children={content} />
+    <UiContainer>
+      <ReactMarkdown>
+        {fileContent}
+      </ReactMarkdown>
+    </UiContainer>
   )
 }
 
