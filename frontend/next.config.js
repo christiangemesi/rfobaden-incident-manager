c019db396f9bee90174490ce44e3811f9e33354c
Andri Wild <andri.wild@students.fhnw.ch>
2022-02-14 12:53:24 +0100

    Update next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@
  **/
 const config = {
   reactStrictMode: true,
+
   // swcMinify: true,
   eslint: {
     dirs: [
@@ -17,11 +18,26 @@ const config = {
   },
 
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
+    return Object.assign({}, config, {
+      externals: Object.assign({}, config.externals, {
+        fs: 'fs',
+      }),
+      module: Object.assign({}, config.module, {
+        rules: config.module.rules.concat([
+          {
+            test: /\.md$/,
+            loader: 'emit-file-loader',
+            options: {
+              name: 'dist/[path][name].[ext]',
+            },
+          },
+          {
+            test: /\.md$/,
+            loader: 'raw-loader',
+          },
+        ]),
+      }),
     })
-    return config
   },
 
   webpackDevMiddleware: (config) => {
