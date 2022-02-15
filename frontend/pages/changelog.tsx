f39fc4141fb2a7319d31a292d5aed067afd3159c
Andri Wild <andri.wild@students.fhnw.ch>
2022-02-15 11:26:17 +0100

    Update changelog page style

--- frontend/pages/changelog.tsx
+++ frontend/pages/changelog.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import UiContainer from '@/components/Ui/Container/UiContainer'
-import changelog from '../CHANGELOG.md'
+import styled from 'styled-components'
 
+import changelog from '../CHANGELOG.md'
 import 'github-markdown-css/github-markdown-light.css'
 
 const ChangelogPage : React.VFC = () => {
   return (
-    <article className="markdown-body">
-      <UiContainer>
-        <ReactMarkdown children={changelog} />
-      </UiContainer>
-    </article>
+    <UiContainer>
+      <LogContainer className="markdown-body">
+        <ReactMarkdown children={changelog} components={{
+          ul: ({ ...children }) => <ul style={{ listStyle: 'disc' }} {...children} /> }} />
+      </LogContainer>
+    </UiContainer>
   )
 }
 
 export default ChangelogPage
+
+const LogContainer = styled.div`
+  background-color: ${({ theme }) => theme.colors.tertiary.value};
+`
