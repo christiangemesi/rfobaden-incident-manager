b01c408e5361ce5e9586d66c73add9daabe1b011
Andri Wild <andri.wild@students.fhnw.ch>
2022-02-16 13:52:23 +0100

    Update list-item style of LogContainer

--- frontend/pages/changelog.tsx
+++ frontend/pages/changelog.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import styled from 'styled-components'
-
 import changelog from '../CHANGELOG.md'
 import 'github-markdown-css/github-markdown-light.css'
 
@@ -10,8 +9,7 @@ const ChangelogPage : React.VFC = () => {
   return (
     <UiContainer>
       <LogContainer className="markdown-body">
-        <ReactMarkdown children={changelog} components={{
-          ul: ({ ...children }) => <ul style={{ listStyle: 'disc' }} {...children} /> }} />
+        <ReactMarkdown children={changelog} />
       </LogContainer>
     </UiContainer>
   )
@@ -21,4 +19,8 @@ export default ChangelogPage
 
 const LogContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.tertiary.value};
+  
+  ul {
+    list-style: disc;
+  }
 `
