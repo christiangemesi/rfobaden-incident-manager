d4f990693d69c81a4b9daee06d16e4fd931fbfe2
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-02-19 16:27:48 +0100

    - change of example page

--- frontend/pages/examples/ui-date-input.tsx
+++ frontend/pages/examples/ui-date-input.tsx
@@ -1,14 +1,16 @@
 import UiDateInput from '@/components/Ui/Input/Date/UiDateInput'
 import { useState } from 'react'
+import UiContainer from '@/components/Ui/Container/UiContainer'
+import UiTextInput from '@/components/Ui/Input/Text/UiTextInput'
 
 const UiDateInputExample: React.VFC = () => {
   const [value, setValue] = useState<Date | null>(null)
 
   return(
-    <div>
-      <UiDateInput value={value} onChange={setValue} label="Date Input" placeholder="dd.mm.yyyy hh:mm" />
-      {value?.toLocaleString()}
-    </div>
+    <UiContainer>
+      <UiDateInput value={value} onChange={setValue} label="Date Input" placeholder="dd.MM.yyyy hh:mm" />
+      <UiTextInput value={value?.toLocaleString()??''} onChange={function () {return}} />
+    </UiContainer>
   )
 }
 
