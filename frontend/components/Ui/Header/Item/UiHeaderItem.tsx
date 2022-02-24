ed2348600c01c9162e9f851146dddad91e0ce2b6
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-02-24 09:33:09 +0100

    Change design of header items

--- frontend/components/Ui/Header/Item/UiHeaderItem.tsx
+++ frontend/components/Ui/Header/Item/UiHeaderItem.tsx
@@ -1,51 +1,60 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { useRouter } from 'next/router'
 import { StyledProps } from '@/utils/helpers/StyleHelper'
 import styled, { css } from 'styled-components'
 import UiLink from '@/components/Ui/Link/UiLink'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
+import Link from 'next/link'
 
 interface Props extends StyledProps {
-  children: string
-  href: string
+  href?: string
+  onClick?: () => void
+  title?: string
+  children: ReactNode
 }
 
-const UiHeaderItem: React.VFC<Props> = ({ href, children }) => {
-
+const UiHeaderItem: React.VFC<Props> = ({ href, onClick, title, children }) => {
   const router = useRouter()
-
   return (
-    <NavItem data-name={children}>
-      <NavLink href={href} isActive={router.asPath === href}>
-        {children}
-      </NavLink>
+    <NavItem>
+      {href ? (
+        <Link href={href} passHref>
+          <NavLink isActive={router.asPath === href} title={title}>
+            {children}
+          </NavLink>
+        </Link>
+      ) : (
+        <NavLink as="span" isActive={false} title={title} onClick={onClick}>
+          {children}
+        </NavLink>
+      )}
     </NavItem>
   )
 }
 export default UiHeaderItem
 
 const NavItem = styled.li`
-  padding: 0.75rem;
-
-  &::before {
-    display: block;
-    content: attr(data-name);
-    font-weight: bold;
-    height: 1px;
-    color: transparent;
-    overflow: hidden;
-    visibility: hidden;
-  }
+  padding: 0.75rem 0;
+  list-style: none;
 `
-const NavLink = styled(UiLink)<{ isActive: boolean }>`
-  color: ${({ theme }) => theme.colors.secondary.contrast};
-  font-size: 1rem;
-  transition: 150ms ease-out;
-  transition-property: font-weight;
 
+const NavLink = styled.a<{ isActive: boolean }>`
+  display: flex;
+  align-items: center;
+  
+  font-size: 1rem;
+  letter-spacing: 1px;
+  color: currentColor;
+  text-decoration: none;
+  cursor: pointer;
+  
+  transition: 150ms ease;
+  transition-property: text-shadow;
+  
   ${({ isActive }) => {
     if (isActive) {
       return css`
-        border-bottom: solid 1px ${({ theme }) => theme.colors.secondary.contrast};
+        border-bottom: solid 1px currentColor;
       `
     }
     return css`
@@ -54,6 +63,22 @@ const NavLink = styled(UiLink)<{ isActive: boolean }>`
   }}
   
   :hover {
-    font-weight: bold;
+    text-shadow: 0 0 1px black;
+    
+    ${UiIcon} {
+      transform: scale(1.1);
+    }
+  }
+
+  ${UiIcon} {
+    transition: 150ms ease;
+    transition-property: transform;
+    
+    :not(:first-child) {
+      margin-left: 0.25rem;
+    }
+    :not(:last-child) {
+      margin-right: 0.25rem;
+    }
   }
 `
