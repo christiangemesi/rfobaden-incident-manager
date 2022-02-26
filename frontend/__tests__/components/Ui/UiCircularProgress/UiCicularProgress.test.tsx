1ed6e9a0f0df8ccd3f1ed759068c9b6c8523c51d
Andri Wild <andri.wild@students.fhnw.ch>
2022-02-26 15:48:34 +0100

    Update UiCircularProgress test

--- frontend/__tests__/components/Ui/UiCircularProgress/UiCicularProgress.test.tsx
+++ frontend/__tests__/components/Ui/UiCircularProgress/UiCicularProgress.test.tsx
@@ -4,9 +4,30 @@
 
 import { shallow } from 'enzyme'
 import UiCircularProgress from '@/components/Ui/CircularProgress/UiCircularProgress'
+import UiTitle from '@/components/Ui/Title/UiTitle'
 
-describe('UiTextInput', () => {
-  it('should be "button" tag', () => {
-    const html = shallow(<UiCircularProgress done={0} total={0} />)
+describe('UiCircularProgress', () => {
+  describe('With two titles in it', () => {
+    it('Should be the first a h5 and the second a h6 title', () => {
+      const html = shallow(<UiCircularProgress done={0} total={0} />)
+      const title1 = html.find(UiTitle).first()
+      expect(title1.html()).toContain('h5')
+      expect(title1.text()).toBe('0/0')
+
+      const title2 = html.find(UiTitle).at(1)
+      expect(title2.html()).toContain('h6')
+      expect(title2.text()).toBe('0%')
+    })
+
+    it('Should correctly display the amount of completed tasks', () => {
+      const html = shallow(<UiCircularProgress done={1} total={2} />)
+      const title1 = html.find(UiTitle).first()
+      expect(title1.html()).toContain('h5')
+      expect(title1.text()).toBe('1/2')
+
+      const title2 = html.find(UiTitle).at(1)
+      expect(title2.html()).toContain('h6')
+      expect(title2.text()).toBe('50%')
+    })
   })
 })
