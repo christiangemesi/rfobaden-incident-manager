ceb6b60a9a9fd152c759c1cb9e4982ae847587b6
Andri Wild <andri.wild@students.fhnw.ch>
2022-02-27 11:21:39 +0100

    Add target prop to UiLink

--- frontend/components/Ui/Footer/UiFooter.tsx
+++ frontend/components/Ui/Footer/UiFooter.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image'
 const UiFooter: React.VFC = () => {
   return (
     <FooterContainer>
-      <UiLink href="https://rfobaden.ch/">
+      <UiLink href="https://rfobaden.ch/" target="_blank">
         RFO Baden
       </UiLink>
-        &copy; {new Date().getFullYear()} RFO Baden
-      <UiLink href="https://www.fhnw.ch/de/">
+      &copy; {new Date().getFullYear()} RFO Baden
+      <UiLink href="https://www.fhnw.ch/de/" target="_blank">
         <Image src="/fhnw-logo.svg" alt="FHNW Logo" width="108" height="25" />
       </UiLink>
     </FooterContainer>
@@ -20,11 +20,12 @@ const UiFooter: React.VFC = () => {
 export default UiFooter
 
 const FooterContainer = styled.footer`
-  background: ${({ theme }) => theme.colors.secondary.value};
-  height: 4rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  height: 4rem;
   padding: 10px 50px;
+  background: ${({ theme }) => theme.colors.secondary.value};
   font-size: 0.8em;
 `
+
