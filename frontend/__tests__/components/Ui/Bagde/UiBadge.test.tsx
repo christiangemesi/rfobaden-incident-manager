7f71a0479cec13888e401438e6797240b16e5619
Andri Wild <andri.wild@students.fhnw.ch>
2022-03-02 15:39:16 +0100

    Add UiBadge test

--- frontend/__tests__/components/Ui/Bagde/UiBadge.test.tsx
+++ frontend/__tests__/components/Ui/Bagde/UiBadge.test.tsx
@@ -2,14 +2,33 @@
  * @jest-environment jsdom
  */
 
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import UiBadge from '@/components/Ui/Badge/UiBadge'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '@/theme'
 
-describe('UiDateLabel', () => {
-  it('should correctly display a single past date', () => {
-    const children = <div></div>
+describe('UiBadge', () => {
+  it('should be a span', () => {
+    const children = <div />
     const value = 123
-    const html = shallow(<UiBadge value={value} children={children} />)
-    expect(html.text()).toBe(value.toString())
+    const wrapper = mount(
+      <ThemeProvider theme={defaultTheme}>
+        <UiBadge value={value} children={children} />
+      </ThemeProvider>
+    )
+    expect(wrapper.find('span').first().html()).toContain('</span>')
+  })
+  it('should correctly display the value', () => {
+    const children = <div />
+    const value = 123
+    const wrapper = mount(
+      <ThemeProvider theme={defaultTheme}>
+        <UiBadge value={value} children={children} />
+      </ThemeProvider>
+    )
+
+    const content = wrapper.find('span')
+    expect(content).toHaveLength(2)
+    expect(content.get(1).props.children).toBe(value)
   })
 })
