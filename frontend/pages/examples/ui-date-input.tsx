7e2a254e31b087d3bc8425b85491ad370c3c8637
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-03-04 00:12:58 +0100

    - extend example

--- frontend/pages/examples/ui-date-input.tsx
+++ frontend/pages/examples/ui-date-input.tsx
@@ -4,12 +4,25 @@ import UiContainer from '@/components/Ui/Container/UiContainer'
 
 const UiDateInputExample: React.VFC = () => {
   const [value, setValue] = useState<Date | null>(null)
+  const [value2, setValue2] = useState<Date | null>(null)
 
   return (
     <UiContainer>
-      <UiDateInput value={value} onChange={setValue} label="Date Input" placeholder="dd.MM.yyyy hh:mm" />
-      <div>Date:</div>
-      {value?.toLocaleString()}
+      <UiDateInput
+        value={value}
+        onChange={setValue}
+        label="Date Input as Popover"
+        placeholder="dd.MM.yyyy hh:mm" />
+      <div>Date: {value?.toLocaleString()}</div>
+      <br />
+
+      <UiDateInput
+        value={value2}
+        isModal={true}
+        onChange={setValue2}
+        label="Date Input as Modal"
+        placeholder="dd.MM.yyyy hh:mm" />
+      <div>Date: {value2?.toLocaleString()}</div>
     </UiContainer>
   )
 }
