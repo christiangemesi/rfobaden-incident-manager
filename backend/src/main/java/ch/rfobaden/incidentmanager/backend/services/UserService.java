b852c372a11e89f9fcdc5cbe234be9a0cabcebcd
Lucifer Uchiha <rowla101@gmail.com>
2022-03-15 15:51:25 +0100

    fixed checkstyle in userService

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/UserService.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/UserService.java
@@ -1,10 +1,5 @@
 package ch.rfobaden.incidentmanager.backend.services;
 
-import java.security.SecureRandom;
-import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.Optional;
-
 import ch.rfobaden.incidentmanager.backend.EmailConfig;
 import ch.rfobaden.incidentmanager.backend.models.User;
 import ch.rfobaden.incidentmanager.backend.models.UserCredentials;
@@ -17,6 +12,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class UserService extends ModelRepositoryService.Basic<User, UserRepository> {
