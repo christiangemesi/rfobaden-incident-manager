618013a19f476c0b7cbef58ebb5375fe09865c3b
Andri Wild <andri.wild@students.fhnw.ch>
2022-03-18 19:42:51 +0100

    Add entity image

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Image.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Image.java
@@ -1,2 +1,64 @@
-package ch.rfobaden.incidentmanager.backend.models;public class Image {
+package ch.rfobaden.incidentmanager.backend.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "image")
+public class Image {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Image() {}
+
+    public Image(String location, String name) {
+        this.location = location;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
