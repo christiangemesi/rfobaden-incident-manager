38eca335b7fa3b610d3c725c12a9da7a19f8a46e
Andri Wild <andri.wild@students.fhnw.ch>
2022-03-18 19:45:51 +0100

    Add FileLocationService

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/FileLocationService.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/FileLocationService.java
@@ -1,2 +1,31 @@
-package ch.rfobaden.incidentmanager.backend.services;public class FileLocationService {
+package ch.rfobaden.incidentmanager.backend.services;
+
+import ch.rfobaden.incidentmanager.backend.models.Image;
+import ch.rfobaden.incidentmanager.backend.repos.FileSystemRepository;
+import ch.rfobaden.incidentmanager.backend.repos.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class FileLocationService {
+
+    @Autowired
+    private FileSystemRepository fileSystemRepository;
+    @Autowired
+    private ImageRepository imageRepository;
+
+    public Long save(byte[] bytes, String imageName) {
+        String location = fileSystemRepository.save(bytes, imageName);
+
+        return imageRepository.save(new Image(location, imageName)).getId();
+    }
+
+    public FileSystemResource find(Long imageId) {
+        Image image = imageRepository.findById(imageId)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return fileSystemRepository.findInFileSystem(image.getLocation());
+    }
 }
