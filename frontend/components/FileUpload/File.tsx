9404c0d568f938d52e0bd106cf15aa3eeef2f821
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2022-03-24 09:37:32 +0100

    updated fileupload

--- frontend/components/FileUpload/File.tsx
+++ frontend/components/FileUpload/File.tsx
@@ -1,15 +1,47 @@
-function File() {
+import { UiInputProps } from '@/components/Ui/Input'
+import React, { ChangeEvent, ReactNode, useCallback } from 'react'
 
-  const onSubmit = (data) => {
-    console.log(data)
-  }
+
+interface Props extends UiInputProps<string | null> {
+  label?: string
+  type?: 'file'
+  placeholder?: string
+  children?: ReactNode
+  onClick?: () => void
+}
+
+const File: React.VFC<Props> = ({
+  onChange: setValue,
+
+
+
+}) => {
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file != undefined) {
+      console.log('Uploaded :' + file.name)
+    }
+
+    if (setValue) {
+      const newValue = e.target.value
+      if (newValue.length === 0) {
+        setValue(null)
+      } else {
+        setValue(newValue)
+      }
+    }
+
+
+  }, [setValue])
 
   return (
     <form>
-      <input type="file" name="picture" />
+      <input type="file" name="picture" onChange={handleChange} />
       <button> Submit</button>
+
     </form>
   )
 }
 
 export default File
+
