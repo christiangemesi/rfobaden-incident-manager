929ef78eb53ed6fdebf97c1bce991f70df03ea6b
Daniel von Atzigen <daniel@vatz.ch>
2022-03-27 22:11:20 +0200

    Extract common model functionality into interfaces

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Subtask.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Subtask.java
@@ -15,13 +15,15 @@ import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.sound.midi.Track;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "subtask")
-public class Subtask extends Model implements PathConvertible<SubtaskPath>, Serializable {
+public class Subtask extends Model
+    implements PathConvertible<SubtaskPath>, Trackable, Serializable {
     private static final long serialVersionUID = 1L;
 
     @ManyToOne
@@ -54,32 +56,14 @@ public class Subtask extends Model implements PathConvertible<SubtaskPath>, Seri
     @Column(nullable = false)
     private Priority priority;
 
-    @JsonIgnore
+    @Override
     public User getAssignee() {
         return assignee;
     }
 
-    @JsonIgnore
+    @Override
     public void setAssignee(User assignee) {
         this.assignee = assignee;
-    }
-
-    public Long getAssigneeId() {
-        if (assignee == null) {
-            return null;
-        }
-        return assignee.getId();
-    }
-
-    public void setAssigneeId(Long assigneeId) {
-        if (assigneeId == null) {
-            this.assignee = null;
-            return;
-        }
-        if (this.assignee == null) {
-            this.assignee = new User();
-        }
-        this.assignee.setId(assigneeId);
     }
 
     public Long getTaskId() {
@@ -112,53 +96,74 @@ public class Subtask extends Model implements PathConvertible<SubtaskPath>, Seri
         this.task = task;
     }
 
+    @Override
     public String getTitle() {
         return title;
     }
 
+    @Override
     public void setTitle(String title) {
         this.title = title;
     }
 
+    @Override
     public String getDescription() {
         return description;
     }
 
+    @Override
     public void setDescription(String description) {
         this.description = description;
     }
 
+    @Override
     public LocalDateTime getStartsAt() {
         return startsAt;
     }
 
+    @Override
     public void setStartsAt(LocalDateTime startsAt) {
         this.startsAt = startsAt;
     }
 
+    @Override
     public LocalDateTime getEndsAt() {
         return endsAt;
     }
 
+    @Override
     public void setEndsAt(LocalDateTime endsAt) {
         this.endsAt = endsAt;
     }
 
+    @Override
     public Priority getPriority() {
         return priority;
     }
 
+    @Override
     public void setPriority(Priority priority) {
         this.priority = priority;
     }
 
-    @JsonProperty("isClosed")
+    @Override
     public boolean isClosed() {
         return isClosed;
     }
 
+    @Override
     public void setClosed(boolean closed) {
         isClosed = closed;
+    }
+
+    @Override
+    public String getLink() {
+        return getTask().getLink() + "/unterauftraege/" + getId();
+    }
+
+    @Override
+    public String getFullTitle() {
+        return getTask().getFullTitle() + "/" + getTitle();
     }
 
     @Override
@@ -183,8 +188,17 @@ public class Subtask extends Model implements PathConvertible<SubtaskPath>, Seri
 
     @Override
     public int hashCode() {
-        return Objects.hash(modelHashCode(), assignee, task, title,
-            description, startsAt, endsAt, priority, isClosed);
+        return Objects.hash(
+            modelHashCode(),
+            assignee,
+            task,
+            title,
+            description,
+            startsAt,
+            endsAt,
+            priority,
+            isClosed
+        );
     }
 
     @Override
