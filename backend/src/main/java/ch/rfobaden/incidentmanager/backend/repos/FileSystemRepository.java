537734380c7b14ca849ff9d847f3f86fcdeb4590
Andri Wild <andri.wild@students.fhnw.ch>
2022-03-30 12:55:26 +0200

    Removed thumbnail from FileSystemRepository

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/repos/FileSystemRepository.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/repos/FileSystemRepository.java
@@ -1,28 +1,42 @@
 package ch.rfobaden.incidentmanager.backend.repos;
 
+import ch.rfobaden.incidentmanager.backend.models.Image;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Repository;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
+import javax.imageio.ImageIO;
 
 @Repository
 public class FileSystemRepository {
-    public static final String RESOURCES_DIR = "images/";
-//        FileSystemRepository.class.getResource("/").getPath();
+    public static final String RESOURCES_DIR = "files/images";
+    public static final String IMAGE_FORMAT = "jpeg";
 
-    public String save(byte[] content, String imageName) {
-        Path newFile = Paths.get(RESOURCES_DIR + new Date().getTime() + "-" + imageName);
+    public Image save(byte[] content, String imageName) {
+        final long dateTime = new Date().getTime();
+        Path originalImgPath = Paths.get(
+                RESOURCES_DIR + dateTime + "-" + imageName
+        );
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(content);
+        BufferedImage originalImg;
 
         try {
-            Files.write(newFile, content);
+            originalImg = ImageIO.read(byteArrayInputStream);
+            ImageIO.write(originalImg, IMAGE_FORMAT, new File(originalImgPath.toAbsolutePath().toString()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return newFile.toAbsolutePath().toString();
+
+        Image image = new Image(imageName);
+        image.setLocation(originalImgPath.toAbsolutePath().toString());
+        return image;
     }
 
     public FileSystemResource findInFileSystem(String location) {
