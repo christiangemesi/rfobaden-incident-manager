deb5ac6a39f0824b365d9bc241e35ad145c0dc8b
Andri Wild <andri.wild@students.fhnw.ch>
2022-04-01 15:17:11 +0200

    Add ImageTest

--- backend/src/test/java/ch/rfobaden/incidentmanager/backend/models/ImageTest.java
+++ backend/src/test/java/ch/rfobaden/incidentmanager/backend/models/ImageTest.java
@@ -1,6 +1,54 @@
 package ch.rfobaden.incidentmanager.backend.models;
 
-public class ImageTest {
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.Test;
+
+class ImageTest {
+
+    public static final String PATH_TO_FILE = "src/test/resources/testImage/fish.jpeg";
+
+    @Test
+    void equalsTest() {
+        // given
+        Long id = 42L;
+        Image image1 = new Image("fish");
+        image1.setLocation(PATH_TO_FILE);
+        image1.setId(id);
+        Image image2 = new Image("fish");
+        image2.setLocation(PATH_TO_FILE);
+        image2.setId(id);
+        
+        // then
+        assertEquals(image1, image2);
+        assertEquals(image1.hashCode(), image2.hashCode());
+    }
+
+    @Test
+    void notEqualsTest() {
+        // given
+        Image image1 = new Image("fish");
+        image1.setLocation(PATH_TO_FILE);
+        image1.setId(42L);
+        Image image2 = new Image("fish");
+        image2.setLocation(PATH_TO_FILE);
+        image2.setId(43L);
+
+        // then
+        assertNotEquals(image1, image2);
+    }
+
+    @Test
+    void notSameClassTest() {
+        // given
+        Image image1 = new Image("fish");
+        image1.setLocation(PATH_TO_FILE);
+        image1.setId(42L);
+        Object o = new Object();
+
+        // then
+        assertNotEquals(image1, o);
+    }
 }
