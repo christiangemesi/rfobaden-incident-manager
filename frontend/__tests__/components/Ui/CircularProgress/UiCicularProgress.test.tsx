843ee31177d4ebd9f28a978b78bc036703e9b4cb
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-04-01 17:35:55 +0200

    Fix frontend tests

--- frontend/__tests__/components/Ui/CircularProgress/UiCicularProgress.test.tsx
+++ frontend/__tests__/components/Ui/CircularProgress/UiCicularProgress.test.tsx
@@ -2,32 +2,35 @@
  * @jest-environment jsdom
  */
 
-import { shallow } from 'enzyme'
-import UiTitle from '@/components/Ui/Title/UiTitle'
+import { render } from 'enzyme'
 import UiCircularProgress from '@/components/Ui/CircularProgress/UiCircularProgress'
+import { defaultTheme } from '@/theme'
+import { ThemeProvider } from 'styled-components'
 
 describe('UiCircularProgress', () => {
-  describe('With two titles in it', () => {
-    it('Should be the first a h5 and the second a h6 title', () => {
-      const html = shallow(<UiCircularProgress done={0} total={0} />)
-      const title1 = html.find(UiTitle).first()
-      expect(title1.html()).toContain('h5')
-      expect(title1.text()).toBe('0/0')
+  it('Should contain the correct progress numbers', () => {
+    const html = render(
+      <ThemeProvider theme={defaultTheme}>
+        <UiCircularProgress done={0} total={0} />
+      </ThemeProvider>
+    )
+    const stats = html.find('text').first()
+    expect(stats.text()).toBe('0/0')
 
-      const title2 = html.find(UiTitle).at(1)
-      expect(title2.html()).toContain('h6')
-      expect(title2.text()).toBe('0%')
-    })
+    const percentage = html.find('text').last()
+    expect(percentage.text()).toBe('0%')
+  })
 
-    it('Should correctly display the amount of completed tasks', () => {
-      const html = shallow(<UiCircularProgress done={1} total={2} />)
-      const title1 = html.find(UiTitle).first()
-      expect(title1.html()).toContain('h5')
-      expect(title1.text()).toBe('1/2')
+  it('should correctly display the amount of completed tasks', () => {
+    const html = render(
+      <ThemeProvider theme={defaultTheme}>
+        <UiCircularProgress done={1} total={2} />
+      </ThemeProvider>
+    )
+    const stats = html.find('text').first()
+    expect(stats.text()).toBe('1/2')
 
-      const title2 = html.find(UiTitle).at(1)
-      expect(title2.html()).toContain('h6')
-      expect(title2.text()).toBe('50%')
-    })
+    const percentage = html.find('text').last()
+    expect(percentage.text()).toBe('50%')
   })
 })
