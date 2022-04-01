8866d7d7a580c07153f413a8529ba04327f2faa6
Lucifer Uchiha <rowla101@gmail.com>
2022-04-01 16:17:38 +0200

    finsished up

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { isAdmin } from '@/models/User'
+import { useCurrentUser } from '@/stores/SessionStore'
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import UiTitle from '@/components/Ui/Title/UiTitle'
 import UiGrid from '@/components/Ui/Grid/UiGrid'
@@ -7,31 +9,35 @@ import UiIcon from '@/components/Ui/Icon/UiIcon'
 import UiLink from '@/components/Ui/Link/UiLink'
 
 const DashboardPage: React.VFC = () => {
+  const currentUser = useCurrentUser()
+
   return (
     <UiContainer>
       <UiTitle level={1}>Dashboard</UiTitle>
       <UiGrid gap={1.5}>
         {data.map((card) => (
-          <UiGrid.Col key={card.label} size={{ sm: 6, lg: 4, xxl: 3 }}>
-            <UiLink href={card.link}>
-              <Card>
-                <card.icon size={5} />
-                <CardTitle level={4}>{card.label}</CardTitle>
-              </Card>
-            </UiLink>
-          </UiGrid.Col>
-        ))}
+          (isAdmin(currentUser) || card.forAll) &&(
+            <UiGrid.Col key={card.label} size={{ sm: 6, lg: 4, xxl: 3 }}>
+              <UiLink href={card.link}>
+                <Card>
+                  <card.icon size={5} />
+                  <CardTitle level={4}>{card.label}</CardTitle>
+                </Card>
+              </UiLink>
+            </UiGrid.Col>
+          ))
+        )}
       </UiGrid>
     </UiContainer>
   )
 }
 
 const data = [
-  { icon: UiIcon.IncidentManagement, label: 'Ereignisse', link: '/ereignisse' }, 
-  { icon: UiIcon.Transport, label: 'Transporte', link: '/benutzer' },
-  { icon: UiIcon.Monitoring, label: 'Übersicht', link: '/benutzer' },
-  { icon: UiIcon.User, label: 'Benutzer', link: '/benutzer' },
-  { icon: UiIcon.Organization, label: 'Organizationen', link: '/benutzer' },
+  { icon: UiIcon.IncidentManagement, label: 'Ereignisse', link: '/ereignisse', forAll: true }, 
+  { icon: UiIcon.Transport, label: 'Transporte', link: '/benutzer', forAll: true },
+  { icon: UiIcon.Monitoring, label: 'Übersicht', link: '/benutzer', forAll: true },
+  { icon: UiIcon.User, label: 'Benutzer', link: '/benutzer', forAll: false },
+  { icon: UiIcon.Organization, label: 'Organizationen', link: '/benutzer', forAll: false },
 ]
 
 const Card = styled.div`
