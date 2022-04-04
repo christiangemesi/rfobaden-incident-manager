4114ca9ba6f0d55e4363f71c4abbfc5d24b4e7a0
Lucifer Uchiha <rowla101@gmail.com>
2022-04-04 08:30:39 +0200

    Cleaned up Transport Actions

--- frontend/components/Transport/Actions/TransportActions.tsx
+++ frontend/components/Transport/Actions/TransportActions.tsx
@@ -1,4 +1,4 @@
-import Transport, { parseTransport } from '@/models/Transport'
+import Transport from '@/models/Transport'
 import React, { useCallback } from 'react'
 import UiDropDown from '@/components/Ui/DropDown/UiDropDown'
 import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
@@ -6,7 +6,7 @@ import UiIcon from '@/components/Ui/Icon/UiIcon'
 import UiModal from '@/components/Ui/Modal/UiModal'
 import UiTitle from '@/components/Ui/Title/UiTitle'
 import TransportForm from '@/components/Transport/Form/TransportForm'
-import BackendService, { BackendResponse } from '@/services/BackendService'
+import BackendService from '@/services/BackendService'
 import TransportStore from '@/stores/TransportStore'
 import Incident from '@/models/Incident'
 
@@ -26,36 +26,6 @@ const ReportActions: React.VFC<Props> = ({ incident, transport, onDelete: handle
       TransportStore.remove(transport.id)
     }
   }, [transport, handleDeleteCb])
-
-  const handleClose = useCallback(async () => {
-    if (confirm(`Sind sie sicher, dass sie den Transport "${transport.title}" schliessen wollen?`)) {
-      const newTransport = { ...transport, isClosed: true }
-      const [data, error]: BackendResponse<Transport> = await BackendService.update(
-        `incidents/${transport.incidentId}/transports`,
-        transport.id,
-        newTransport
-      )
-      if (error !== null) {
-        throw error
-      }
-      TransportStore.save(parseTransport(data))
-    }
-  }, [transport])
-
-  const handleReopen = useCallback(async () => {
-    if (confirm(`Sind sie sicher, dass sie den Transport "${transport.title}" öffnen wollen?`)) {
-      const newTransport = { ...transport, isClosed: false }
-      const [data, error]: BackendResponse<Transport> = await BackendService.update(
-        `incidents/${transport.incidentId}/transports`,
-        transport.id,
-        newTransport
-      )
-      if (error !== null) {
-        throw error
-      }
-      TransportStore.save(parseTransport(data))
-    }
-  }, [transport])
 
   return (
     <UiDropDown>
@@ -82,12 +52,6 @@ const ReportActions: React.VFC<Props> = ({ incident, transport, onDelete: handle
             )}
           </UiModal.Body>
         </UiModal>
-        {transport.isClosed ? (
-          <UiDropDown.Item onClick={handleReopen}>Öffnen</UiDropDown.Item>
-        ) : (
-          <UiDropDown.Item onClick={handleClose}>Schliessen</UiDropDown.Item>
-        )}
-
         <UiDropDown.Item onClick={handleDelete}>Löschen</UiDropDown.Item>
       </UiDropDown.Menu>
     </UiDropDown>
