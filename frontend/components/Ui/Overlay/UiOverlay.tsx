97e709851b9fea15b9c8ecdf51a01dc3ce187b38
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-04-07 12:50:38 +0200

    Fix `UiOverlay` hiding transition when nested

--- frontend/components/Ui/Overlay/UiOverlay.tsx
+++ frontend/components/Ui/Overlay/UiOverlay.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components'
-import React, { useCallback, useRef } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import ScrollHelper from '@/utils/helpers/ScrollHelper'
-import { useUpdateEffect } from 'react-use'
+import { useTimeoutFn, useUpdateEffect } from 'react-use'
 import { ElementProps } from '@/utils/helpers/StyleHelper'
 import { noop } from '@/utils/control-flow'
 
@@ -33,15 +33,29 @@ const UiOverlay: React.VFC<Props> = ({
     }
   }, [handleClickCallback])
 
+  // Shows if the overlay is completely hidden.
+  // A hidden overlay is moved to a lower z-index, so it does not block pointer events.
+  const [isHidden, setHidden] = useState(false)
+
+  // Makes the overlay visible right when it opens,
+  // and hides it after its close transition has ended.
+  useEffect(function hideOrShow() {
+    if (isOpen) {
+      setHidden(false)
+    } else {
+      setTimeout(() => setHidden(true), 300)
+    }
+  }, [isOpen])
+
   return (
-    <Background {...elementProps} ref={ref} isOpen={isOpen} onClick={handleClick}>
+    <Background {...elementProps} ref={ref} isOpen={isOpen} isHidden={isHidden} onClick={handleClick}>
       {children}
     </Background>
   )
 }
 export default UiOverlay
 
-const Background = styled.div<Props>`
+const Background = styled.div<Props & { isHidden: boolean }>`
   position: fixed;
   top: 0;
   left: 0;
@@ -55,12 +69,15 @@ const Background = styled.div<Props>`
   
   background: rgba(25, 25, 25, 0.4);
   transition: ${({ theme }) => theme.transitions.slideIn};
-  transition-property: background-color, opacity, z-index;
+  transition-property: background-color, opacity, display;
 
   ${({ isOpen }) => !isOpen && css`
-    z-index: -1;
     transition: ${({ theme }) => theme.transitions.slideOut};
     background: transparent;
     opacity: 0;
+  `}
+
+  ${({ isHidden }) => isHidden && css`
+    z-index: -1;
   `}
 `
