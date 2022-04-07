7c0c5e0d984f311847faf0b96dd424cc0005e43f
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-04-07 11:15:07 +0200

    Refactor incident create form to use new modal

--- frontend/components/Incident/List/IncidentList.tsx
+++ frontend/components/Incident/List/IncidentList.tsx
@@ -5,8 +5,6 @@ import IncidentListItem from '@/components/Incident/List/Item/IncidentListItem'
 import IncidentCreateButton from '@/components/Incident/CreateButton/IncidentCreateButton'
 import UiModal from '@/components/Ui/Modal/UiModal'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
-import UiContainer from '@/components/Ui/Container/UiContainer'
-import UiTitle from '@/components/Ui/Title/UiTitle'
 import IncidentForm from '@/components/Incident/Form/IncidentForm'
 import { useCurrentUser } from '@/stores/SessionStore'
 import { isAdmin } from '@/models/User'
@@ -27,17 +25,14 @@ const IncidentList: React.VFC<Props> = ({ incidents }) => {
       ))}
       {isAdmin(currentUser) && (
         <UiGrid.Col size={{ sm: 6, lg: 4, xxl: 3 }}>
-          <UiModal isFull>
-            <UiModal.Activator>{({ open }) => (
+          <UiModal title="Ereignis erstellen">
+            <UiModal.Trigger>{({ open }) => (
               <IncidentCreateButton onClick={open}>
                 <UiIcon.CreateAction size={2} />
               </IncidentCreateButton>
-            )}</UiModal.Activator>
+            )}</UiModal.Trigger>
             <UiModal.Body>{({ close }) => (
-              <UiContainer>
-                <UiTitle level={1} isCentered>Ereignis erstellen</UiTitle>
-                <IncidentForm onClose={close} />
-              </UiContainer>
+              <IncidentForm onClose={close} />
             )}</UiModal.Body>
           </UiModal>
         </UiGrid.Col>
