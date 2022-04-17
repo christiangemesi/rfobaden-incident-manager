0f94d455e34ca9e1a9cee7957d0a828b52dd23c0
Andri Wild <andri.wild@students.fhnw.ch>
2022-04-17 11:19:05 +0200

    Update UiImage designe

--- frontend/components/Ui/Image/UiImage.tsx
+++ frontend/components/Ui/Image/UiImage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styled from 'styled-components'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
 import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
-
+import UiModal from '@/components/Ui/Modal/UiModal'
 
 interface Props {
   src: string
@@ -15,19 +15,38 @@ const UiImage: React.VFC<Props> = ({
   text = '',
 }) => {
 
-  const deleteImage = () => {
+  const deleteImage = (e: React.MouseEvent) => {
+    e.stopPropagation()
     // TODO
     console.error('Operation not supported yet')
   }
 
   return (
-    <ImageCard>
-      <Image src={src} width={200} height={200} alt="Kein Bild" />
-      <TextArea>
-        {text}
-        <DeleteButton onClick={deleteImage}><UiIcon.Trash /></DeleteButton>
-      </TextArea>
-    </ImageCard>
+    <UiModal>
+      <UiModal.Trigger>{({ open }) => (
+        <ImageCard onClick={open}>
+
+          <ImageArea>
+            <div>
+              <Image src={src} width={200} height={200} alt="Kein Bild" />
+            </div>
+            <DeleteButton onClick={deleteImage}><UiIcon.Trash /></DeleteButton>
+          </ImageArea>
+
+          <TextArea>
+            {text}
+          </TextArea>
+
+        </ImageCard>
+      )}</UiModal.Trigger>
+      <UiModal.Body>
+
+        <ImageContainer>
+          <Image src={src} width={1280} height={720} objectFit="contain" alt="Kein Bild" />
+        </ImageContainer>
+
+      </UiModal.Body>
+    </UiModal>
   )
 }
 
@@ -35,13 +54,16 @@ export default UiImage
 
 const ImageCard = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
 
   :hover {
     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+
     & > div {
       & > button {
         visibility: visible;
+      }
+      & > div {
+        filter: opacity(50%);
       }
     }
   }
@@ -49,10 +71,23 @@ const ImageCard = styled.div`
 
 const DeleteButton = styled(UiIconButton)`
   visibility: hidden;
-  transition: 250ms;
-  transition-property: visibility;
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 1rem;
 `
 
 const TextArea = styled.div`
   padding: 8px 16px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 200px;
+`
+const ImageArea = styled.div`
+position: relative;
+`
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
 `
