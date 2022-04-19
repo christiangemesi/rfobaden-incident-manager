4f5b6c8e4fd5700f9f693a73e834737943eb2c7b
Andri Wild <andri.wild@students.fhnw.ch>
2022-04-19 16:18:39 +0200

    Add image drawer and image delete support

--- frontend/components/Report/Info/ReportInfo.tsx
+++ frontend/components/Report/Info/ReportInfo.tsx
@@ -5,6 +5,11 @@ import UiCaptionList from '@/components/Ui/Caption/List/UiCaptionList'
 import { useUsername } from '@/models/User'
 import { useUser } from '@/stores/UserStore'
 import UiCaption from '@/components/Ui/Caption/UiCaption'
+import UiDrawer from '@/components/Ui/Drawer/UiDrawer'
+import UiTitle from '@/components/Ui/Title/UiTitle'
+import UiImageList from '@/components/Ui/Image/List/UiImageList'
+import { FileId } from '@/models/FileUpload'
+import ReportStore from '@/stores/ReportStore'
 
 interface Props {
   report: Report
@@ -12,6 +17,10 @@ interface Props {
 
 const ReportInfo: React.VFC<Props> = ({ report }) => {
   const assigneeName = useUsername(useUser(report.assigneeId))
+
+  const storeImageIds = (ids: FileId[]) => {
+    ReportStore.save({ ...report, imageIds: ids })
+  }
 
   return (
     <UiCaptionList>
@@ -31,6 +40,25 @@ const ReportInfo: React.VFC<Props> = ({ report }) => {
       <UiCaption>
         <UiDateLabel start={report.startsAt ?? report.createdAt} end={report.endsAt} />
       </UiCaption>
+      <UiDrawer size="full">
+        <UiDrawer.Trigger>{({ open }) => (
+          <UiCaption onClick={open}>
+            {report.imageIds.length}
+            &nbsp;
+            {report.imageIds.length === 1 ? 'Bild' : 'Bilder'}
+          </UiCaption>
+        )}</UiDrawer.Trigger>
+        <UiDrawer.Body>
+          <UiTitle level={1}>
+            Bilder
+          </UiTitle>
+          <UiImageList
+            storeImageIds={storeImageIds}
+            imageIds={report.imageIds}
+            modelId={report.id}
+            modelName="report" />
+        </UiDrawer.Body>
+      </UiDrawer>
     </UiCaptionList>
   )
 }
