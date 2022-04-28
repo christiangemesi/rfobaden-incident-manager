5803d5ea92b450d095bd78ab44c78a1da8def4a7
Andri Wild <andri.wild@students.fhnw.ch>
2022-04-28 16:36:57 +0200

    Add DocumentItem component

---
+++ frontend/components/Document/Item/DocumentItem.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { FileId } from '@/models/FileUpload'
+import UiGrid from '@/components/Ui/Grid/UiGrid'
+import UiListItem from '@/components/Ui/List/Item/UiListItem'
+import styled from 'styled-components'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
+import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
+
+interface Props {
+  src: string
+  text: string
+  id: FileId
+  onDelete: (id: FileId) => void
+}
+
+const DocumentItem: React.VFC<Props> = ({
+  src = '',
+  text = '',
+  id,
+  onDelete,
+}) => {
+
+  const handleDelete = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    onDelete(id)
+  }
+  return (
+    <DocumentListItem>
+      <UiGrid style={{ padding: '0 0.5rem' }} align="center" gapH={0.5}>
+        <UiGrid.Col size={8}>
+          <FileText href="1.pdf" download>File mit ID: {id}</FileText>
+        </UiGrid.Col>
+        <UiGrid.Col size={3}>
+          <p>PDF</p>
+        </UiGrid.Col>
+        <UiGrid.Col size={1}>
+          <DeleteButton onClick={handleDelete}><UiIcon.Trash /></DeleteButton>
+        </UiGrid.Col>
+      </UiGrid>
+    </DocumentListItem>
+  )
+}
+
+export default DocumentItem
+
+const DeleteButton = styled(UiIconButton)`
+  visibility: hidden;
+  color: ${({ theme }) => theme.colors.secondary.contrast};
+`
+
+const FileText = styled.a`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;  
+  color: black;
+  text-decoration: none;
+`
+
+const DocumentListItem = styled(UiListItem)`
+  :hover {
+    & * ${DeleteButton} {
+      visibility: visible;
+    }
+  }
+`
+
