942b813d2d61c688c156237a89101b59bdccd563
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-04-29 21:55:43 +0200

    refactoring meine aufgaben page variables

--- frontend/pages/meine-aufgaben.tsx
+++ frontend/pages/meine-aufgaben.tsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import UiTitle from '@/components/Ui/Title/UiTitle'
-import { useSession } from '@/stores/SessionStore'
 import TransportStore, { useTransports } from '@/stores/TransportStore'
 import ReportStore, { useReports } from '@/stores/ReportStore'
 import TaskStore, { useTasks } from '@/stores/TaskStore'
 import SubtaskStore, { useSubtasks } from '@/stores/SubtaskStore'
 import { BackendResponse, getSessionFromRequest } from '@/services/BackendService'
-import Subtask, { isOpenedSubtask, parseSubtask } from '@/models/Subtask'
-import Task, { isOpenedTask, parseTask } from '@/models/Task'
-import Report, { isOpenedReport, parseReport } from '@/models/Report'
-import Transport, { isOpenedTransport, parseTransport } from '@/models/Transport'
+import Subtask, { isOpenSubtask, parseSubtask } from '@/models/Subtask'
+import Task, { isOpenTask, parseTask } from '@/models/Task'
+import Report, { isOpenReport, parseReport } from '@/models/Report'
+import Transport, { isOpenTransport, parseTransport } from '@/models/Transport'
 import { GetServerSideProps } from 'next'
 import Priority from '@/models/Priority'
 import IncidentStore, { useIncidents } from '@/stores/IncidentStore'
@@ -18,6 +17,7 @@ import Incident, { isClosedIncident, parseIncident } from '@/models/Incident'
 import styled from 'styled-components'
 import AssignedList from '@/components/AssignedList/AssignedList'
 import Assignments from '@/models/Assignments'
+import { useEffectOnce } from 'react-use'
 
 
 interface Props {
@@ -31,59 +31,54 @@ interface Props {
 }
 
 const MeineAufgabenPage: React.VFC<Props> = ({ data }) => {
-  const { currentUser } = useSession()
-  if (currentUser === null) {
-    throw new Error('not signed in')
-  }
-
-  useEffect(() => {
+  useEffectOnce(() => {
     IncidentStore.saveAll(data.incidents.map(parseIncident))
     TransportStore.saveAll(data.transports.map(parseTransport))
     ReportStore.saveAll(data.reports.map(parseReport))
     TaskStore.saveAll(data.tasks.map(parseTask))
     SubtaskStore.saveAll(data.subtasks.map(parseSubtask))
-  }, [data, currentUser])
+  })
 
   const openedIncidents = useIncidents((incidents) => incidents.filter((incident) => !isClosedIncident(incident)))
-  const usersTransports = useTransports((transports) => transports.filter(isOpenedTransport)).sort((a, b) => a.incidentId - b.incidentId)
-  const usersReports = useReports((reports) => reports.filter(isOpenedReport)).sort((a, b) => a.incidentId - b.incidentId)
-  const usersTasks = useTasks((tasks) => tasks.filter(isOpenedTask)).sort((a, b) => a.incidentId - b.incidentId)
-  const usersSubtasks = useSubtasks((subtasks) => subtasks.filter(isOpenedSubtask)).sort((a, b) => a.incidentId - b.incidentId)
-  const usersDoneTransports = useTransports((transports) => transports.filter((transport) => !isOpenedTransport(transport)).sort((a, b) => a.incidentId - b.incidentId))
-  const usersDoneReports = useReports((reports) => reports.filter((report) => !isOpenedReport(report)).sort((a, b) => a.incidentId - b.incidentId))
-  const usersDoneTasks = useTasks((tasks) => tasks.filter((task) => !isOpenedTask(task)).sort((a, b) => a.incidentId - b.incidentId))
-  const usersDoneSubtasks = useSubtasks((subtasks) => subtasks.filter((subtask) => !isOpenedSubtask(subtask)).sort((a, b) => a.incidentId - b.incidentId))
+  const assignedTransports = useTransports((transports) => transports.filter(isOpenTransport)).sort((a, b) => a.incidentId - b.incidentId)
+  const assignedReports = useReports((reports) => reports.filter(isOpenReport)).sort((a, b) => a.incidentId - b.incidentId)
+  const assignedTasks = useTasks((tasks) => tasks.filter(isOpenTask)).sort((a, b) => a.incidentId - b.incidentId)
+  const assignedSubtasks = useSubtasks((subtasks) => subtasks.filter(isOpenSubtask)).sort((a, b) => a.incidentId - b.incidentId)
+  const closedTransports = useTransports((transports) => transports.filter((transport) => !isOpenTransport(transport)).sort((a, b) => a.incidentId - b.incidentId))
+  const closedReports = useReports((reports) => reports.filter((report) => !isOpenReport(report)).sort((a, b) => a.incidentId - b.incidentId))
+  const closedTasks = useTasks((tasks) => tasks.filter((task) => !isOpenTask(task)).sort((a, b) => a.incidentId - b.incidentId))
+  const closedSubtasks = useSubtasks((subtasks) => subtasks.filter((subtask) => !isOpenSubtask(subtask)).sort((a, b) => a.incidentId - b.incidentId))
 
   const dataTrackableHigh = useMemo(() => ({
     incidents: openedIncidents,
-    transports: usersTransports.filter((e) => e.priority == Priority.HIGH),
-    reports: usersReports.filter((e) => e.priority == Priority.HIGH),
-    tasks: usersTasks.filter((e) => e.priority == Priority.HIGH),
-    subtasks: usersSubtasks.filter((e) => e.priority == Priority.HIGH),
-  }), [openedIncidents, usersTransports, usersReports, usersTasks, usersSubtasks])
+    transports: assignedTransports.filter((e) => e.priority == Priority.HIGH),
+    reports: assignedReports.filter((e) => e.priority == Priority.HIGH),
+    tasks: assignedTasks.filter((e) => e.priority == Priority.HIGH),
+    subtasks: assignedSubtasks.filter((e) => e.priority == Priority.HIGH),
+  }), [openedIncidents, assignedTransports, assignedReports, assignedTasks, assignedSubtasks])
 
   const dataTrackableMedium = useMemo(() => ({
     incidents: openedIncidents,
-    transports: usersTransports.filter((e) => e.priority == Priority.MEDIUM),
-    reports: usersReports.filter((e) => e.priority == Priority.MEDIUM),
-    tasks: usersTasks.filter((e) => e.priority == Priority.MEDIUM),
-    subtasks: usersSubtasks.filter((e) => e.priority == Priority.MEDIUM),
-  }), [openedIncidents, usersTransports, usersReports, usersTasks, usersSubtasks])
+    transports: assignedTransports.filter((e) => e.priority == Priority.MEDIUM),
+    reports: assignedReports.filter((e) => e.priority == Priority.MEDIUM),
+    tasks: assignedTasks.filter((e) => e.priority == Priority.MEDIUM),
+    subtasks: assignedSubtasks.filter((e) => e.priority == Priority.MEDIUM),
+  }), [openedIncidents, assignedTransports, assignedReports, assignedTasks, assignedSubtasks])
 
   const dataTrackableLow = useMemo(() => ({
     incidents: openedIncidents,
-    transports: usersTransports.filter((e) => e.priority == Priority.LOW),
-    reports: usersReports.filter((e) => e.priority == Priority.LOW),
-    tasks: usersTasks.filter((e) => e.priority == Priority.LOW),
-    subtasks: usersSubtasks.filter((e) => e.priority == Priority.LOW),
-  }), [openedIncidents, usersTransports, usersReports, usersTasks, usersSubtasks])
+    transports: assignedTransports.filter((e) => e.priority == Priority.LOW),
+    reports: assignedReports.filter((e) => e.priority == Priority.LOW),
+    tasks: assignedTasks.filter((e) => e.priority == Priority.LOW),
+    subtasks: assignedSubtasks.filter((e) => e.priority == Priority.LOW),
+  }), [openedIncidents, assignedTransports, assignedReports, assignedTasks, assignedSubtasks])
 
   const dataTrackableDone = {
     incidents: openedIncidents,
-    transports: usersDoneTransports,
-    reports: usersDoneReports,
-    tasks: usersDoneTasks,
-    subtasks: usersDoneSubtasks,
+    transports: closedTransports,
+    reports: closedReports,
+    tasks: closedTasks,
+    subtasks: closedSubtasks,
   }
 
   return (
@@ -105,6 +100,7 @@ const MeineAufgabenPage: React.VFC<Props> = ({ data }) => {
   )
 }
 export default MeineAufgabenPage
+
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({
   req,
