5355e5eb8e2ee0f7c7ac554db8dd481eb09b2143
MilenaRudig <milena.rudig@students.fhnw.ch>
2022-04-29 18:59:25 +0200

    add drawer

--- frontend/components/Organization/List/Item/OrganizationListItem.tsx
+++ frontend/components/Organization/List/Item/OrganizationListItem.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { useOrganization, useOrganizations } from '@/stores/OrganizationStore'
-import User, { isAdmin } from '@/models/User'
+import React, { EventHandler, MouseEvent } from 'react'
+import { isAdmin } from '@/models/User'
 import { StyledProps } from '@/utils/helpers/StyleHelper'
 import UiListItem from '@/components/Ui/List/Item/UiListItem'
 import UiTitle from '@/components/Ui/Title/UiTitle'
@@ -8,7 +7,6 @@ import UiGrid from '@/components/Ui/Grid/UiGrid'
 import UiDropDown from '@/components/Ui/DropDown/UiDropDown'
 import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
 import UiModal from '@/components/Ui/Modal/UiModal'
-import UserForm from '@/components/User/Form/UserForm'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
 import BackendService from '@/services/BackendService'
 import UserStore from '@/stores/UserStore'
@@ -16,9 +14,11 @@ import Id from '@/models/base/Id'
 import { useCurrentUser } from '@/stores/SessionStore'
 import Organization from '@/models/Organization'
 import OrganizationForm from '@/components/Organization/Form/OrganizationForm'
+import UiDrawer from '@/components/Ui/Drawer/UiDrawer'
 
 interface Props extends StyledProps {
   organization: Organization
+  onClick?: EventHandler<MouseEvent>
 }
 
 const OrganizationListItem: React.VFC<Props> = ({
@@ -34,44 +34,51 @@ const OrganizationListItem: React.VFC<Props> = ({
   }
 
   return (
-    <UiListItem>
-      <UiGrid align="center" gapH={1.5}>
-        <UiGrid.Col size={5}>
-          <UiTitle level={5}>
-            {organization.name}
-          </UiTitle>
-        </UiGrid.Col>
-        <UiGrid.Col size={2}>
-          <UiTitle level={6}>
-            {organization.userIds.length}
-          </UiTitle>
-        </UiGrid.Col>
-        <UiGrid.Col size={1}>
-          {(isAdmin(currentUser)) && (
-            <UiDropDown>
-              <UiDropDown.Trigger>{({ toggle }) => (
-                <UiIconButton onClick={toggle}>
-                  <UiIcon.More />
-                </UiIconButton>
-              )}</UiDropDown.Trigger>
-              <UiDropDown.Menu>
-                <UiModal title="Organisation bearbeiten">
-                  <UiModal.Trigger>{({ open }) => (
-                    <UiDropDown.Item onClick={open}>Bearbeiten</UiDropDown.Item>
-                  )}</UiModal.Trigger>
-                  <UiModal.Body>{({ close }) => (
-                    <OrganizationForm organization={organization} onClose={close} />
-                  )}</UiModal.Body>
-                </UiModal>
-                {isAdmin(currentUser) && (
-                  <UiDropDown.Item onClick={() => handleDelete(organization.id)}>Löschen</UiDropDown.Item>
-                )}
-              </UiDropDown.Menu>
-            </UiDropDown>
-          )}
-        </UiGrid.Col>
-      </UiGrid>
-    </UiListItem>
+    <UiDrawer position="right" size="full">
+      <UiDrawer.Trigger>{({ open }) => (
+        <UiListItem onClick={open}>
+          <UiGrid align="center" gapH={1.5}>
+            <UiGrid.Col size={7}>
+              <UiTitle level={5}>
+                {organization.name}
+              </UiTitle>
+            </UiGrid.Col>
+            <UiGrid.Col size={4}>
+              <UiTitle level={6}>
+                {organization.userIds.length}
+              </UiTitle>
+            </UiGrid.Col>
+            <UiGrid.Col size={1}>
+              {(isAdmin(currentUser)) && (
+                <UiDropDown>
+                  <UiDropDown.Trigger>{({ toggle }) => (
+                    <UiIconButton onClick={toggle}>
+                      <UiIcon.More />
+                    </UiIconButton>
+                  )}</UiDropDown.Trigger>
+                  <UiDropDown.Menu>
+                    <UiModal title="Organisation bearbeiten">
+                      <UiModal.Trigger>{({ open }) => (
+                        <UiDropDown.Item onClick={open}>Bearbeiten</UiDropDown.Item>
+                      )}</UiModal.Trigger>
+                      <UiModal.Body>{({ close }) => (
+                        <OrganizationForm organization={organization} onClose={close} />
+                      )}</UiModal.Body>
+                    </UiModal>
+                    {isAdmin(currentUser) && (
+                      <UiDropDown.Item onClick={() => handleDelete(organization.id)}>Löschen</UiDropDown.Item>
+                    )}
+                  </UiDropDown.Menu>
+                </UiDropDown>
+              )}
+            </UiGrid.Col>
+          </UiGrid>
+        </UiListItem>
+      )}</UiDrawer.Trigger>
+      <UiDrawer.Body>
+        I appear from the right!
+      </UiDrawer.Body>
+    </UiDrawer>
   )
 }
 export default OrganizationListItem
