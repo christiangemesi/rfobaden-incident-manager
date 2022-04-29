e6d03650ed980a8194ad68a89c52674f8f97f542
MilenaRudig <milena.rudig@students.fhnw.ch>
2022-04-29 15:08:41 +0200

    adapt orgaPage

--- frontend/pages/organisationen.tsx
+++ frontend/pages/organisationen.tsx
@@ -8,8 +8,9 @@ import UiGrid from '@/components/Ui/Grid/UiGrid'
 import { useEffectOnce } from 'react-use'
 import UserStore, { useUsers } from '@/stores/UserStore'
 import Organization, { parseOrganization } from '@/models/Organization'
-import OrganizationStore from '@/stores/OrganizationStore'
+import OrganizationStore, { useOrganizations } from '@/stores/OrganizationStore'
 import UiTitle from '@/components/Ui/Title/UiTitle'
+import OrganizationList from '@/components/Organization/List/OrganizationList'
 
 interface Props {
   data: {
@@ -18,13 +19,13 @@ interface Props {
   }
 }
 
-const BenutzerPage: React.VFC<Props> = ({ data }) => {
+const OrganisationenPage: React.VFC<Props> = ({ data }) => {
   useEffectOnce(() => {
     UserStore.saveAll(data.users.map(parseUser))
     OrganizationStore.saveAll(data.organizations.map(parseOrganization))
   })
 
-  const users = useUsers()
+  const organizations = useOrganizations()
 
   return (
     <UiContainer>
@@ -32,18 +33,19 @@ const BenutzerPage: React.VFC<Props> = ({ data }) => {
         <UiGrid>
           <UiGrid.Col>
             <UiTitle level={1}>
-              Benutzer verwalten
+              Organisationen verwalten
             </UiTitle>
           </UiGrid.Col>
         </UiGrid>
+        {/*todo: adapt respnsiveness*/}
         <UiGrid.Col size={{ md: 10, lg: 8, xl: 6 }}>
-          <UserList users={users} />
+          <OrganizationList organizations={organizations} />
         </UiGrid.Col>
       </section>
     </UiContainer>
   )
 }
-export default BenutzerPage
+export default OrganisationenPage
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({ req }) => {
   const { user, backendService } = getSessionFromRequest(req)
