8313333f3066e449efeb2ecac039d0bf8504d50e
Christians Desktop <christian.gemesi@students.fhnw.ch>
2022-04-30 11:30:29 +0200

    reverted documentFileRepository and imageFileRepoisotry

---
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/repos/DocumentFileRepository.java
@@ -0,0 +1,42 @@
+package ch.rfobaden.incidentmanager.backend.repos;
+
+import ch.rfobaden.incidentmanager.backend.errors.ApiException;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DocumentFileRepository {
+
+    public static final String RESOURCES_DIR = "files/documents/";
+
+    public void save(byte[] content, Long id) {
+        try {
+            Path newFile = Paths.get(RESOURCES_DIR + id );
+            if (!Files.exists(newFile.getParent())) {
+                Files.createDirectories(newFile.getParent());
+            }
+            Files.write(newFile, content);
+        } catch (IOException e) {
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, e.toString());
+        }
+    }
+
+    public FileSystemResource findInFileSystem(Long id) {
+        return new FileSystemResource(Paths.get(RESOURCES_DIR + id));
+    }
+
+    public boolean delete(Long id) {
+        Path file = Paths.get(RESOURCES_DIR + id);
+        try {
+            Files.delete(file);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+}
