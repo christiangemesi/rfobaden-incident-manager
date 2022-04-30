7fe39cde99f5b944c9cca206541a7e9852cd5d4b
Andri Wild <andri.wild@students.fhnw.ch>
2022-04-30 17:18:56 +0200

    Update to load document by clicking on list item

--- frontend/components/Document/Item/DocumentItem.tsx
+++ frontend/components/Document/Item/DocumentItem.tsx
@@ -8,14 +8,12 @@ import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
 
 interface Props {
   src: string
-  text: string
   id: FileId
   onDelete: (id: FileId) => void
 }
 
 const DocumentItem: React.VFC<Props> = ({
   src = '',
-  text = '',
   id,
   onDelete,
 }) => {
@@ -25,19 +23,21 @@ const DocumentItem: React.VFC<Props> = ({
     onDelete(id)
   }
   return (
-    <DocumentListItem>
-      <UiGrid style={{ padding: '0 0.5rem' }} align="center" gapH={0.5}>
-        <UiGrid.Col size={8}>
-          <FileText href={src} download>{'File with ID: ' + id}</FileText>
-        </UiGrid.Col>
-        <UiGrid.Col size={3}>
-          <p>PDF</p>
-        </UiGrid.Col>
-        <UiGrid.Col size={1}>
-          <DeleteButton onClick={handleDelete}><UiIcon.Trash /></DeleteButton>
-        </UiGrid.Col>
-      </UiGrid>
-    </DocumentListItem>
+    <FileText href={src} download>
+      <DocumentListItem>
+        <UiGrid style={{ padding: '0 0.5rem' }} align="center" gapH={0.5}>
+          <UiGrid.Col size={8}>
+            {'File with ID: ' + id}
+          </UiGrid.Col>
+          <UiGrid.Col size={3}>
+            <p>PDF</p>
+          </UiGrid.Col>
+          <UiGrid.Col size={1}>
+            <DeleteButton onClick={handleDelete}><UiIcon.Trash /></DeleteButton>
+          </UiGrid.Col>
+        </UiGrid>
+      </DocumentListItem>
+    </FileText>
   )
 }
 
@@ -51,7 +51,7 @@ const DeleteButton = styled(UiIconButton)`
 const FileText = styled.a`
   white-space: nowrap;
   overflow: hidden;
-  text-overflow: ellipsis;  
+  text-overflow: ellipsis;
   color: black;
   text-decoration: none;
 `
