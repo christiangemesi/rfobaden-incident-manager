d40b66ccfcba483168bf4ae6f6567b8d0450891d
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-05-01 11:46:28 +0200

    Fix `ImageFileRepository` imports

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/repos/ImageFileRepository.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/repos/ImageFileRepository.java
@@ -3,6 +3,11 @@ package ch.rfobaden.incidentmanager.backend.repos;
 import ch.rfobaden.incidentmanager.backend.repos.base.FileRepository;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Repository
 public class ImageFileRepository extends FileRepository {
 
@@ -11,7 +16,7 @@ public class ImageFileRepository extends FileRepository {
     }
 
     public boolean delete(Long id) {
-        Path file = Paths.get(RESOURCES_DIR + id + ".jpeg");
+        Path file = Paths.get(getResourceDir() + id + ".jpeg");
         try {
             Files.delete(file);
         } catch (IOException e) {
