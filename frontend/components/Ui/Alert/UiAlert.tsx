d93763b90e4f5a66ce0269382fd73c704311a60b
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-09 09:08:53 +0200

    Add css to component

--- frontend/components/Ui/Alert/UiAlert.tsx
+++ frontend/components/Ui/Alert/UiAlert.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 import UiTitle from '@/components/Ui/Title/UiTitle'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
 
 interface Props {
   type: 'Success' | 'info' | 'Warning' | 'Error'
@@ -8,10 +9,26 @@ interface Props {
   children: ReactNode
 }
 
-const Alert : React.VFC<Props> = () => {
-  return <div>
-    <UiTitle level={6}>Title</UiTitle>
-  </div>
+const UiAlert: React.VFC<Props> = () => {
+  return (
+    <Container>
+      <div>
+        <UiIcon.AlertCircle />
+      </div>
+      <div>
+        <UiTitle level={6}>Title</UiTitle>
+      </div>
+    </Container>
+  )
 }
 
-export default Alert
+export default UiAlert
+
+const Container = styled.div`
+  display: inline-flex;
+  background: blue;
+  opacity: 90%;
+  padding: 0.5rem;
+  border-radius: 5px;
+  
+`
