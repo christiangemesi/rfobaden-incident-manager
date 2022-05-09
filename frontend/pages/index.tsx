5ba2a49df52698319e8ed09c7d6c1a20ad17924c
MilenaRudig <milena.rudig@students.fhnw.ch>
2022-05-09 10:17:06 +0200

    add archive Card

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -30,6 +30,7 @@ const HomePage: React.VFC<Props> = ({ data }) => {
       { icon: UiIcon.IncidentManagement, label: 'Ereignisse', link: '/ereignisse' },
       { icon: UiIcon.UserManagement, label: 'Benutzer', link: '/benutzer' },
       { icon: UiIcon.Organization, label: 'Organisationen', link: '/organisationen' },
+      { icon: UiIcon.Archive, label: 'Archiv', link: '/ereignisse/archive' },
     ]
     if (firstIncident !== null) {
       // Only show transport panel if there is at least one open incident.
