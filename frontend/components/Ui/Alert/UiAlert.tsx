670919e5994f8562f4b764f96de7fa13f3a7d029
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-09 13:19:19 +0200

    Update UiAlert to remove element by callback

--- frontend/components/Ui/Alert/UiAlert.tsx
+++ frontend/components/Ui/Alert/UiAlert.tsx
@@ -1,34 +1,87 @@
-import React, { ReactNode } from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react'
+import styled, { css } from 'styled-components'
 import UiTitle from '@/components/Ui/Title/UiTitle'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
+import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
+import { useEffectOnce } from 'react-use'
 
 interface Props {
-  type: 'Success' | 'info' | 'Warning' | 'Error'
-  timeout: number
-  children: ReactNode
+  type: 'Success' | 'Info' | 'Warning' | 'Error'
+  text: string
+  onRemove : () => void
+  timeout?: number
 }
 
-const UiAlert: React.VFC<Props> = () => {
+const UiAlert: React.VFC<Props> = ({
+  type,
+  text,
+  timeout = 3000,
+  onRemove,
+}) => {
+  const [isVisible, setVisibility] = useState(true)
+
+  useEffectOnce(() => {
+    setTimeout(() => {
+      onRemove()
+    }, timeout)
+    setTimeout(() => {
+      setVisibility(false)
+    }, timeout-500)
+  })
+
   return (
-    <Container>
-      <div>
-        <UiIcon.AlertCircle />
-      </div>
-      <div>
-        <UiTitle level={6}>Title</UiTitle>
-      </div>
-    </Container>
+    <Box type={type} isVisible={isVisible}>
+      <TextContainer>
+        <IconContainer>
+          <UiIcon.AlertCircle />
+        </IconContainer>
+        <UiTitle level={6}>{text}</UiTitle>
+      </TextContainer>
+      <IconContainer>
+        <UiIconButton onClick={onRemove}><DeleteIcon /></UiIconButton>
+      </IconContainer>
+    </Box>
   )
 }
 
 export default UiAlert
 
-const Container = styled.div`
-  display: inline-flex;
-  background: blue;
-  opacity: 90%;
-  padding: 0.5rem;
+const Box = styled.div<{ type: string, isVisible: boolean }>`
+  background: ${({ theme, type }) => {
+    switch (type){
+    case 'Info' : return theme.colors.primary.value
+    case 'Success' : return theme.colors.success.value
+    case 'Error' : return theme.colors.error.value
+    case 'Warning' : return theme.colors.warning.value
+    }
+  }};
   border-radius: 5px;
+  width: 20rem;
+
+  display: inline-flex;
+  justify-content: space-between;
+  padding: 1rem;
+  opacity: 75%;
+  transition: 500ms ease-out;
+  transition-property: opacity, display;
   
+  ${({ isVisible }) => !isVisible && css`
+    opacity: 0;
+  `}
+`
+
+const TextContainer = styled.div`
+  display: inline-flex;
+  color: white;
+`
+
+const DeleteIcon = styled(UiIcon.CancelAction)`
+  color: white;
+`
+
+const IconContainer = styled.div`
+  color: white;
+
+  margin-right: 0.5rem;
+  margin-left: 0.5rem;
 `
