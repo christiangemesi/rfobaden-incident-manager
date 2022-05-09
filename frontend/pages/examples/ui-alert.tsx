6c31d7741e600e2cb4b8996862cf10ccb6c9bb1f
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-09 09:08:10 +0200

    Add AlertExample component

---
+++ frontend/pages/examples/ui-alert.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import UiAlert from '@/components/Ui/Alert/UiAlert'
+
+const UiAlertExample : React.VFC = () => {
+  return <UiAlert timeout={500} type="info">Test</UiAlert>
+}
+
+export default UiAlertExample
