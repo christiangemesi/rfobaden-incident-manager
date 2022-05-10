d7261fd764c7e8d42099d28475c05b2820ede956
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-10 14:51:20 +0200

    Add CreateButton to image drawer

--- frontend/components/Document/List/DocumentList.tsx
+++ frontend/components/Document/List/DocumentList.tsx
@@ -9,12 +9,18 @@ import UiTitle from '@/components/Ui/Title/UiTitle'
 import UiList from '@/components/Ui/List/UiList'
 import UiGrid from '@/components/Ui/Grid/UiGrid'
 import DocumentListItem from '@/components/Document/List/Item/DocumentListItem'
+import UiModal from '@/components/Ui/Modal/UiModal'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
+import DocumentForm from '@/components/Document/Form/DocumentForm'
+import styled from 'styled-components'
+import UiCreateButton from '@/components/Ui/Button/UiCreateButton'
 
 interface Props {
   fileIds: FileId[]
   modelId: Id<Incident | Report | Task>
   modelName: 'incident' | 'report' | 'task' | 'subtask'
   storeFileIds: (ids: FileId[]) => void
+  onAddFile: (fileId: FileId) => void
 }
 
 const DocumentList: React.VFC<Props> = ({
@@ -22,11 +28,11 @@ const DocumentList: React.VFC<Props> = ({
   modelId,
   modelName,
   storeFileIds,
+  onAddFile,
 }) => {
 
   const handleDelete = async (id: FileId) => {
     if (confirm('Sind sie sicher, dass sie das Dokument löschen wollen?')) {
-
       const error = await BackendService.delete('documents', id, {
         modelName: modelName,
         modelId: modelId.toString(),
@@ -44,6 +50,21 @@ const DocumentList: React.VFC<Props> = ({
       <UiTitle level={1}>
         Dokumente
       </UiTitle>
+      <UiModal title={'Dokument hinzufügen'} size="fixed">
+        <UiModal.Trigger>{({ open }) => (
+          <Button onClick={open}>
+            <UiIcon.CreateAction size={1.5} />
+          </Button>
+        )}</UiModal.Trigger>
+        <UiModal.Body>{({ close }) => (
+          <DocumentForm
+            modelId={modelId}
+            modelName={modelName}
+            onSave={onAddFile}
+            onClose={close}
+          />
+        )}</UiModal.Body>
+      </UiModal>
       <UiGrid style={{ padding: '0 0.5rem' }} gapH={0.5}>
 
         <UiGrid.Col size={8}>
@@ -67,3 +88,9 @@ const DocumentList: React.VFC<Props> = ({
 }
 
 export default DocumentList
+
+const Button = styled(UiCreateButton)`
+  position: relative;
+  font-size: 120%;
+  margin: 1rem 0;
+`
