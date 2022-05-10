78bd746a359b85ef4482493843c0576baf6c3d21
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-10 11:55:46 +0200

    Update ModelController after testing

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/base/ModelController.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/base/ModelController.java
@@ -9,7 +9,6 @@ import ch.rfobaden.incidentmanager.backend.models.paths.PathConvertible;
 import ch.rfobaden.incidentmanager.backend.services.base.ModelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 import java.util.Objects;
-import javax.servlet.http.HttpServletRequest;
 
 @RequireAdmin
 public abstract class ModelController<
@@ -39,8 +37,7 @@ public abstract class ModelController<
     @GetMapping
     @RequireAgent
     public List<TModel> list(@ModelAttribute TPath path) {
-        throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "Test");
-//        return service.list(path);
+        return service.list(path);
     }
 
     @GetMapping(value = "{id}")
