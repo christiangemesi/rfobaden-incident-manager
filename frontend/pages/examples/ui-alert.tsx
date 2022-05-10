806c5e29e95b6bf59ce88434e866533249d13d2c
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-10 11:31:03 +0200

    Update UiAlert example

--- frontend/pages/examples/ui-alert.tsx
+++ frontend/pages/examples/ui-alert.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import UiAlert from '@/components/Ui/Alert/UiAlert'
 import UiAlertList from '@/components/Ui/Alert/List/UiAlertList'
+import Alert from '@/models/Alert'
+import AlertStore, { useAlerts } from '@/stores/AlertStore'
+import { useEffectOnce } from 'react-use'
 
-const UiAlertExample : React.VFC = () => {
-  const [isVisible, setVisible] = useState(true)
+const UiAlertExample: React.VFC = () => {
+
+  const remove = (alert: Alert) => {
+    AlertStore.removeAlert(alert)
+  }
+
+  useEffectOnce(() => {
+    AlertStore.addAlert({ text: 'Beispiel', type: 'warning' })
+  })
+
+
+  const alerts = useAlerts()
+
   return (
     <UiAlertList>
-      {isVisible && (
-        <UiAlert type="info"  text="Das ist die Meldung" onRemove={() => setVisible(false)} />
+      {alerts.map((alert, idx) =>
+        <UiAlert key={idx} text={alert.text} type={alert.type} onRemove={() => remove(alert)}></UiAlert>,
       )}
     </UiAlertList>
   )
