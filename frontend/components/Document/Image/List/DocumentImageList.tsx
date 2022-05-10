6db725ba077ebd114bd4a1ab9990c7f9ef605e40
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-10 14:51:41 +0200

    Add CreateButton to image drawer

--- frontend/components/Document/Image/List/DocumentImageList.tsx
+++ frontend/components/Document/Image/List/DocumentImageList.tsx
@@ -9,19 +9,29 @@ import Task from '@/models/Task'
 import UiTitle from '@/components/Ui/Title/UiTitle'
 import UiContainer from '@/components/Ui/Container/UiContainer'
 import DocumentImageItem from '@/components/Document/Image/List/Item/DocumentImageItem'
+import UiIcon from '@/components/Ui/Icon/UiIcon'
+import UiCreateButton from '@/components/Ui/Button/UiCreateButton'
+import UiModal from '@/components/Ui/Modal/UiModal'
+import DocumentForm from '@/components/Document/Form/DocumentForm'
 
 interface Props {
   imageIds: FileId[]
   modelId: Id<Incident | Report | Task>
   modelName: 'incident' | 'report' | 'task' | 'subtask'
   storeImageIds: (ids: FileId[]) => void
+  onAddFile: (fileId: FileId) => void
 }
 
-const DocumentImageList: React.VFC<Props> = ({ imageIds, modelId, modelName, storeImageIds }) => {
+const DocumentImageList: React.VFC<Props> = ({
+  imageIds,
+  modelId,
+  modelName,
+  storeImageIds,
+  onAddFile,
+}) => {
 
   const handleDelete = async (id: FileId) => {
     if (confirm('Sind sie sicher, dass sie das Bild löschen wollen?')) {
-
       const error = await BackendService.delete('images', id, {
         modelName: modelName,
         modelId: modelId.toString(),
@@ -40,17 +50,31 @@ const DocumentImageList: React.VFC<Props> = ({ imageIds, modelId, modelName, sto
         Bilder
       </UiTitle>
       <ImageContainer>
-        {imageIds.length > 0
-          ? imageIds.map((id) => (
-            <DocumentImageItem
-              key={id}
-              src={getImageUrl(id)}
-              text="Filename"
-              id={id}
-              onDelete={handleDelete} />
-          ))
-          : <p>Keine gespeicherten Bilder</p>
-        }
+        <UiModal title={'Bild hinzufügen'} size="fixed">
+          <UiModal.Trigger>{({ open }) => (
+            <Button onClick={open}>
+              <UiIcon.CreateAction size={2} />
+            </Button>
+          )}</UiModal.Trigger>
+          <UiModal.Body>{({ close }) => (
+            <DocumentForm
+              modelId={modelId}
+              modelName={modelName}
+              type="image"
+              onSave={onAddFile}
+              onClose={close}
+            />
+          )}</UiModal.Body>
+        </UiModal>
+        {imageIds.map((id) => (
+          <DocumentImageItem
+            key={id}
+            src={getImageUrl(id)}
+            text="Filename"
+            id={id}
+            onDelete={handleDelete}
+          />
+        ))}
       </ImageContainer>
     </UiContainer>
   )
@@ -62,5 +86,12 @@ const ImageContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  gap: 0.7rem;
+  gap: 1.5rem;
+`
+const Button = styled(UiCreateButton)`
+  position: relative;
+  max-width: 12rem;
+  border-radius: 0;
+  width: 200px;
+  height: inherit;
 `
