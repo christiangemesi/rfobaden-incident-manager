bec113950d0c419f17e942137f128957772e2749
MilenaRudig <milena.rudig@students.fhnw.ch>
2022-05-10 13:36:47 +0200

    implement frontend logic for pagination

--- frontend/pages/ereignisse/archiv.tsx
+++ frontend/pages/ereignisse/archiv.tsx
@@ -10,21 +10,30 @@ import IncidentArchiveList from '@/components/Incident/Archive/List/IncidentArch
 import UiTitle from '@/components/Ui/Title/UiTitle'
 
 interface Props {
+  offset: number
   data: {
-    incidents: Incident[]
+    page: Page
   }
 }
 
-const ArchivPage: React.VFC<Props> = ({ data }) => {
+const ArchivPage: React.VFC<Props> = ({ offset, data }) => {
   useEffectOnce(() => {
-    IncidentStore.saveAll(data.incidents.map(parseIncident))
+    IncidentStore.saveAll(data.page.data.map(parseIncident))
   })
 
-  const closedIncidents = useIncidents((incidents) => incidents.filter(isClosedIncident))
+  const closedIncidents = useIncidents((incidents) => incidents
+    .filter(isClosedIncident)
+    .filter( (it) => data.page.data.find( (incident) => incident.id === it.id) )
+    .sort( (a, b) => b.closeReason.createdAt.getTime() - a.closeReason.createdAt.getTime() )
+  )
+
+  const totalPages = Math.ceil( data.page.total / PAGE_LIMIT )
 
   return (
     <UiContainer>
-
+      {[...Array(totalPages)].map((_element, i) => (
+        <div key={i}>{i + 1}</div>
+      ))}
       <UiGrid style={{ padding: '0 0 1rem 0' }}>
         <UiGrid.Col>
           <UiTitle level={1}>
@@ -42,21 +51,33 @@ const ArchivPage: React.VFC<Props> = ({ data }) => {
 }
 export default ArchivPage
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({ req }) => {
+
+const PAGE_LIMIT = 10
+
+
+interface Page {
+  total: number
+  data: Incident[]
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({ query, req }) => {
   const { user, backendService } = getSessionFromRequest(req)
   if (user === null) {
     return { redirect: { statusCode: 302, destination: '/anmelden' }}
   }
+  const offset = typeof query.p === 'string' ? parseInt(query.p) : 0
 
-  const [incidents, incidentsError]: BackendResponse<Incident[]> = await backendService.list('incidents')
-  if (incidentsError !== null) {
-    throw incidentsError
+
+  const [page, pageError]: BackendResponse<Page> = await backendService.get(`incidents/archive?limit=${PAGE_LIMIT}&offset=${offset}`)
+  if (pageError !== null) {
+    throw pageError
   }
 
   return {
     props: {
+      offset,
       data: {
-        incidents,
+        page,
       },
     },
   }
