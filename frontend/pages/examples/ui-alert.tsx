c44034b952513a6596ada0854156079de5313ebf
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-11 11:39:55 +0200

    Update UiAlertExample to use state for example data

--- frontend/pages/examples/ui-alert.tsx
+++ frontend/pages/examples/ui-alert.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UiAlert from '@/components/Ui/Alert/UiAlert'
 import UiAlertList from '@/components/Ui/Alert/List/UiAlertList'
 import Alert from '@/models/Alert'
-import AlertStore, { useAlerts } from '@/stores/AlertStore'
-import { useEffectOnce } from 'react-use'
 
 const UiAlertExample: React.VFC = () => {
 
-  const remove = (alert: Alert) => {
-    AlertStore.removeAlert(alert)
-  }
+  const [alerts, setAlerts] = useState<Alert[]>(
+    [
+      { id: 1, text: 'Info Message', type: 'info', isFading: true },
+      { id: 2, text: 'Warning Message', type: 'warning', isFading: false },
+      { id: 3, text: 'Error Message', type: 'error', isFading: false },
+      { id: 4, text: 'Success Message', type: 'success', isFading: true },
+    ],
+  )
 
-  useEffectOnce(() => {
-    AlertStore.addAlert({ text: 'Scheisse passiert', type: 'error' })
-    AlertStore.addAlert({ text: 'Irgendwas passiert', type: 'info' })
-    AlertStore.addAlert({ text: 'Etwas erstellt', type: 'success' })
-    AlertStore.addAlert({ text: 'Etwas erstellt', type: 'warning' })
+  const handleRemove = (alert: Alert) => setAlerts((allAlerts) => {
+    const index = allAlerts.indexOf(alert)
+    if (index !== -1) {
+      const newAlerts = [...allAlerts]
+      newAlerts.splice(index, 1)
+      return newAlerts
+    }
+    return allAlerts
   })
-
-  const alerts = useAlerts()
 
   return (
     <UiAlertList>
-      {alerts.map((alert, idx) =>
-        <UiAlert key={idx} text={alert.text} type={alert.type} onRemove={() => remove(alert)}></UiAlert>,
+      {alerts.map((alert) =>
+        <UiAlert key={alert.id} alert={alert} onRemove={handleRemove} />,
       )}
     </UiAlertList>
   )
