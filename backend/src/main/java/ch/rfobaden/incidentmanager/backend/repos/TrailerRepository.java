4b2540375d783ae120f87fd719e6ade8943c2b2f
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-12 12:03:21 +0200

    change transport for trailer, add data to sql

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/repos/TrailerRepository.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/repos/TrailerRepository.java
@@ -4,10 +4,12 @@ import ch.rfobaden.incidentmanager.backend.models.Trailer;
 import ch.rfobaden.incidentmanager.backend.repos.base.ModelRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TrailerRepository
     extends JpaRepository<Trailer, Long>, ModelRepository.Basic<Trailer> {
 
--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/TrailerService.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/TrailerService.java
@@ -3,10 +3,12 @@ package ch.rfobaden.incidentmanager.backend.services;
 import ch.rfobaden.incidentmanager.backend.models.Trailer;
 import ch.rfobaden.incidentmanager.backend.repos.TrailerRepository;
 import ch.rfobaden.incidentmanager.backend.services.base.ModelRepositoryService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TrailerService
     extends ModelRepositoryService.Basic<Trailer, TrailerRepository> {
 
