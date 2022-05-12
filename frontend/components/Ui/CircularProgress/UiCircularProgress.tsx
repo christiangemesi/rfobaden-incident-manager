85bf61b8e0b5b04a63333c8509872e47a7599bdd
MilenaRudig <milena.rudig@students.fhnw.ch>
2022-05-12 16:24:29 +0200

    adapt progressCercle

--- frontend/components/Ui/CircularProgress/UiCircularProgress.tsx
+++ frontend/components/Ui/CircularProgress/UiCircularProgress.tsx
@@ -13,9 +13,9 @@ const UiCircularProgress: React.VFC<Props> = ({ done, total }) => {
     return (
       <svg width={SVG_SIZE} height={SVG_SIZE}>
         <g transform={`rotate(-90 ${RADIUS_OUTER} ${RADIUS_OUTER})`}>
-          <OuterCircle radius={RADIUS_OUTER} center={SVG_CENTER} />
-          <ProgressCircle progress={progress} radius={RADIUS_PROGRESS} center={SVG_CENTER} />
-          <InnerCircle radius={RADIUS_INNER} center={SVG_CENTER} />
+          <OuterCircle radius={RADIUS_OUTER} centerX={SVG_CENTER_X} centerY={SVG_CENTER_Y} />
+          <ProgressCircle progress={progress} radius={RADIUS_PROGRESS} centerX={SVG_CENTER_X} centerY={SVG_CENTER_Y} />
+          <InnerCircle radius={RADIUS_INNER} centerX={SVG_CENTER_X} centerY={SVG_CENTER_Y} />
         </g>
         <ProgressText done={done} total={total} />
         <Percentage progress={progress} />
@@ -25,19 +25,22 @@ const UiCircularProgress: React.VFC<Props> = ({ done, total }) => {
 }
 export default UiCircularProgress
 
+const BORDER_SIZE = 2
 const RADIUS_PROGRESS = 3.5 * 16
 const RADIUS_OUTER = 4 * 16
 const RADIUS_INNER = 3 * 16
-const SVG_SIZE = RADIUS_OUTER * 2
-const SVG_CENTER = SVG_SIZE / 2
+const SVG_SIZE = RADIUS_OUTER * 2 + BORDER_SIZE * 3
+const SVG_CENTER_X = SVG_SIZE / 2 - BORDER_SIZE * 2
+const SVG_CENTER_Y = SVG_SIZE / 2
 
 interface ProgressCircleProps{
   progress: number
   radius: number
-  center: number
-} 
+  centerX: number
+  centerY: number
+}
 
-const ProgressCircle: React.VFC<ProgressCircleProps> = ({ progress, radius, center }) => {
+const ProgressCircle: React.VFC<ProgressCircleProps> = ({ progress, radius, centerX, centerY }) => {
   const theme = useTheme()
   const color = theme.colors.success.value
   const circ = 2 * Math.PI * radius
@@ -46,8 +49,8 @@ const ProgressCircle: React.VFC<ProgressCircleProps> = ({ progress, radius, cent
   return (
     <circle
       r={radius}
-      cx={center}
-      cy={center}
+      cx={centerX}
+      cy={centerY}
       fill="transparent"
       stroke={Math.round(strokePct) !== Math.round(circ) ? color : ''}
       strokeWidth="1rem"
@@ -60,35 +63,39 @@ const ProgressCircle: React.VFC<ProgressCircleProps> = ({ progress, radius, cent
 
 interface OuterCircleProps{
   radius: number
-  center: number
+  centerX: number
+  centerY: number
 }
 
-const OuterCircle: React.VFC<OuterCircleProps> =  ({ radius, center }) => {
+const OuterCircle: React.VFC<OuterCircleProps> =  ({ radius, centerX, centerY }) => {
   const theme = useTheme()
 
   return(
     <circle
       r={radius}
-      cx={center}
-      cy={center}
+      cx={centerX}
+      cy={centerY}
       fill={theme.colors.success.contrast}
+      stroke={theme.colors.secondary.value}
+      strokeWidth={`${BORDER_SIZE}px`}
     />
   )
 }
 
 interface InnerCircleProps{
   radius: number
-  center: number
+  centerX: number
+  centerY: number
 }
 
-const InnerCircle: React.VFC<InnerCircleProps> =  ({ radius, center }) => {
+const InnerCircle: React.VFC<InnerCircleProps> =  ({ radius, centerX, centerY }) => {
   const theme = useTheme()
 
   return(
     <circle
       r={radius}
-      cx={center}
-      cy={center}
+      cx={centerX}
+      cy={centerY}
       fill={theme.colors.secondary.value}
     />
   )
@@ -102,7 +109,7 @@ const Percentage: React.VFC<TextProps> =  ({ progress }) => {
   return (
     <text
       x="50%"
-      y="60%"
+      y="61%"
       dominantBaseline="central"
       textAnchor="middle"
       fontSize="15"
