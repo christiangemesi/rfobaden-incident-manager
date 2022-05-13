dc78c8cb07c6fef3acbe49b3b302ecae11a7b1db
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-13 09:23:45 +0200

    Update grid cap from 0.5 to 1 in TransportForm

--- frontend/components/Transport/Form/TransportForm.tsx
+++ frontend/components/Transport/Form/TransportForm.tsx
@@ -204,7 +204,7 @@ const TransportForm: React.VFC<Props> = ({ incident, transport = null, onSave: h
             <UiTextInput {...props} label="Anhänger" placeholder="Anhänger" />
           )}</UiForm.Field>
 
-          <UiGrid gap={0.5}>
+          <UiGrid gap={1}>
             <UiGrid.Col size={{ xs: 12, md: 6 }}>
               <UiForm.Field field={form.pointOfDeparture}>{(props) => (
                 <UiTextInput {...props} label="Abfahrtsort" placeholder="Abfahrtsort" />
@@ -227,7 +227,7 @@ const TransportForm: React.VFC<Props> = ({ incident, transport = null, onSave: h
             />
           )}</UiForm.Field>
 
-          <UiGrid gap={0.5}>
+          <UiGrid gap={1}>
             <UiGrid.Col size={{ xs: 12, md: 6 }}>
               <UiForm.Field field={form.startsAt}>{(props) => (
                 <UiDateInput {...props} label="Beginn" placeholder="dd.mm.yyyy hh:mm" />
