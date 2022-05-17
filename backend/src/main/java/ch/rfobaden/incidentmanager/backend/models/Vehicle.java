c32094e639cfc86baa6cfaa9c12246924a07ba7e
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-17 23:13:40 +0200

    add vehicle model documentation

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Vehicle.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Vehicle.java
@@ -3,6 +3,11 @@ package ch.rfobaden.incidentmanager.backend.models;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+/**
+ * {@code Vehicle} is a specific type with no additional fields or functionality.
+ * It provides basic fields, functionality and utilities
+ * of the extended base class {@link ModelWithName}.
+ */
 @Entity
 @Table(name = "vehicle")
 public final class Vehicle extends ModelWithName {
