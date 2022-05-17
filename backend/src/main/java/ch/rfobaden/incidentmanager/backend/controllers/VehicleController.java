e54c6fdd875933990018a25f76891380dc2f923d
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-17 23:52:57 +0200

    add vehicle controller documentation

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/VehicleController.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/controllers/VehicleController.java
@@ -3,6 +3,7 @@ package ch.rfobaden.incidentmanager.backend.controllers;
 
 import ch.rfobaden.incidentmanager.backend.controllers.base.ModelController;
 import ch.rfobaden.incidentmanager.backend.controllers.base.annotations.RequireAgent;
+import ch.rfobaden.incidentmanager.backend.errors.ApiException;
 import ch.rfobaden.incidentmanager.backend.models.Vehicle;
 import ch.rfobaden.incidentmanager.backend.models.paths.EmptyPath;
 import ch.rfobaden.incidentmanager.backend.services.VehicleService;
@@ -17,14 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * {@code VehicleController} extends {@link ModelController.Basic}.
+ * It provides an additional method as well as changed access on existing methods.
+ */
 @RestController
 @RequestMapping(path = "api/v1/vehicles")
 public class VehicleController
     extends ModelController.Basic<Vehicle, VehicleService> {
 
+    /**
+     * Constructor to create a new vehicle controller.
+     */
     public VehicleController() {
     }
 
+    /**
+     * Loads all visible vehicles over the defined get path.
+     *
+     * @return All vehicles whose visibility is true.
+     */
     @GetMapping("visible")
     @ResponseStatus(HttpStatus.OK)
     @RequireAgent
@@ -32,6 +45,15 @@ public class VehicleController
         return service.listVisible();
     }
 
+    /**
+     * Creates a new vehicle by calling {@link ModelController.Basic#create} with modified access.
+     * If the vehicle already exists with a given name, update the visibility to true.
+     *
+     * @param path   The vehicles' path.
+     * @param entity The vehicle.
+     * @return Created or updated vehicle.
+     * @throws ApiException If the id does not exist.
+     */
     @Override
     @RequireAgent
     public Vehicle create(@ModelAttribute EmptyPath path, @RequestBody Vehicle entity) {
@@ -43,6 +65,16 @@ public class VehicleController
         return super.create(path, entity);
     }
 
+    /**
+     * Update an existing vehicle of a given id by calling {@link ModelController.Basic#update}
+     * with modified access.
+     *
+     * @param emptyPath The vehicles' path.
+     * @param id        The id of the vehicle to be updated.
+     * @param entity    The vehicles new data.
+     * @return Updated vehicle.
+     * @throws ApiException If the id does not exist or is not equal to id of new data.
+     */
     @Override
     @RequireAgent
     public Vehicle update(
@@ -53,12 +85,29 @@ public class VehicleController
         return super.update(emptyPath, id, entity);
     }
 
+
+    /**
+     * Find a vehicle by a given id by calling {@link ModelController.Basic#find}
+     * with modified access.
+     *
+     * @param emptyPath The vehicles' path.
+     * @param id        The id of the vehicle.
+     * @return Vehicle with the given id.
+     * @throws ApiException If the id does not exist.
+     */
     @Override
     @RequireAgent
     public Vehicle find(@ModelAttribute EmptyPath emptyPath, @PathVariable Long id) {
         return super.find(emptyPath, id);
     }
 
+
+    /**
+     * List all vehicles by calling {@link ModelController.Basic#list} with modified access.
+     *
+     * @param emptyPath The vehicles' path.
+     * @return All vehicles.
+     */
     @Override
     @RequireAgent
     public List<Vehicle> list(@ModelAttribute EmptyPath emptyPath) {
