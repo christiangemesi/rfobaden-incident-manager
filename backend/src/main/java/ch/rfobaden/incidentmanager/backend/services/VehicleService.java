fcf5bcd81b21393d6c248903d7557887af0e1c6d
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-17 22:52:30 +0200

    add vehicle service documentation

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/VehicleService.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/VehicleService.java
@@ -8,14 +8,30 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * {@code VehicleService} extends {@link ModelRepositoryService.Basic}
+ * offering more vehicle specific methods in addition to the extended CRUD.
+ */
 @Service
 public class VehicleService
     extends ModelRepositoryService.Basic<Vehicle, VehicleRepository> {
 
+    /**
+     * Loads all visible vehicles.
+     *
+     * @return All vehicles whose visibility is true.
+     */
     public List<Vehicle> listVisible() {
         return repository.findAllVisible();
     }
 
+    /**
+     * Attempts to load a vehicle with a specific name.
+     *
+     * @param name The name of the vehicle.
+     * @return An {@link Optional} containing the entity,
+     *     or {@link Optional#empty()}, if no matching entity exists.
+     */
     public Optional<Vehicle> findByName(String name) {
         return repository.findByName(name);
     }
