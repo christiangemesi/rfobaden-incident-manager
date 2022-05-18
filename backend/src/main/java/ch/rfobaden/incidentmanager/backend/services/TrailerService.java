99b71f44ed08a97805d539ec47bc229a907c8d21
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-18 12:54:37 +0200

    add trailer service documentation

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/TrailerService.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/services/TrailerService.java
@@ -8,14 +8,30 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * {@code TrailerService} extends {@link ModelRepositoryService.Basic}
+ * offering more trailer specific methods in addition to the extended CRUD.
+ */
 @Service
 public class TrailerService
     extends ModelRepositoryService.Basic<Trailer, TrailerRepository> {
 
+    /**
+     * Loads all visible trailers.
+     *
+     * @return All trailers whose visibility is true.
+     */
     public List<Trailer> listVisible() {
         return repository.findAllVisible();
     }
 
+    /**
+     * Attempts to load a trailer with a specific name.
+     *
+     * @param name The name of the trailer.
+     * @return An {@link Optional} containing the trailer,
+     *     or {@link Optional#empty()}, if no matching trailer exists.
+     */
     public Optional<Trailer> findByName(String name) {
         return repository.findByName(name);
     }
