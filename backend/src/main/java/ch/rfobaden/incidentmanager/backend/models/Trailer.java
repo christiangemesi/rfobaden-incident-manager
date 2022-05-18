6fd775b8aa814d954c4e365ff8d5cad26feab35e
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-18 13:01:13 +0200

    add trailer model documentation

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Trailer.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Trailer.java
@@ -3,6 +3,10 @@ package ch.rfobaden.incidentmanager.backend.models;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+/**
+ * {@code Trailer} extends {@link ModelWithName}.
+ * It provides basic fields, functionality and utilities of {@link ModelWithName}.
+ */
 @Entity
 @Table(name = "trailer")
 public class Trailer extends ModelWithName {}
