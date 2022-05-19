72adb376592c070a60450e4144a1cecd751fcef7
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-19 21:36:50 +0200

    add vehicle store frontend documentation

--- frontend/stores/VehicleStore.ts
+++ frontend/stores/VehicleStore.ts
@@ -2,6 +2,9 @@ import { createModelStore } from '@/stores/base/Store'
 import { parseVehicle } from '@/models/Vehicle'
 import { createUseRecord, createUseRecords } from '@/stores/base/hooks'
 
+/**
+ * Store that manages name-sorted vehicles
+ */
 const VehicleStore = createModelStore(parseVehicle, {
   sortBy: (vehicle) => [
     vehicle.name.toLowerCase(),
@@ -10,6 +13,13 @@ const VehicleStore = createModelStore(parseVehicle, {
 
 export default VehicleStore
 
+/**
+ * Gets a specific vehicle from the vehicle store by its id
+ */
 export const useVehicle = createUseRecord(VehicleStore)
+
+/**
+ * Gets all vehicles from the vehicle store with an optional transformation
+ */
 export const useVehicles = createUseRecords(VehicleStore)
 
