90ed3704deaed62a192a701d0225a0e396dff734
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-20 12:38:39 +0200

    Add documentation to DocumentDrawer

--- frontend/components/Document/Drawer/DocumentDrawer.tsx
+++ frontend/components/Document/Drawer/DocumentDrawer.tsx
@@ -5,14 +5,37 @@ import DocumentList from '@/components/Document/List/DocumentList'
 import UiCaption from '@/components/Ui/Caption/UiCaption'
 
 interface Props {
+  /**
+   * The documents to be displayed.
+   */
   documents: Document[]
+
+  /**
+   * Callback to store the manipulated list of documents in the corresponding {@link Store}.
+   */
   storeDocuments: (documents: Document[]) => void
+
+  /**
+   * The {@link Id} of the corresponding model.
+   */
   modelId: number
+
+  /**
+   * The name of the corresponding model.
+   */
   modelName: 'incident' | 'report' | 'task' | 'subtask'
   children?: (props: { open: () => void }) => ReactNode
+
+  /**
+   * Caused by adding a new `Document`.
+   */
   onAddDocument: (document: Document) => void
 }
 
+/**
+ * `DocumentDrawer` is a component which uses a {@link @UiDrawer} to display documents.
+ * The `DocumentDrawer` is opening by clicking on the {@link UiCaption}.
+ */
 const DocumentDrawer: React.VFC<Props> = ({
   documents,
   storeDocuments,
