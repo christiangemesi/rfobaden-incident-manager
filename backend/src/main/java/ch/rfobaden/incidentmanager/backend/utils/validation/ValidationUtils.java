985deca807283e9c48c1189eafbea362b03f4281
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-05-20 17:54:25 +0200

    Write JavaDoc for `ValidationUtils`

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/utils/validation/ValidationUtils.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/utils/validation/ValidationUtils.java
@@ -6,19 +6,42 @@ import org.springframework.stereotype.Component;
 import java.util.function.BiConsumer;
 import javax.validation.Validator;
 
+/**
+ * {@code ValidationUtils} allows for simple in-place validations of any possible value.
+ */
 @Component
 public final class ValidationUtils {
+    /**
+     * The validator used to validate values.
+     */
     private final Validator validator;
 
     public ValidationUtils(Validator validator) {
         this.validator = validator;
     }
 
-
+    /**
+     * Validates a value using only the validations configured directly on it.
+     *
+     * @param value The value to validate.
+     * @param <T> The type of {@code value}.
+     *
+     * @throws ValidationException If the validation fails.
+     */
     public <T> void validate(T value) {
         this.validate(value, (v, vs) -> {});
     }
 
+    /**
+     * Validates a value, and allows manipulation of the resulting {@link Violations} instance.
+     *
+     * @param value The value to validate.
+     * @param compute Allows manipulation of the resulting {@link Violations} instance.
+     * @param <T> The type of {@code value}.
+     *
+     * @throws ValidationException If the {@link Violations} instance contains errors
+     *                             after applying {@code compute} to it.
+     */
     public <T> void validate(T value, BiConsumer<T, Violations> compute) {
         var violations = new Violations();
 
