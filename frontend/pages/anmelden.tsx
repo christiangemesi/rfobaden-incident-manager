716c454c76e90fcdaffe5ab8ccea6d8817d5864b
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-21 23:22:55 +0200

    fix unstable welcome text

--- frontend/pages/anmelden.tsx
+++ frontend/pages/anmelden.tsx
@@ -7,6 +7,8 @@ import styled from 'styled-components'
 import Page from '@/components/Page/Page'
 import Image from 'next/image'
 import logo from '@/public/rfobaden-logo-text.png'
+import { Themed } from '@/theme'
+import UiTitle from '@/components/Ui/Title/UiTitle'
 
 interface Props {
   imageIndex: number
@@ -45,10 +47,6 @@ const images = [
   '/assets/background1.jpg',
   '/assets/background2.jpg',
   '/assets/background3.jpg',
-  // '/assets/bg1.jpeg',
-  // '/assets/bg2.jpeg',
-  // '/assets/bg3.jpeg',
-  // '/assets/bg4.jpeg',
 ]
 
 const LogoContainer = styled.div`
@@ -60,9 +58,24 @@ const LogoContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.tertiary.value};
   z-index: 0;
   padding: 3rem;
-  box-shadow: 0 0 4px 1px ${({ theme }) => theme.colors.tertiary.contrast}
+  box-shadow: 0 0 4px 1px ${({ theme }) => theme.colors.tertiary.contrast};
+
+  @media (max-height: ${({ theme }) => theme.breakpoints.sm.min}px) {
+    margin-bottom: 2rem;
+    padding: 2rem;
+
+    img {
+      transform: scale(80%);
+    }
+    
+    + ${UiTitle} {
+      margin-bottom: 1.5rem;
+    }
+  }
 `
+
 const CenterContainer = styled(UiContainer)`
+  min-height: 370px;
   height: 100vh;
   position: relative;
   display: flex;
@@ -70,7 +83,17 @@ const CenterContainer = styled(UiContainer)`
   justify-content: center;
   align-items: center;
   padding-bottom: 10%;
+  margin-bottom: 2rem;
+
+  @media (max-height: ${({ theme }) => theme.breakpoints.sm.min}px) {
+    padding-bottom: 0;
+
+    ${Themed.media.xs.max} {
+      margin-top: -5rem;
+    }
+  }
 `
+
 const BackgroundContainer = styled.div<{ image: string }>`
   position: fixed;
   left: 0;
