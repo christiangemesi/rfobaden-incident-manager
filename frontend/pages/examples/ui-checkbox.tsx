d7269ce6168944b5233ab4279e06f1f4b0ea7cd9
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-22 11:24:24 +0200

    add example ui checkbox documentation

--- frontend/pages/examples/ui-checkbox.tsx
+++ frontend/pages/examples/ui-checkbox.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react'
 import UiCheckbox from '@/components/Ui/Checkbox/UiCheckbox'
+import UiContainer from '@/components/Ui/Container/UiContainer'
 
+/**
+ * Example to demonstrate the use of a {@link UiCheckbox}.
+ */
 const UiCheckboxExample: React.VFC = () => {
-
+  // Prepare value
   const [value, setValue] = useState(false)
+
   return (
-    <div>
+    <UiContainer style={{ paddingTop: '4rem' }}>
+      {/* Default checkbox */}
       <UiCheckbox label="Checkbox Label" value={value} onChange={setValue} />
       <br />
+
+      {/* Disabled checkbox */}
       <UiCheckbox label="Disabled Checkbox Label" isDisabled value={value} onChange={setValue} />
       <br />
+
+      {/* Validation failed checkbox */}
       <UiCheckbox label="Error Checkbox Label" value={value} onChange={setValue} errors={['Error 1', 'Error 2']} />
-    </div>
+    </UiContainer>
   )
 }
 
