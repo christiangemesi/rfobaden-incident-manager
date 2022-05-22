04ab6991ccb076e6ba49014488505b464084b1ce
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-22 11:21:35 +0200

    add example ui toggle documentation

--- frontend/pages/examples/ui-toggle.tsx
+++ frontend/pages/examples/ui-toggle.tsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react'
 import UiToggle from '@/components/Ui/Toggle/UiToggle'
+import UiContainer from '@/components/Ui/Container/UiContainer'
 
-
+/**
+ * Example to demonstrate the use of a {@link UiToggle}.
+ */
 const UiToggleExample: React.VFC = () => {
+  // Prepare value
   const [value, setValue] = useState(false as boolean | null)
+
   return (
-    <div>
-      <UiToggle value={value} onChange={setValue} label = "test" errors={['1','2']} />
-      <UiToggle value={value} onChange={setValue} label = "Apples are great" errors={['I am an error!','with multiple',' arguments','in Array']} />
-      <UiToggle value={value} onChange={setValue} label = "I am good at programming" errors={['Nothing','to','judge']} />
-    </div>
+    <UiContainer style={{ paddingTop: '4rem' }}>
+      {/* Default toggle */}
+      <UiToggle value={value} onChange={setValue} label="test" />
+
+      {/* Validation failed toggle */}
+      <UiToggle value={value} onChange={setValue} label="I am good at programming" errors={['Nothing to judge']} />
+
+      {/* Validation failed toggle with multiple fails */}
+      <UiToggle
+        value={value}
+        onChange={setValue}
+        label="Apples are great"
+        errors={['I am an error!', 'with multiple', ' arguments', 'in Array']}
+      />
+    </UiContainer>
   )
 }
 export default UiToggleExample
