a77210c47e5a849519ee176717a543dfe2189139
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-05-23 13:31:09 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- frontend/components/Ui/Input/Select/UiSelectInput.tsx
+++ frontend/components/Ui/Input/Select/UiSelectInput.tsx
@@ -249,7 +249,7 @@ type OptionAttribute<T> =
  * It re-renders whenever the option attributes changes.
  *
  * @param attr Determines how options are mapped.
- * @return A mapping function with an option parameter and a string return
+ * @return A function mapping an option to its display value.
  */
 const useOptionAttribute = <T, >(attr: OptionAttribute<T>): (option: T) => string | null => {
   return useMemo(() => {
