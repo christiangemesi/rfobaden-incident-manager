74814abec61e299e2227bdf53da41bd433281630
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-25 00:08:23 +0200

    add abschliessen to transport dropdown actions

--- frontend/components/Transport/Actions/TransportActions.tsx
+++ frontend/components/Transport/Actions/TransportActions.tsx
@@ -1,15 +1,16 @@
-import Transport from '@/models/Transport'
+import Transport, { parseTransport } from '@/models/Transport'
 import React, { useCallback } from 'react'
 import UiDropDown from '@/components/Ui/DropDown/UiDropDown'
 import UiIconButton from '@/components/Ui/Icon/Button/UiIconButton'
 import UiIcon from '@/components/Ui/Icon/UiIcon'
 import TransportForm from '@/components/Transport/Form/TransportForm'
-import BackendService from '@/services/BackendService'
+import BackendService, { BackendResponse } from '@/services/BackendService'
 import TransportStore from '@/stores/TransportStore'
 import Incident from '@/models/Incident'
 import TrackableEditAction from '@/components/Trackable/Actions/TrackableEditAction'
 import UiPrinter from '@/components/Ui/Printer/UiPrinter'
 import TransportPrintView from '@/components/Transport/PrintView/TransportPrintView'
+import TrackableCloseAction from '@/components/Trackable/Actions/TrackableCloseAction'
 
 interface Props {
   incident: Incident
@@ -28,6 +29,28 @@ const ReportActions: React.VFC<Props> = ({ incident, transport, onDelete: handle
     }
   }, [transport, handleDeleteCb])
 
+  const handleClose = useCallback(async () => {
+    if (confirm(`Sind sie sicher, dass sie den Transport "${transport.title}" abschliessen wollen?`)) {
+      const newTransport = { ...transport, isClosed: true }
+      const [data, error]: BackendResponse<Transport> = await BackendService.update(`incidents/${transport.incidentId}/transports`, transport.id, newTransport)
+      if (error !== null) {
+        throw error
+      }
+      TransportStore.save(parseTransport(data))
+    }
+  }, [transport])
+
+  const handleReopen = useCallback(async () => {
+    if (confirm(`Sind sie sicher, dass sie den Transport "${transport.title}" öffnen wollen?`)) {
+      const newTransport = { ...transport, isClosed: false }
+      const [data, error]: BackendResponse<Transport> = await BackendService.update(`incidents/${transport.incidentId}/transports`, transport.id, newTransport)
+      if (error !== null) {
+        throw error
+      }
+      TransportStore.save(parseTransport(data))
+    }
+  }, [transport])
+
   return (
     <UiDropDown>
       <UiDropDown.Trigger>
@@ -41,6 +64,8 @@ const ReportActions: React.VFC<Props> = ({ incident, transport, onDelete: handle
         <TrackableEditAction title="Transport bearbeiten">{({ close }) => (
           <TransportForm incident={incident} transport={transport} onClose={close} />
         )}</TrackableEditAction>
+
+        <TrackableCloseAction isClosed={transport.isClosed} onClose={handleClose} onReopen={handleReopen} />
 
         <UiPrinter renderContent={() => <TransportPrintView transport={transport} />}>{({ trigger }) => (
           <UiDropDown.Item onClick={trigger}>
