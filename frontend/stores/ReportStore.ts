14b74868991ab1a83143e18dc1f66e3ccfa72a33
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-25 12:12:13 +0200

    add frontend documentation report store

--- frontend/stores/ReportStore.ts
+++ frontend/stores/ReportStore.ts
@@ -6,6 +6,9 @@ import TaskStore from '@/stores/TaskStore'
 import { createUseRecord, createUseRecords } from '@/stores/base/hooks'
 import { getPriorityIndex } from '@/models/Priority'
 
+/**
+ * {@code ReportStore} manages all loaded {@link Transport transports}.
+ */
 const ReportStore = createModelStore(parseReport, {
   sortBy: (report) => [
     // Closed reports are always at the bottom.
@@ -19,24 +22,54 @@ const ReportStore = createModelStore(parseReport, {
     report.id,
   ],
 })
-
 export default ReportStore
 
+/**
+ * {@code useReport} is a React hook which loads a specific report from {@link ReportStore}.
+ * It re-renders whenever the report is changed.
+ *
+ * @param id The id of the report.
+ * @return The report.
+ */
 export const useReport = createUseRecord(ReportStore)
+
+/**
+ * {@code useReports} is a React hook that loads all reports from {@link ReportStore}.
+ * It re-renders whenever the store is modified.
+ *
+ * @param idsOrTransform? An list of ids to load, or a function that modifies the returned list.
+ * @return The list of reports.
+ */
 export const useReports = createUseRecords(ReportStore)
 
+/**
+ * {@code useReportsOfIncident} is a React hook that loads all vehicles from {@link VehicleStore}.
+ * It re-renders whenever the store is modified.
+ *
+ * @param incidentId An list of ids to load, or a function that modifies the returned list.
+ * @return The list of reports.
+ */
 export const useReportsOfIncident = (incidentId: Id<Incident>): readonly Report[] => (
   useReports((reports) => (
     reports.filter((report) => report.incidentId === incidentId)
   ), [incidentId])
 )
 
+/*
+ * After a task creation the report needs to update the `tasksIds`, `closedTasksIds` and `isDone`.
+ *
+ * <p>
+ *    A completed report, with the condition all tasks are completed, has to reopen
+ *    when a new tasks is added.
+ * </p>
+ */
 TaskStore.onCreate((task) => {
   const report = ReportStore.find(task.reportId)
   if (report === null) {
     return
   }
 
+  // Save the new task in taskIds and if necessary adjust closedTaskIds and isDone.
   ReportStore.save({
     ...report,
     taskIds: [...new Set([...report.taskIds, task.id])],
@@ -48,12 +81,23 @@ TaskStore.onCreate((task) => {
     isDone: report.isDone && (task.isClosed || task.isDone),
   })
 })
+
+/*
+ * After a task update the report needs to update the `closedTasksIds` and `isDone`.
+ *
+ * <p>
+ *    A completed report, with the condition all tasks are completed, has to reopen
+ *    when a completed tasks is reopened. An uncompleted report has to complete
+ *    when the last uncompleted task is completed.
+ * </p>
+ */
 TaskStore.onUpdate((task) => {
   const report = ReportStore.find(task.reportId)
   if (report === null) {
     return
   }
 
+  // Add/remove the updated task to/from closedTaskIds.
   const closedTaskIds = new Set(report.closedTaskIds)
   if (task.isClosed || task.isDone) {
     closedTaskIds.add(task.id)
@@ -67,12 +111,21 @@ TaskStore.onUpdate((task) => {
     isDone: closedTaskIds.size === report.taskIds.length,
   })
 })
+
+/*
+ * After a task deletion the report needs to update the `tasksIds`, `closedTasksIds` and `isDone`.
+ *
+ * <p>
+ *    An uncompleted report has to complete when the last tasks is completed.
+ * </p>
+*/
 TaskStore.onRemove((task) => {
   const report = ReportStore.find(task.reportId)
   if (report === null) {
     return
   }
 
+  // Remove the deleted task from taskIds and closedTaskIds.
   const taskIds = [...report.taskIds]
   taskIds.splice(taskIds.indexOf(task.id), 1)
 
