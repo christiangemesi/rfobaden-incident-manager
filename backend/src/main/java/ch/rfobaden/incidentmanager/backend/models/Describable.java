acea5c1aaafb82faa06fd94fbab61324d14be71f
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-05-25 08:28:22 +0200

    Write JavaDoc for `Describable`

--- backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Describable.java
+++ backend/src/main/java/ch/rfobaden/incidentmanager/backend/models/Describable.java
@@ -1,14 +1,34 @@
 package ch.rfobaden.incidentmanager.backend.models;
 
 /**
- * {@code Describable} defines a model who is described by a title and description field.
+ * {@code Describable} defines an entity who is described by a title and description field.
  */
 public interface Describable {
+    /**
+     * The entity's title.
+     *
+     * @return The title.
+     */
     String getTitle();
 
+    /**
+     * Sets the entity's title.
+     *
+     * @param title The new title.
+     */
     void setTitle(String title);
 
+    /**
+     * The entity's description.
+     *
+     * @return The description.
+     */
     String getDescription();
 
+    /**
+     * Sets the entity's description.
+     *
+     * @param description The new description.
+     */
     void setDescription(String description);
 }
