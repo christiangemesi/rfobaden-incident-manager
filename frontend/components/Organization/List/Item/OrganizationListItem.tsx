fd91f0a9fd4fa50470728e553102348cb3fb12a8
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-25 09:34:53 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- frontend/components/Organization/List/Item/OrganizationListItem.tsx
+++ frontend/components/Organization/List/Item/OrganizationListItem.tsx
@@ -32,7 +32,7 @@ interface Props extends StyledProps {
 }
 
 /**
- * `OrganizationListItem` is a component that displays one of multiple {@link Organization} in a list.
+ * `OrganizationListItem` is a component that displays one of multiple {@link Organization organizations} in a list.
  */
 const OrganizationListItem: React.VFC<Props> = ({
   organization,
