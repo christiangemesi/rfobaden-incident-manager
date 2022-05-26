93f157c210985ca1811a3af0869f036d063ed377
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-05-26 18:16:33 +0200

    add frontend documentation trackable list item

--- frontend/components/Trackable/List/Item/TrackableListItem.tsx
+++ frontend/components/Trackable/List/Item/TrackableListItem.tsx
@@ -8,14 +8,40 @@ import Trackable from '@/models/Trackable'
 import TrackableSuffix from '@/components/Trackable/Suffix/TrackableSuffix'
 
 export interface Props<T> {
+  /**
+   * The record to display.
+   */
   record: T
+
+  /**
+   * Whether the record is active/selected.
+   */
   isActive: boolean
+
+  /**
+   * Whether the record item is small.
+   */
   isSmall: boolean
+
+  /**
+   * Whether the record is closed.
+   */
   isClosed: boolean
+
+  /**
+   * Event caused by clicking on the record.
+   */
   onClick?: (record: T) => void
+
+  /**
+   * Additional content of the record to display.
+   */
   children?: ReactNode
 }
 
+/**
+ * `TrackableListItem` is a component that displays a {@link Trackable trackable entity} in a list.
+ */
 const TrackableListItem = <T extends Trackable>({
   record,
   isActive,
@@ -72,7 +98,6 @@ const Bridge = styled.div<{ isActive: boolean }>`
   border-top: 1px solid ${({ theme }) => theme.colors.grey.value};
   border-bottom: 1px solid ${({ theme }) => theme.colors.grey.value};
   
-
   ${({ isActive, theme }) => isActive && css`
     transform: scaleX(1);
     transform-origin: right center;
