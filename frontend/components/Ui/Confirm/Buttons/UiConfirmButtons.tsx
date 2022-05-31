81cd3b599cecd18a2236f27584f33f6f25997cd2
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-31 17:27:32 +0200

    Add UiConfirmButtons documentation

--- frontend/components/Ui/Confirm/Buttons/UiConfirmButtons.tsx
+++ frontend/components/Ui/Confirm/Buttons/UiConfirmButtons.tsx
@@ -3,12 +3,30 @@ import UiGrid from '../../Grid/UiGrid'
 import UiButton from '@/components/Ui/Button/UiButton'
 
 interface Props {
+  /**
+   * Defines the type of the {@link UiButton}.
+   */
   type?: 'button' | 'submit'
+
+  /**
+   * The {@link UiButton} text.
+   */
   text?: string
+
+  /**
+   * Sets the {@link UiButton} to be disabled.
+   */
   allowSubmit?: boolean
+
+  /**
+   * Event caused by clicking the {@link UiButton}.
+   */
   onSubmit?: () => void
 }
 
+/**
+ * `UiConfirmButtons` is a wrapper for a {@link UiButton}.
+ */
 const UiConfirmButtons: React.VFC<Props> = ({
   type,
   text,
