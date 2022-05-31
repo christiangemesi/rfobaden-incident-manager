d13eeeeed9eebc74310c731363f00083890111b1
Andri Wild <andri.wild@students.fhnw.ch>
2022-05-31 11:48:19 +0200

    Add documentation to UiCaption

--- frontend/components/Ui/Caption/UiCaption.tsx
+++ frontend/components/Ui/Caption/UiCaption.tsx
@@ -3,11 +3,26 @@ import styled, { css } from 'styled-components'
 import { StyledProps } from '@/utils/helpers/StyleHelper'
 
 interface Props extends StyledProps {
+
+  /**
+   * Sets the caption to be bold.
+   */
   isEmphasis?: boolean
+
+  /**
+   * The component to be styled as caption.
+   */
   children: ReactNode
+
+  /**
+   * Event caused by clicking on the {@link UiCaption}.
+   */
   onClick?: () => void
 }
 
+/**
+ * `UiCaption` is a wrapper to style a component as a caption.
+ */
 const UiCaption: React.VFC<Props> = (props) => {
   return <Caption {...props} />
 }
