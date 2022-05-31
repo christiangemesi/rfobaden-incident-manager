f238e1fdfdeb270d8a5aec1b1e99822a1febe9c5
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-05-31 13:00:42 +0200

    Write `UiDropDownTrigger` documentation

--- frontend/components/Ui/DropDown/Trigger/UiDropDownTrigger.tsx
+++ frontend/components/Ui/DropDown/Trigger/UiDropDownTrigger.tsx
@@ -2,9 +2,17 @@ import React, { ReactNode, useContext, useMemo } from 'react'
 import UiDropDownContext, { UiDropDownState } from '@/components/Ui/DropDown/Context/UiDropDownContext'
 
 interface Props {
+  /**
+   * The dropdown trigger to render.
+   *
+   * @param state The state of the dropdown.
+   */
   children: (state: UiDropDownState) => ReactNode
 }
 
+/**
+ * `UiDropDownTrigger` is a component that renders a dropdown trigger.
+ */
 const UiDropDownTrigger: React.VFC<Props> = ({ children }) => {
   const context = useContext(UiDropDownContext)
   const child = useMemo(() => children(context), [children, context])
