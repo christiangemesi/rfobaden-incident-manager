16f1dba488f041fe9e40834ffd9f0452e6728958
Ramona Marti <ramona.marti@students.fhnw.ch>
2022-06-02 18:27:56 +0200

    add documentation ui date label

--- frontend/components/Ui/DateLabel/UiDateLabel.tsx
+++ frontend/components/Ui/DateLabel/UiDateLabel.tsx
@@ -3,11 +3,25 @@ import UiDate, { UiDateType } from '@/components/Ui/Date/UiDate'
 import DateHelper from '@/utils/helpers/DateHelper'
 
 interface Props {
+  /**
+   * The start of a date range.
+   */
   start: Date
+
+  /**
+   * The end of a date range, or `null` if the range has no end.
+   */
   end?: Date | null
+
+  /**
+   * The date's format.
+   */
   type?: UiDateType
 }
 
+/**
+ * `UiDateLabel` displays a formatted date time range.
+ */
 const UiDateLabel: React.VFC<Props> = ({ start, end = null, type = 'auto' }) => {
   const dateType = useMemo(() => {
     if (type !== 'auto') {
@@ -15,6 +29,7 @@ const UiDateLabel: React.VFC<Props> = ({ start, end = null, type = 'auto' }) => 
     }
     return DateHelper.isDayRange(start, end) ? 'date' : 'datetime'
   }, [type, start, end])
+
   return (
     <span suppressHydrationWarning={true}>
       {end === null ? (
@@ -26,7 +41,6 @@ const UiDateLabel: React.VFC<Props> = ({ start, end = null, type = 'auto' }) => 
           <p><UiDate value={start} type={dateType} /> - <UiDate value={end} type={dateType} /></p>
         </React.Fragment>
       )}
-
     </span>
   )
 }
