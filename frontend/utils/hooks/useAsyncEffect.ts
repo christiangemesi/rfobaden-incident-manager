3cb191874ce0cfba5f77a4848c95608073e65f19
Daniel von Atzigen <daniel.vonatzigen@students.fhnw.ch>
2022-06-04 15:26:56 +0200

    Write JsDoc for `useAsyncEffect`

--- frontend/utils/hooks/useAsyncEffect.ts
+++ frontend/utils/hooks/useAsyncEffect.ts
@@ -1,5 +1,15 @@
 import { useEffect } from 'react'
 
+/**
+ * `useAsyncEffect` is React hook that works like `useEffect`,
+ * but allows for async callbacks.
+ * <p>
+ *   Note that due to the nature of async callbacks, the hook
+ *   does not support returning a destructor.
+ * </p>
+ * @param effect The async effect.
+ * @param deps The dependencies which can cause the effect.
+ */
 const useAsyncEffect = (effect: () => Promise<void>, deps: unknown[]): void => {
   useEffect(() => {
     effect().then()
